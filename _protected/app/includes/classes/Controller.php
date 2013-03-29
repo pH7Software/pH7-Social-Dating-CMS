ca7419f0d27c078728f48155edf97b120ad9bc8e
Pierre-Henry Soria <hizup.com@hotmail.com>
2013-03-29 18:52:30 -0400

    Adding the old Interface Controller class which had been forgotten in the implementation

--- _protected/app/includes/classes/Controller.php
+++ _protected/app/includes/classes/Controller.php
@@ -1,3 +1,13 @@
 <?php
+/**
+ * @author           Pierre-Henry Soria <ph7software@gmail.com>
+ * @copyright        (c) 2012-2013, Pierre-Henry Soria. All Rights Reserved.
+ * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package          PH7 / App / Include / Class
+ * @version          1.0
+ */
+
 namespace PH7;
-class Controller extends Framework\Mvc\Controller\Controller {}
+use PH7\Framework\Mvc\Controller as FwkCtrl;
+
+class Controller extends FwkCtrl\Controller implements FwkCtrl\IController {}
