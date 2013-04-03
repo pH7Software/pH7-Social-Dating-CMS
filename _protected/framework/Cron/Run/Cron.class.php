fe5834312f554e4090e852fba5a8aa6241deab4d
Pierre-Henry Soria <hizup.com@hotmail.com>
2013-04-03 08:17:00 -0400

    New features

--- _protected/framework/Cron/Run/Cron.class.php
+++ _protected/framework/Cron/Run/Cron.class.php
@@ -13,4 +13,63 @@
 namespace PH7\Framework\Cron\Run;
 defined('PH7') or exit('Restricted access');
 
-abstract class Cron extends \PH7\Framework\Core\Core {}
+abstract class Cron extends \PH7\Framework\Core\Core
+{
+
+    protected $sCurrentDate;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->sCurrentDate = $this->dateTime->get()->dateTime('Y-m-d H');
+    }
+
+    /**
+     * @return boolean Returns TRUE if the delay is valid, otherwise FALSE.
+     */
+    public function checkDelay()
+    {
+        $sFullPath = PH7_PATH_SYS . 'core/assets/cron/_delay/' . $this->getFileName() . '.txt';
+
+        if (!$this->file->existsFile($sFullPath))
+        {
+            $this->file->putFile($sFullPath, $this->sCurrentDate);
+            return true;
+        }
+
+        $sData = $this->file->getFile($sFullPath);
+        $sCronDate = $sData + $this->getDelay();
+
+        $bRet = ($sCronDate < $this->sCurrentDate);
+
+        if ($bRet)
+            $this->file->deleteFile($sFullPath);
+
+        return $bRet;
+    }
+
+    /**
+     * Get file name.
+     *
+     * @return string File name.
+     */
+    protected function getFileName()
+    {
+        return str_replace(array('\\', 'ph7', 'core', 'cron'), '', strtolower(get_called_class()));
+    }
+
+    /**
+     * Get the cron delay.
+     *
+     * @return integer Delay in hour.
+     */
+    protected function getDelay()
+    {
+        /**
+         * @internal We cast the value into integer type to get only the integer data (without the 'h' character).
+         */
+        return (int) \PH7\Framework\Url\Uri::getInstance()->fragment(2);
+    }
+
+}
