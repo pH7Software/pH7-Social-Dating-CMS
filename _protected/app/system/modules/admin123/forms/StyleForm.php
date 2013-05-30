38c869a7f1a01c6333dd40d0e5a1e0186c759f13
Pierre-Henry Soria <hizup.com@hotmail.com>
2013-05-30 13:09:23 -0400

    Update the CSS custom

---
+++ _protected/app/system/modules/admin123/forms/StyleForm.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <ph7software@gmail.com>
+ * @copyright      (c) 2013, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package        PH7 / App / System / Module / Admin / From
+ */
+namespace PH7;
+
+use PH7\Framework\Mvc\Model\Design as DesignModel;
+
+class StyleForm
+{
+
+    public static function display()
+    {
+        if (isset($_POST['submit_style']))
+        {
+            if (\PFBC\Form::isValid($_POST['submit_style']))
+                new StyleFormProcessing;
+            Framework\Url\HeaderUrl::redirect();
+        }
+
+        $oForm = new \PFBC\Form('form_style', '100%');
+        $oForm->configure(array('action' => ''));
+        $oForm->addElement(new \PFBC\Element\Hidden('submit_style', 'form_style'));
+        $oForm->addElement(new \PFBC\Element\Token('style'));
+        $oForm->addElement(new \PFBC\Element\Textarea(t('Your custon CSS code'), 'code', array('value' => (new DesignModel)->customCode('css'), 'description' => t("WARNING! Here you don't have to put the %0% tags.", '<b><i>&lt;style&gt;&lt;/style&gt;</i></b>'), 'style' => 'height:450px')));
+        $oForm->addElement(new \PFBC\Element\Button);
+        $oForm->render();
+    }
+
+}
