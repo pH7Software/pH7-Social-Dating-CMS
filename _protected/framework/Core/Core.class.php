ee9a9269efbbdd333aad27308a80ee1554fb7cc2
Pierre-Henry Soria <hizup.com@hotmail.com>
2013-06-05 19:20:32 -0400

    Preparation for the 1.0.1 version

--- _protected/framework/Core/Core.class.php
+++ _protected/framework/Core/Core.class.php
@@ -7,7 +7,7 @@
  * @copyright        (c) 2012-2013, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / Core
- * @version          1.0
+ * @version          1.1
  */
 
 namespace PH7\Framework\Core;
