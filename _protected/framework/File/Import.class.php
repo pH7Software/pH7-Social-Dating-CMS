e9885ecd83adb2ba357a75f7286ac980ba7174db
Pierre-Henry Soria <hizup.com@hotmail.com>
2013-06-08 06:10:03 -0400

    Remove unnecessary version number classes, and other ...

--- _protected/framework/File/Import.class.php
+++ _protected/framework/File/Import.class.php
@@ -1,13 +1,12 @@
 <?php
 /**
  * @title            Import Class
- * @desc             Importing the files.
+ * @desc             Importing files.
  *
  * @author           Pierre-Henry Soria <ph7software@gmail.com>
  * @copyright        (c) 2012-2013, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / File
- * @version          1.1
  */
 
 namespace PH7\Framework\File;
--- _protected/framework/File/Permission/Permission.class.php
+++ _protected/framework/File/Permission/Permission.class.php
@@ -6,7 +6,6 @@
  * @copyright        (c) 2013, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / File / Permission
- * @version          0.6
  */
 
 namespace PH7\Framework\File\Permission;
