424a9d5a7cfbc74d3ff4c354385bd1fc1e20fb33
Pierre-Henry Soria <hizup.com@hotmail.com>
2013-07-13 10:47:13 +0100

    Change languages key

--- public/_install/langs/en/install.lang.php
+++ public/_install/langs/en/install.lang.php
@@ -6,7 +6,7 @@
  * @copyright        (c) 2012-2013, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Lang / EN
- * @update           02/14/13 23:51
+ * @update           07/13/13 00:22
  */
 
 namespace PH7;
@@ -20,11 +20,11 @@ $LANG = array (
     <p>Please follow the six step of the installation.</p>',
     'chose_lang_for_install' => 'Please choose your language to begin the installation',
     'requirements_desc' => 'WARNING! Please be sure that your server has the necessary <a href="'.Controller::SOFTWARE_REQUIREMENTS_URL.'" target="_blank">requirements</a> to correctly run pH7CMS.',
-    'config_path' => 'Directory path &quot;protected&quot;',
+    'config_path' => '&quot;protected&quot; directory path',
     'desc_config_path' => 'Please specify the full path to your &quot;protected&quot; folder.<br />
     It is wise and advisable to put this directory outside the public root of the your Web server.',
     'need_frame' => 'You must use a Web browser that supports inline frames!',
-    'path_protected' => 'Path of folder &quot;protected&quot;',
+    'path_protected' => 'Path of the &quot;protected&quot; folder',
     'next' => 'Next',
     'go' => 'Next step =>',
     'license' => 'License',
@@ -44,14 +44,14 @@ $LANG = array (
     'error_page_not_found_desc' => 'Sorry, but the page you are looking for could not be found.',
     'success_license' => 'Your license key is correct!',
     'failure_license' => 'Sorry, your license key is incorrect!',
-    'no_dir_exist' => 'Sorry, but we did not find the directory &quot;protected&quot;',
-    'no_dir_writable' => 'Please update the directory &quot;protected&quot; in write (permission 777).',
+    'no_dir_exist' => 'Sorry, but we did not find the &quot;protected&quot; directory.',
+    'no_dir_writable' => 'Please change the permissions of the &quot;protected&quot; directory in write mode (CHMOD 777).',
     'database_error' => 'Error connecting to your database.<br /> ',
     'error_sql_import' => 'An error occurred while importing the file to your SQL database',
     'field_required' => 'This field is required',
     'all_fields_mandatory' => 'All fields marked with an asterisk (*) are required',
     'db_hostname' => 'Database Server hostname',
-    'desc_db_hostname' => '(Generally "localhost" or "127.0.0.1")',
+    'desc_db_hostname' => '(Generally &quot;localhost&quot; or &quot;127.0.0.1&quot;)',
     'db_name' =>'Name of the database',
     'db_username' => 'Username of your database',
     'db_password' => 'Database Password',
@@ -76,14 +76,14 @@ $LANG = array (
     'admin_username' => 'Your username to login in your administration panel',
     'admin_login_email' => 'Your e-mail to login in your administration panel',
     'admin_email' => 'The email address for administration',
-    'admin_return_email' => 'Email Address noreply (Generally noreply@yoursite.com)',
+    'admin_return_email' => 'Email Address noreply (generally noreply@yoursite.com)',
     'admin_feedback_email' => 'The email address for the contact form (feedback)',
     'admin_password' => 'Your password',
     'admin_passwordS' => 'Please confirm your password',
     'bad_first_name' => 'Please enter your first name, it must also be between 2 and 20 characters.',
     'bad_last_name'=> 'Please enter your last name, it must also be between 2 and 20 characters.',
-    'remove_install_folder_auto' => 'Automatically delete the directory "install" of your website (This requires access rights to delete directory "install").',
-    'confirm_remove_install_folder_auto' => 'WARNING, All files in the folder /_install/ will be removed',
+    'remove_install_folder_auto' => 'Automatically delete the &quot;install&quot; directory of your website (this requires access rights to delete the &quot;install&quot; directory).',
+    'confirm_remove_install_folder_auto' => 'WARNING, All files in the /_install/ folder will be removed.',
     'title_email_finish_install' => 'Congratulations, the installation of your site is finished!',
     'content_email_finish_install' => '<p><strong>Congratulations, your site is now successfully installed!</strong></p>
     <p>We hope you\'ll enjoy using this CMS </p>
