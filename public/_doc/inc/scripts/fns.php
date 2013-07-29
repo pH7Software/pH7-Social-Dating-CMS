de0f4698a97e39a283417adde7acd37b7dada51c
Pierre-Henry Soria <hizup.com@hotmail.com>
2013-07-29 10:39:19 -0400

    Cleaning code and fixing title bug

--- public/_doc/inc/scripts/fns.php
+++ public/_doc/inc/scripts/fns.php
@@ -14,7 +14,8 @@ defined('PH7') or exit('Restricted access');
  * @desc Detect the user's preferred language.
  * @return string The first two lowercase letter of the browser language.
  */
-function get_browser_lang() {
+function get_browser_lang()
+{
     $aLang = explode(',' ,@$_SERVER['HTTP_ACCEPT_LANGUAGE']);
     return escape(strtolower(substr(chop($aLang[0]), 0, 2)));
 }
@@ -24,8 +25,9 @@ function get_browser_lang() {
  * @param string $sPage The page.
  * @return void
  */
-function get_page($sPage) {
-    if(is_file($sPage))
+function get_page($sPage)
+{
+    if (is_file($sPage))
     {
         $sPage = file_get_contents($sPage);
         echo parse_var($sPage);
@@ -41,7 +43,8 @@ function get_page($sPage) {
  * @param string $sContent The text.
  * @return string The text parsed.
  */
-function parse_var($sContent) {
+function parse_var($sContent)
+{
     $sContent = str_replace('{site_url}', RELATIVE, $sContent);
     $sContent = str_replace('{static_url}', STATIC_URL, $sContent);
     $sContent = str_replace('{tpl_name}', TPL, $sContent);
@@ -57,11 +60,15 @@ function parse_var($sContent) {
  * @desc Get the page name.
  * @return string
  */
-function get_page_name() {
-    if(empty($_GET['p'])) {
+function get_page_name()
+{
+    if (empty($_GET['p']))
+    {
         $sName = SITE_SLOGAN;
-    } else {
-        $sPageName = str_replace(array('-','_'), '', $_GET['p']);
+    }
+    else
+    {
+        $sPageName = str_replace(array('-','_'), ' ', $_GET['p']);
         $sName = ucfirst($sPageName);
     }
 
@@ -72,7 +79,8 @@ function get_page_name() {
  * @see set_lang()
  * @return string The language available.
  */
-function get_lang() {
+function get_lang()
+{
     return set_lang();
 }
 
@@ -80,26 +88,30 @@ function get_lang() {
  * @param string $sDir The directory.
  * @return string The list of the folder that is in the directory.
  */
-function get_dir_list($sDir) {
+function get_dir_list($sDir)
+{
     $aDirList = array();
 
-    if ($rHandle = opendir($sDir)) {
-        while(false !== ($sFile = readdir($rHandle))) {
-            if ($sFile != '.' && $sFile != '..' && is_dir($sDir . '/' . $sFile)) {
+    if ($rHandle = opendir($sDir))
+    {
+        while(false !== ($sFile = readdir($rHandle)))
+        {
+            if ($sFile != '.' && $sFile != '..' && is_dir($sDir . '/' . $sFile))
                 $aDirList[] = $sFile;
-            }
         }
         closedir($rHandle);
         asort($aDirList);
         reset($aDirList);
     }
+
     return $aDirList;
 }
 
 /**
  * @return string The current URL.
  */
-function get_current_url() {
+function get_current_url()
+{
     return PROT_URL . strip_tags($_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']);
 }
 
@@ -107,8 +119,9 @@ function get_current_url() {
  * @desc Check if the language folder and the language core folder exists.
  * @return string The language available.
  */
-function set_lang() {
-    if(!empty($_GET['l']) && is_file(DATA_PATH . $_GET['l'] . '/core/welcome.tpl') && is_file(DATA_PATH . $_GET['l'] . '/core/404-error.tpl'))
+function set_lang()
+{
+    if (!empty($_GET['l']) && is_file(DATA_PATH . $_GET['l'] . '/core/welcome.tpl') && is_file(DATA_PATH . $_GET['l'] . '/core/404-error.tpl'))
     {
         setcookie('pH7_doc_lang', $_GET['l'], time()+60*60*24*365, null, null, false, true);
         $sLang = $_GET['l'];
@@ -117,7 +130,7 @@ function set_lang() {
     {
         $sLang = $_COOKIE['pH7_doc_lang'];
     }
-    elseif(is_dir(DATA_PATH . get_browser_lang() . '/core/'))
+    elseif (is_dir(DATA_PATH . get_browser_lang() . '/core/'))
     {
         $sLang = get_browser_lang();
     }
@@ -133,7 +146,8 @@ function set_lang() {
  * @desc Escape function with htmlspecialchars() PHP function.
  * @return string
  */
-function escape($sVal) {
+function escape($sVal)
+{
     return htmlspecialchars($sVal, ENT_QUOTES);
 }
 
@@ -141,7 +155,8 @@ function escape($sVal) {
  * @desc Sets an error 404 page with HTTP 404 code status.
  * @return void
  */
-function error_404() {
+function error_404()
+{
     header('HTTP/1.1 404 Not Found');
     get_page(DATA_PATH . LANG . '/core/404-error.tpl');
 }
