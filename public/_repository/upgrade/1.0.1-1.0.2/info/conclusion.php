ba07f385b2860c1b9732c0ff073b4c48e88fa633
Pierre-Henry Soria <hizup.com@hotmail.com>
2013-08-06 09:20:16 -0400

    Bugfixes in writing

--- public/_repository/upgrade/1.0.1-1.0.2/info/conclusion.php
+++ public/_repository/upgrade/1.0.1-1.0.2/info/conclusion.php
@@ -7,13 +7,7 @@ $sHtml = '';
 
 /*** Begin Contents ***/
 
-$sHtml .= '<p>' . t('Done!') . '</p>';
-$sHtml .= '<p class="red">' . t('Warning, this is not finished yet ;-)') . '</p>';
-$sHtml .= '<p>';
-$sHtml .= t('Edit your "%0%" file.', PH7_PATH_APP_CONFIG . 'config.ini') . '<br />';
-$sHtml .= t('Add M after the size of the "upload.max_size" variable (e.g., this: "upload.max_size = 500M" instead of "upload.max_size = 500"') . '<br /><br />';
-$sHtml .= t('Good luck :-)');
-$sHtml .= '</p>';
+$sHtml .= t('Done!');
 
 /*** End Contents ***/
 
