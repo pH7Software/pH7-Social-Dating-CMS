3907b5ad12f72eefb02a81ec46ca4c0552c55119
Pierre-Henry Soria <hizup.com@hotmail.com>
2013-09-09 23:11:12 -0400

    pH7CMS 1.10, upgrade pack

---
+++ public/_repository/upgrade/1.0.3-1.0.10/data/sql/MySQL/upgrade.sql
@@ -0,0 +1,22 @@
+ALTER TABLE pH7_Admins MODIFY email varchar(120) NOT NULL;
+ALTER TABLE pH7_Members MODIFY email varchar(120) NOT NULL;
+ALTER TABLE pH7_Affiliates MODIFY email varchar(120) NOT NULL;
+ALTER TABLE pH7_Subscribers MODIFY email varchar(120) NOT NULL;
+ALTER TABLE pH7_Language MODIFY email varchar(120) DEFAULT NULL;
+ALTER TABLE pH7_AdminsLogLogin MODIFY email varchar(120) NOT NULL DEFAULT '';
+ALTER TABLE pH7_MembersLogLogin MODIFY email varchar(120) NOT NULL DEFAULT '';
+ALTER TABLE pH7_AffiliatesLogLogin MODIFY email varchar(120) NOT NULL DEFAULT '';
+ALTER TABLE pH7_AdminsLogSess MODIFY email varchar(120) DEFAULT NULL;
+ALTER TABLE pH7_MembersLogSess MODIFY email varchar(120) DEFAULT NULL;
+ALTER TABLE pH7_AffiliatesLogSess MODIFY email varchar(120) DEFAULT NULL;
+
+ALTER TABLE pH7_MembersInfo MODIFY website varchar(120) DEFAULT NULL;
+ALTER TABLE pH7_MembersInfo MODIFY socialNetworkSite varchar(120) DEFAULT NULL;
+ALTER TABLE pH7_AffiliatesInfo MODIFY website varchar(120) DEFAULT NULL;
+ALTER TABLE pH7_Language MODIFY website varchar(120) DEFAULT NULL;
+
+ALTER TABLE pH7_MembersInfo DROP fax;
+
+ALTER TABLE pH7_Admins MODIFY timeZone varchar(6) NOT NULL DEFAULT '-6';
+
+INSERT INTO pH7_Settings (`name`, value, `desc`, `group`) VALUES ('maxUsernameLength', '30', '', 'registration');
