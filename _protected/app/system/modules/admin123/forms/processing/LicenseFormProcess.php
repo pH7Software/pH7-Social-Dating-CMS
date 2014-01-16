c2f26b5ab864d1c2c6be1bbebe391ce540e7c687
Pierre-Henry Soria <hizup.com@hotmail.com>
2014-01-16 19:12:03 +0000

    New system to manage the license key

---
+++ _protected/app/system/modules/admin123/forms/processing/LicenseFormProcess.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <ph7software@gmail.com>
+ * @copyright      (c) 2014, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package        PH7 / App / System / Module / Admin / From / Processing
+ */
+namespace PH7;
+defined('PH7') or die('Restricted access');
+
+use PH7\Framework\Mvc\Model\License, PH7\Framework\Mvc\Request\Http;
+
+class LicenseFormProcess extends Form
+{
+
+    public function __construct($iLicenseId)
+    {
+        parent::__construct();
+
+        $oLicense = new License;
+        $sKey = $this->httpRequest->post('basic_key', Http::NO_CLEAN) . ';' . $this->httpRequest->post('copyright_key', Http::NO_CLEAN) . ';';
+
+        if (!$this->str->equals($sKey, $oLicense->get($iLicenseId)))
+            $oLicense->save($sKey, $iLicenseId);
+
+        \PFBC\Form::setSuccess('form_license', t('Your License Key was saved successfully!'));
+    }
+
+}
---
+++ _protected/framework/Mvc/Model/License.class.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author           Pierre-Henry Soria <ph7software@gmail.com>
+ * @copyright        (c) 2014, Pierre-Henry Soria. All Rights Reserved.
+ * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package          PH7 / Framework / Mvc / Model
+ */
+
+namespace PH7\Framework\Mvc\Model;
+defined('PH7') or exit('Restricted access');
+
+class License extends Engine\Model
+{
+
+    /**
+     * Get the license key.
+     *
+     * @param integer $iId The License ID. Default 1
+     * @return string
+     */
+    public function get($iId = 1)
+    {
+        return $this->orm->getOne('License', 'licenseId', $iId, 'licenseKey')->licenseKey;
+    }
+
+    /**
+     * Update the license key.
+     *
+     * @param string $sKey
+     * @param integer $iId Column ID. Default 1
+     * @return mixed (integer | boolean) Returns the number of rows on success or FALSE on failure.
+     */
+    public function save($sKey, $iId = 1)
+    {
+        return $this->orm->update('License', 'licenseKey', $sKey, 'licenseId', $iId);
+    }
+
+}
