784c07e40a71121efb4ebf65ffb1c476ed53b347
Pierre-Henry Soria <hizup.com@hotmail.com>
2014-03-28 20:23:21 +0000

    Release date updated

--- _protected/framework/Security/Version.class.php
+++ _protected/framework/Security/Version.class.php
@@ -23,7 +23,7 @@ final class Version
     /***** Framework Kernel *****/
     const KERNEL_VERSION = Kernel::SOFTWARE_VERSION;
     const KERNEL_BUILD = Kernel::SOFTWARE_BUILD;
-    const KERNEL_RELASE_DATE = '2014-01-30';
+    const KERNEL_RELASE_DATE = '2014-03-29';
     const KERNEL_VERSION_NAME = Kernel::SOFTWARE_VERSION_NAME;
 
     /***** Form PFBC *****/
