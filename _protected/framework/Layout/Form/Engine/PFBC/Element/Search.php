5d954a83315dd4a6e41163576a9518644ed8ca50
Pierre-Henry Soria <hizup.com@hotmail.com>
2014-05-18 13:37:42 +0100

    Adding Search by voice

--- _protected/framework/Layout/Form/Engine/PFBC/Element/Search.php
+++ _protected/framework/Layout/Form/Engine/PFBC/Element/Search.php
@@ -11,6 +11,7 @@ class Search extends Textbox
     public function render()
     {
         $this->attributes['type'] = 'search'; // Search Type
+        $this->attributes['x-webkit-speech'] = 'x-webkit-speech'; // Voice search. Only for Webkit engine.
         parent::render();
     }
 
