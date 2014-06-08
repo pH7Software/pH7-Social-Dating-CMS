b7d4aa31b5e949c252d396d87705032ab54cbdc5
Pierre-Henry Soria <hizup.com@hotmail.com>
2014-06-08 23:38:00 +0100

    Add a new Benchmark library

--- _protected/framework/Benchmark/Benchmark.class.php
+++ _protected/framework/Benchmark/Benchmark.class.php
@@ -2,27 +2,156 @@
 /**
  * @title            Benchmark Class
  *
- * @author           Pierre-Henry Soria <ph7software@gmail.com>
- * @copyright        (c) 2012-2014, Pierre-Henry Soria. All Rights Reserved.
- * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / Navigation
- * @version          0.1
+ *
+ * Copyright (c) 2012 Jeremy Perret
+ * File Modified by Pierre-Henry Soria, Copyright (c) 2014
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is furnished
+ * to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 
 namespace PH7\Framework\Benchmark;
 defined('PH7') or exit('Restricted access');
-/**
- * This class is still under development, if you are a developer and you want to help us and join our volunteer team of developers to continue development of this class, you're welcome!
- * Please contact us by email: ph7software@gmail.com
- *
- * Thank you,
- * pH7 developers team.
- */
+
 class Benchmark
 {
 
-    public function __construct()
+    protected $fStartTime;
+
+    protected $fEndTime;
+
+    protected $iMemoryUsage;
+
+    /**
+     * Sets start microtime
+     *
+     * @return void
+     */
+    public function start()
     {
+        $this->fStartTime = microtime(true);
+    }
+
+    /**
+     * Sets end microtime
+     *
+     * @return void
+     */
+    public function end()
+    {
+        $this->fEndTime = microtime(true);
+        $this->iMemoryUsage = memory_get_usage(true);
+    }
+
+    /**
+     * Returns the elapsed time, readable or not
+     *
+     * @param  boolean $readable Whether the result must be human readable
+     * @param  string  $format   The format to display (printf format)
+     * @return string|float
+     */
+    public function getTime($raw = false, $format = null)
+    {
+        $elapsed = $this->fEndTime - $this->fStartTime;
+
+        return $raw ? $elapsed : self::readableElapsedTime($elapsed, $format);
+    }
+
+    /**
+     * Returns the memory usage at the end checkpoint
+     *
+     * @param  boolean $readable Whether the result must be human readable
+     * @param  string  $format   The format to display (printf format)
+     * @return string|float
+     */
+    public function getMemoryUsage($raw = false, $format = null)
+    {
+        return $raw ? $this->iMemoryUsage : self::readableSize($this->iMemoryUsage, $format);
+    }
+
+    /**
+     * Returns the memory peak, readable or not
+     *
+     * @param  boolean $readable Whether the result must be human readable
+     * @param  string  $format   The format to display (printf format)
+     * @return string|float
+     */
+    public function getMemoryPeak($raw = false, $format = null)
+    {
+        $memory = memory_get_peak_usage(true);
+
+        return $raw ? $memory : self::readableSize($memory, $format);
+    }
+
+    /**
+     * Returns a human readable memory size
+     *
+     * @param   int    $size
+     * @param   string $format   The format to display (printf format)
+     * @param   int    $round
+     * @return  string
+     */
+    public static function readableSize($size, $format = null, $round = 3)
+    {
+        $mod = 1024;
+
+        if (is_null($format)) {
+            $format = '%.2f%s';
+        }
+
+        $units = explode(' ','B Kb Mb Gb Tb');
+
+        for ($i = 0; $size > $mod; $i++) {
+            $size /= $mod;
+        }
+
+        if (0 === $i) {
+            $format = preg_replace('/(%.[\d]+f)/', '%d', $format);
+        }
+
+        return sprintf($format, round($size, $round), $units[$i]);
+    }
+
+    /**
+     * Returns a human readable elapsed time
+     *
+     * @param  float $microtime
+     * @param  string  $format   The format to display (printf format)
+     * @return string
+     */
+    public static function readableElapsedTime($microtime, $format = null, $round = 3)
+    {
+        if (is_null($format)) {
+            $format = '%.3f%s';
+        }
+
+        if ($microtime >= 1) {
+            $unit = 's';
+            $time = round($microtime, $round);
+        } else {
+            $unit = 'ms';
+            $time = round($microtime*1000);
+
+            $format = preg_replace('/(%.[\d]+f)/', '%d', $format);
+        }
+
+        return sprintf($format, $time, $unit);
     }
 
 }
