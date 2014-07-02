9ccd69930e65dee53e8b1829df1c5ce031f82864
Pierre-Henry Soria <hizup.com@hotmail.com>
2014-07-02 10:19:45 +0100

    New Licensing system

--- _protected/app/system/modules/admin123/forms/LicenseForm.php
+++ _protected/app/system/modules/admin123/forms/LicenseForm.php
@@ -7,7 +7,7 @@
  */
 namespace PH7;
 
-use PH7\Framework\Mvc\Model\License;
+use PH7\Framework\Mvc\Router\Uri, PH7\Framework\Mvc\Model\License;
 
 class LicenseForm
 {
@@ -20,17 +20,14 @@ class LicenseForm
         {
             if (\PFBC\Form::isValid($_POST['submit_license']))
                 new LicenseFormProcess(self::$_iLicenseId);
-            Framework\Url\HeaderUrl::redirect();
+            Framework\Url\HeaderUrl::redirect(Uri::get(PH7_ADMIN_MOD, 'setting', 'license', '?set_msg=1'));
         }
-
-        $aLicenseContent = explode(';', (new License)->get(self::$_iLicenseId));
 
         $oForm = new \PFBC\Form('form_license', 500);
         $oForm->configure(array('action' => ''));
         $oForm->addElement(new \PFBC\Element\Hidden('submit_license', 'form_license'));
         $oForm->addElement(new \PFBC\Element\Token('license'));
-        $oForm->addElement(new \PFBC\Element\Hidden('basic_key', $aLicenseContent[0]));
-        $oForm->addElement(new \PFBC\Element\Textbox(t('Your License Key'), 'copyright_key', array('description' => '<strong><a href="' . Core::SOFTWARE_LICENSE_KEY_URL . '">' . t('Buy a license') . '</a></strong> ' . t('to remove the links from us.'), 'value' => $aLicenseContent[1], 'required' => 1)));
+        $oForm->addElement(new \PFBC\Element\Textbox(t('Your License Key'), 'copyright_key', array('description' => '<strong><a href="' . Core::SOFTWARE_LICENSE_KEY_URL . '">' . t('Buy a license') . '</a></strong> ' . t('to remove the links from us.'), 'value' => (new License)->get(self::$_iLicenseId), 'required' => 1)));
         $oForm->addElement(new \PFBC\Element\Button(t('Register'), 'submit', array('icon' => 'key')));
         $oForm->render();
     }
