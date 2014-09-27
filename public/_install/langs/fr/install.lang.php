77279dc6d11e855aaff1439cc1b67ac8dd7c904f
Pierre-Henry Soria <hizup.com@hotmail.com>
2014-09-27 22:05:30 +0100

    Change some language keys

--- public/_install/langs/fr/install.lang.php
+++ public/_install/langs/fr/install.lang.php
@@ -105,7 +105,7 @@ $LANG = array (
     'service' => 'Services additionnels utiles',
     'buy_copyright_license_title' => 'Acheter une licence',
     'buy_copyright_license' => '<span class="gray">Licence à vie</span> <span class="right">Acheter maintenant</span>',
-    'buy_copyright_license_desc' => 'En achetant une clé de licence, vous n\'aurez plus aucuns liens et mentions de droit d\'auteur sur votre site.',
+    'buy_copyright_license_desc' => 'En achetant une licence, vous n\'aurez plus aucuns liens et mentions de droit d\'auteur sur votre site, et vous serez également capable de le mettre à jour/à niveau.',
     'buy_individual_ticket_support_title' => 'Acheter un service support technique individuel',
     'buy_individual_ticket_support' => '<span class="gray">Support technique individuel complet pour un mois</span> <span class="right">Acheter maintenant</span>',
     'buy_individual_ticket_support_desc' => 'En achetant un support technique individuel, nous allons vous aider à chaque fois que vous aurez un problème avec notre logiciel. Nous serons à votre disposition pour résoudre d\'éventuelles problème que rencontreriez avec pH7CMS.',
