083d5722a8a5e4fde8bf9b0d936f753c39813eaf
Pierre-Henry Soria <hizup.com@hotmail.com>
2014-11-24 22:58:16 +0000

    Change some strings...

--- _protected/app/system/modules/admin123/forms/ImportUserForm.php
+++ _protected/app/system/modules/admin123/forms/ImportUserForm.php
@@ -24,7 +24,7 @@ class ImportUserForm
 
         $oForm = new \PFBC\Form('form_import_user',550);
         $oForm->configure(array('action' => '' ));
-        $oForm->addElement(new \PFBC\Element\HTMLExternal('<p>' . t('Import members into your site from a <strong><abbr title="Comma Separated Values">CSV</abbr></strong> file. You can find database users <strong><a href="%0%">here</a></strong>.', 'http://www.saledatingprofiles.com/?from=hizup.com') . '</p>'));
+        $oForm->addElement(new \PFBC\Element\HTMLExternal('<p>' . t('Import members into your site from a <strong><abbr title="Comma Separated Values">CSV</abbr></strong> file. You can find database users <strong><a href="%0%">here</a></strong>.', 'http://www.saledatingprofiles.com/?from=ph7cms.com') . '</p>'));
         $oForm->addElement(new \PFBC\Element\Hidden('submit_import_user', 'form_import_user'));
         $oForm->addElement(new \PFBC\Element\Token('import_user'));
         $oForm->addElement(new \PFBC\Element\File(t('CSV file:'), 'csv_file', array('accept'=>'.csv', 'required'=>1)));
