cf5426a76eb5815bf379f04cb6c380fea92e0e5d
Pierre-Henry Soria <hizup.com@hotmail.com>
2015-01-07 21:21:24 +0000

    Updating the copyright date in some missing files

--- _install/data/sql/MySQL/pH7_DataGame.sql
+++ _install/data/sql/MySQL/pH7_DataGame.sql
@@ -3,7 +3,7 @@
 -- Title:         SQL Data Game Install File
 --
 -- Author:        Pierre-Henry Soria <ph7software@gmail.com>
--- Copyright:     (c) 2012-2014, Pierre-Henry Soria. All Rights Reserved.
+-- Copyright:     (c) 2012-2015, Pierre-Henry Soria. All Rights Reserved.
 -- License:       GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
 -- Package:       PH7 / Install / Data / Sql
 -- Version:       1.1
