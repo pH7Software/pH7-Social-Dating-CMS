a3f50e1f6e77a681a4e8cae0bc55e364cdf85879
Pierre-Henry Soria <ph7software@gmail.com>
2015-08-16 09:54:14 +0100

    Update Some pH7CMS's Contact Details

--- _install/data/configs/constants.php
+++ _install/data/configs/constants.php
@@ -2,10 +2,10 @@
 /**
  * @title            Constants
  *
- * @author           Pierre-Henry Soria <ph7software@gmail.com>
+ * @author           Pierre-Henry Soria <hello@ph7cms.com>
  * @copyright        (c) 2012-2015, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
- * @link             http://software.hizup.com
+ * @link             http://ph7cms.com
  * @package          PH7
  * @version          1.0
  */
--- index.php
+++ index.php
@@ -4,11 +4,12 @@
  * @desc           Index file for public root.
  *
  * @author         HiZup, Ltd <contact@hizup.net>
- * @author         Pierre-Henry Soria <contact@hizup.net>
+ * @author         Pierre-Henry Soria <hello@ph7cms.com>
  * @copyright      (c) 2014-2015, HiZup, Ltd. All Rights Reserved.
  * @copyright      (c) 2012-2015, Pierre-Henry Soria. All Rights Reserved.
  * @license        See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
- * @link           http://software.hizup.com
+ * @link           http://hizup.uk                 
+ * @link           http://ph7cms.com
  * @package        PH7 / ROOT / Core
  * @version        1.0
  */
