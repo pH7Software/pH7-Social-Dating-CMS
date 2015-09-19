92d931159a23ffd203a4a601663c9af2514f1740
Pierre-Henry Soria <hizup.com@hotmail.com>
2015-09-19 20:11:49 +0100

    Change some default texts

--- _install/data/sql/MySQL/pH7_Core.sql
+++ _install/data/sql/MySQL/pH7_Core.sql
@@ -956,7 +956,7 @@ CREATE TABLE IF NOT EXISTS pH7_Settings (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO pH7_Settings (`name`, value, `desc`, `group`) VALUES
-('siteName', 'My Own Social/Dating Site!', '', 'general'),
+('siteName', 'My Social/Dating App!', '', 'general'),
 ('adminEmail', @sAdminEmail, '', 'email'),
 ('defaultLanguage', 'en_US', '', 'language'),
 ('defaultTemplate', 'base', '', 'design'),
