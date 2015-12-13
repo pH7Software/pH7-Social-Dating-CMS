f6cdb0314611a126f41feccbdf0d8045e5638a31
Pierre-Henry Soria <hizup.com@hotmail.com>
2015-12-13 15:49:23 +0000

    Update some text and string

--- _install/langs/en/install.lang.php
+++ _install/langs/en/install.lang.php
@@ -32,7 +32,7 @@ $LANG = array (
     'site_name' => 'Site Name',
     'license' => 'Your License',
     'license_desc' => 'Please read the license carefully and accept it before continuing the installation of the software!',
-    'registration_for_license' => 'If you haven\'t done it yet, it\'s a good time to buy now <a href="' . Controller::SOFTWARE_LICENSE_KEY_URL . '" target="_blank">a license key</a> in order to get the Premium Features offered by the software.<br /> If your want to try first pH7CMS with its basic features and promo links, you can skip this step.',
+    'registration_for_license' => 'If you haven\'t done it yet, it\'s a good time to buy now <a href="' . Controller::SOFTWARE_LICENSE_KEY_URL . '" target="_blank">a license key</a> to get all Professional Premium Features offered by the software.<br /> If you wish to try first pH7CMS with its basic features and promo links, you can skip this step.',
     'your_license' => 'Your License Key',
     'agree_license' => 'I have read and agree to the above Terms.',
     'step' => 'Step',
@@ -62,7 +62,7 @@ $LANG = array (
     'db_username' => 'Database username',
     'db_password' => 'Database password',
     'db_prefix' => 'Prefix for tables in database',
-    'desc_db_prefix' => 'This option is useful when you have multiple installations of pH7CMS on the same database. We recommend that you still change the default values ​​in order to increase the security of your website.',
+    'desc_db_prefix' => 'This option is useful when you have multiple installations of pH7CMS on the same database. We recommend that you change the default values ​​in order to increase the security.',
     'db_encoding' => 'Encoding',
     'desc_db_encoding' => 'Database encoding. Usually UTF8 encoding for international.',
     'db_port' => 'Database host port number',
@@ -98,7 +98,7 @@ $LANG = array (
     <p>The URL of Your OWN Social/Dating website is: <a href="' . PH7_URL_ROOT . '">' . PH7_URL_ROOT . '</a></p>
     <p>Your Admin Panel URL is: <a href="' . PH7_URL_ROOT . 'admin123/">' . PH7_URL_ROOT . 'admin123/</a></p>
     <p>Don\'t forget to show off by showing YOUR new Social/Dating Site to all your friends, your colleagues and your Facebook\'s mates (and even to your enemies... or not).</p>
-    <p>Finally, if you haven\'t done it yet, it\'s a really good time to buy today a license key by simply <a href="' . Controller::SOFTWARE_LICENSE_KEY_URL . '" target="_blank">visiting our website</a> in order to get Premium Features, Remove all Links and Copyright Notice on your Website and even the access to the Unlimited Support Ticket.</p>
+    <p>Finally, if you haven\'t done it yet, it\'s a really good time to buy a license today by simply <a href="' . Controller::SOFTWARE_LICENSE_KEY_URL . '" target="_blank">visiting our website</a> in order to get all Premium Modules/Features, Remove all Links and Copyright Notice on your Website and even get access to the Unlimited Support Ticket.</p>
     <p>&nbps;</p>
     <p>P.S. For any bug reports, suggestions, partnership, translation, contribution or other,
     please visit our <a href="' . Controller::SOFTWARE_WEBSITE . '">website</a>.</p>
@@ -112,7 +112,7 @@ $LANG = array (
     'service' => 'Useful additional services',
     'buy_copyright_license_title' => 'Buy a Copyright Removal License',
     'buy_copyright_license' => '<span class="gray">One-time Payment</span> <span class="right">Buy Now</span>',
-    'buy_copyright_license_desc' => 'By buying a License, you won\'t have any Links and Copyright Notice on your website, get all Premium Features and you\'ll also be able to Update/Upgrade it.',
+    'buy_copyright_license_desc' => 'By buying a License, you won\'t have any Links and Copyright Notice, you will get all Premium Mods/Features and also be entitled to all next Update/Upgrade versions of the software.',
     'buy_individual_ticket_support_title' => 'Buy an Individual Support Service',
     'buy_individual_ticket_support' => '<span class="gray">Full ticket support for one month</span> <span class="right">Buy Now</span>',
     'buy_individual_ticket_support_desc' => 'By purchasing an individual unlimited ticket support, we\'ll help you whenever you have a issue/problem with our software. We are at your disposal to solve any problem encounter with pH7CMS.',
--- _protected/app/system/modules/page/views/base/tpl/main/link.tpl
+++ _protected/app/system/modules/page/views/base/tpl/main/link.tpl
@@ -1,7 +1,7 @@
 <div class="center">
 
   <ul>
-    <li><a href="http://01tchat.com" title="Free Dating Social">Free Dating Social</a></li>
+    <li>{{ $design->smartLink() }}</li>
   </ul>
 
 </div>
