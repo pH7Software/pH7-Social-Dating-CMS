6d12e0973315c333cda424d8a17230635e71803f
Pierre-Henry Soria <hizup.com@hotmail.com>
2015-12-14 03:18:29 +0000

    Add Validate Site Module

---
+++ _protected/app/system/modules/validate-site/views/base/tpl/main/validationbox.tpl
@@ -0,0 +1,4 @@
+<div id="box_block">
+    <script src="//static.hizup.com/js/validate-site/mailbox.js"></script>
+    <script>$('form input[name=ph7cmsurl]').val(pH7Url.base);$('form input[name=ph7cmsvalidatorurl]').val(pH7Url.base + 'validate-site/main/validator');</script>
+</div>
---
+++ templates/system/modules/validate-site/themes/base/js/validationbox.js
@@ -0,0 +1,19 @@
+/*
+ * Author:        Pierre-Henry Soria <hello@ph7cms.com>
+ * Copyright:     (c) 2015, Pierre-Henry Soria. All Rights Reserved.
+ * License:       GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ */
+
+var $validationBox = (function() {
+    $.get(pH7Url.base + 'validate-site/main/validationbox', function(oData) {
+        if(oData.length > 1) {
+            $.colorbox({
+                width      :  '450px',
+                maxHeight  :  '85%',
+                html       : $(oData).find('#box_block')
+            });
+       }
+   });
+});
+
+$validationBox();
