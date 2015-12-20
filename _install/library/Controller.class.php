d488ab2e5e921a97cf4f206e908808c2f5614d3c
Pierre-Henry Soria <hizup.com@hotmail.com>
2015-12-20 19:26:05 +0000

    Set new version date and version name

--- _install/library/Controller.class.php
+++ _install/library/Controller.class.php
@@ -31,7 +31,8 @@ abstract class Controller implements IController
     SOFTWARE_AUTHOR = 'Pierre-Henry Soria',
     SOFTWARE_LICENSE = 'GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.',
     SOFTWARE_COPYRIGHT = '© (c) 2012-2015, Pierre-Henry Soria. All Rights Reserved.',
-    SOFTWARE_VERSION_NAME = 'pOW', // 1.0 and 1.1 branches were "pOH", 1.2 branch is now "pOW" and the new one should be "p[H]"
+    /** "Xmas" Version is a really "special" one as it will be released on the 24th with some unexpected features!!! Happy Christmas guys! **/
+    SOFTWARE_VERSION_NAME = 'Xmas', // 1.0 and 1.1 branches were "pOH", 1.2 branch is now "pOW" and the new one should be "p[H]"
     SOFTWARE_VERSION = '1.2.7',
     SOFTWARE_BUILD = '1',
     DEFAULT_LANG = 'en',
