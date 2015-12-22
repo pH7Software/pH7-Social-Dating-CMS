ddf318ed993b42c2484e51d9da42712b5b63359a
Pierre-Henry Soria <hizup.com@hotmail.com>
2015-12-22 20:33:59 +0000

    Make the code compatible for PHP7+

--- _protected/framework/Layout/Form/Engine/PFBC/Base.php
+++ _protected/framework/Layout/Form/Engine/PFBC/Base.php
@@ -36,15 +36,20 @@ abstract class Base
                 {
                     /*If the appropriate class has a "set" method for the property provided, then
                     it is called instead or setting the property directly.*/
-                    if(isset($method_reference['set' . $property]))
-                        $this->$method_reference['set' . $property]($value);
-                    elseif(isset($property_reference[$property]))
-                        $this->$property_reference[$property] = $value;
+                    if(isset($method_reference['set' . $property])) {
+                        $methodName = $method_reference['set' . $property];
+                        $this->$methodName($value);
+                    }
+                    elseif(isset($property_reference[$property])) {
+                        $methodName = $property_reference[$property];
+                        $this->$methodName = $value;
+                    }
                     /*Entries that don't match an available class property are stored in the attributes
                     property if applicable.  Typically, these entries will be element attributes such as
                     class, value, onkeyup, etc.*/
-                    elseif(isset($property_reference['attributes']))
+                    elseif(isset($property_reference['attributes'])) {
                         $this->attributes[$property] = $value;
+                    }
                 }
             }
         }
