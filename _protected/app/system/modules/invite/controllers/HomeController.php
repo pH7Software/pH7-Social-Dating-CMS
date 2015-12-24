db9af512c3b67c207e1df7fccf289fe1c7da9b30
Pierre-Henry Soria <hizup.com@hotmail.com>
2015-12-24 00:16:52 +0000

    Improvement, cleanup code because now h1 title is used in tpl file for showing in popup box

--- _protected/app/system/modules/invite/controllers/HomeController.php
+++ _protected/app/system/modules/invite/controllers/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @author         Pierre-Henry Soria <ph7software@gmail.com>
+ * @author         Pierre-Henry Soria <hello@ph7cms.com>
  * @copyright      (c) 2013-2015, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package        PH7 / App / System / Module / Invite / Controller
@@ -9,14 +9,10 @@ namespace PH7;
 
 class HomeController extends Controller
 {
-    private $sTitle;
-
     public function invitation()
     {
-        $this->sTitle = t('Invite your friends');
-        $this->view->page_title = $this->sTitle;
+        $this->view->page_title = t('Invite your Friends');
         $this->view->meta_description = t('Invite your friends to join %site_name%');
-        $this->view->h1_title = $this->sTitle;
         $this->output();
     }
 }
