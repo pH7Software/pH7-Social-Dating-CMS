cf5d2de3f4494938da3cd839f9b94584943c513d
Pierre-Henry Soria <hizup.com@hotmail.com>
2015-12-31 10:27:21 +0000

    Prepare the next pH7CMS version, 1.2.8v

--- _install/data/sql/MySQL/pH7_Core.sql
+++ _install/data/sql/MySQL/pH7_Core.sql
@@ -930,7 +930,7 @@ CREATE TABLE IF NOT EXISTS pH7_Modules (
 
 INSERT INTO pH7_Modules (vendorName, moduleName, version, active) VALUES
 /* Gives the current version of the SQL schema of pH7CMS (this helps to update and shows whether it is necessary to update the database as well) */
-('pH7CMS', 'SQL System Schema', '1.2.1', 1);
+('pH7CMS', 'SQL System Schema', '1.2.2', 1);
 
 
 CREATE TABLE IF NOT EXISTS pH7_Report (
--- _protected/framework/Security/Version.class.php
+++ _protected/framework/Security/Version.class.php
@@ -22,10 +22,10 @@ final class Version
 
     /***** Framework Kernel *****/
     /** "Xmas" Version is a really "special" one as it will be released on the 24th with some unexpected features!!! Happy Christmas guys! **/
-    const KERNEL_VERSION_NAME = 'Xmas'; // 1.0 and 1.1 branches were "pOH", 1.2 branch is now "pOW" and the new one should be "p[H]"
-    const KERNEL_VERSION = '1.2.7';
+    const KERNEL_VERSION_NAME = 'pOW'; // 1.0 and 1.1 branches were "pOH", 1.2 branch is now "pOW" and the new one should be "p[H]"
+    const KERNEL_VERSION = '1.2.8';
     const KERNEL_BUILD = '1';
-    const KERNEL_RELASE_DATE = '2015-12-24';
+    const KERNEL_RELASE_DATE = '2015-01-05';
 
     /***** Framework Server *****/
     const KERNEL_TECHNOLOGY_NAME = 'pH7T/1.0.1'; // Ph7 Technology
