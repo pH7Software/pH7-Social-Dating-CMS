4dc9cc5d650695e1882702b09b9761fd03c6128d
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-02-06 12:27:50 +0000

    Code Refactoring + Code Improvements

--- _protected/app/system/modules/newsletter/inc/class/Newsletter.php
+++ _protected/app/system/modules/newsletter/inc/class/Newsletter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @author         Pierre-Henry Soria <ph7software@gmail.com>
+ * @author         Pierre-Henry Soria <hello@ph7cms.com>
  * @copyright      (c) 2012-2016, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package        PH7 / App / System / Module / Newsletter / Inc / Class
@@ -43,17 +43,7 @@ class Newsletter extends Core
         $oMail = new Mail;
         foreach ($oSubscribers as $oSubscriber)
         {
-            $this->view->content = $this->httpRequest->post('body', Http::NO_CLEAN);
-
-            $sMsgHtml = $this->view->parseMail(PH7_PATH_SYS . 'global/' . PH7_VIEWS . PH7_TPL_NAME . '/mail/sys/mod/newsletter/msg.tpl', $oSubscriber->email);
-
-            $aInfo = [
-                'subject' => $this->httpRequest->post('subject'),
-                'to' => $oSubscriber->email,
-                'to_name' => $oSubscriber->firstName
-            ];
-
-            if (!$iRes = $oMail->send($aInfo, $sMsgHtml)) break;
+            if (!$this->sendMail($oSubscriber, $oMail)) break;
 
             // Do not send all emails at the same time to avoid overloading the mail server.
             if (++self::$_iTotalSent > 250) sleep(10);
@@ -61,6 +51,28 @@ class Newsletter extends Core
         unset($oMail, $oSubscribers);
 
         return ['status' => $iRes, 'nb_mail_sent' => self::$_iTotalSent];
+    }
+
+    /**
+     * Send the newsletter to the subscribers.
+     *
+     * @param object $oSubscriber Subscriber data fron the DB.
+     * @param object \PH7\Framework\Mail\Mail $oMail
+     * @return integer Number of recipients who were accepted for delivery.
+     */
+    protected function sendMail($oSubscriber, Mail $oMail)
+    {
+        $this->view->content = $this->httpRequest->post('body', Http::NO_CLEAN);
+
+        $sMsgHtml = $this->view->parseMail(PH7_PATH_SYS . 'global/' . PH7_VIEWS . PH7_TPL_NAME . '/mail/sys/mod/newsletter/msg.tpl', $oSubscriber->email);
+
+        $aInfo = [
+            'subject' => $this->httpRequest->post('subject'),
+            'to' => $oSubscriber->email,
+            'to_name' => $oSubscriber->firstName
+        ];
+
+        return $oMail->send($aInfo, $sMsgHtml);
     }
 
 }
