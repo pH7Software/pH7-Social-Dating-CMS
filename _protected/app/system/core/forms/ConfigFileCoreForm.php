edcff953972e6faf9bc9a494590bfc0d38886828
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-02-06 18:35:39 +0000

    Typo

--- _protected/app/system/core/forms/ConfigFileCoreForm.php
+++ _protected/app/system/core/forms/ConfigFileCoreForm.php
@@ -51,7 +51,7 @@ class ConfigFileCoreForm
             elseif (false !== strpos($sKey, 'email'))
                 $oForm->addElement(new \PFBC\Element\Email($sLabel, 'config[' . $sKey . ']', array('value' => $sVal)));
             elseif (false !== strpos($sKey, 'environment'))
-                $oForm->addElement(new \PFBC\Element\Select($sLabel, 'config[' . $sKey . ']', array('production' => t('Production'), 'development' => t('Development')), array('description' => t('If you see "Internal Server Error" message, please set to "development" mode in order to see the details of the error. If your site in on production server (and visible by everyone) please set the production mode.'), 'value' => $sVal)));
+                $oForm->addElement(new \PFBC\Element\Select($sLabel, 'config[' . $sKey . ']', array('production' => t('Production'), 'development' => t('Development')), array('description' => t('If you see "Internal Server Error" message in your site, please set to "development" mode in order to see the details of the error. If your site is on production server (and visible by everyone) please set to the production mode for security reason.'), 'value' => $sVal)));
             elseif (ctype_digit($sVal))
                 $oForm->addElement(new \PFBC\Element\Number($sLabel, 'config[' . $sKey . ']', array('step' => 'any', 'value' => $sVal)));
             else
