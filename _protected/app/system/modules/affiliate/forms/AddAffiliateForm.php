ec8787382dae9471e941b8e607af411751523ba1
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-02-07 20:34:13 +0000

    Bugfix in Add User form + text improvements

--- _protected/app/system/modules/affiliate/forms/AddAffiliateForm.php
+++ _protected/app/system/modules/affiliate/forms/AddAffiliateForm.php
@@ -31,14 +31,14 @@ class AddAffiliateForm
         $oForm->addElement(new \PFBC\Element\Password(t('Password:'), 'password', array('required'=>1)));
         $oForm->addElement(new \PFBC\Element\Textbox(t('First Name:'), 'first_name', array('required'=>1, 'validation'=>new \PFBC\Validation\Name)));
         $oForm->addElement(new \PFBC\Element\Textbox(t('Last Name:'), 'last_name', array('required'=>1, 'validation'=>new \PFBC\Validation\Name)));
-        $oForm->addElement(new \PFBC\Element\Textbox(t('Middle Name:'), 'middle_name', array('validation'=>new \PFBC\Validation\Str(2,20))));
+        $oForm->addElement(new \PFBC\Element\Textbox(t('Middle Name:'), 'middle_name', array('validation'=>new \PFBC\Validation\Name)));
         $oForm->addElement(new \PFBC\Element\Radio(t('Sex:'), 'sex', array('female'=>t('Female'), 'male'=>t('Male'), 'couple'=>t('Couple')), array('value'=>'female', 'required'=>1)));
         $oForm->addElement(new \PFBC\Element\Date(t('Date of birth:'), 'birth_date', array('placeholder'=>t('Month/Day/Year'), 'title'=>t('Please specify the birth date using the calendar or with this format: Month/Day/Year.'), 'required'=>1, 'validation'=>new \PFBC\Validation\BirthDate)));
         $oForm->addElement(new \PFBC\Element\Country(t('Country:'), 'country', array('id'=>'str_country', 'value'=>Geo::getCountryCode(), 'required'=>1)));
         $oForm->addElement(new \PFBC\Element\Textbox(t('City:'), 'city', array('id'=>'str_city', 'validation'=>new \PFBC\Validation\Str(2,150), 'required'=>1)));
         $oForm->addElement(new \PFBC\Element\Textbox(t('State:'), 'state', array('id'=>'str_state', 'validation'=>new \PFBC\Validation\Str(2,150), 'required'=>1)));
         $oForm->addElement(new \PFBC\Element\Textbox(t('ZIP/Postal Code:'), 'zip_code', array('id'=>'str_zip_code', 'validation'=>new \PFBC\Validation\Str(2,15), 'required'=>1)));
-        $oForm->addElement(new \PFBC\Element\Phone(t('Phone Number:'), 'phone', array('description'=>t('Enter full phone number with area code.'), 'required'=>1)));
+        $oForm->addElement(new \PFBC\Element\Phone(t('Phone Number:'), 'phone', array('description'=>t('Enter full phone number with area code (e.g., +44768374890).'), 'required'=>1)));
         $oForm->addElement(new \PFBC\Element\CKEditor(t('Description:'), 'description', array('description'=>t("Description of the affiliate's site(s)."), 'validation'=>new \PFBC\Validation\Str(10,2000), 'required' =>1)));
         $oForm->addElement(new \PFBC\Element\Url(t('Website:'), 'website', array('description'=>t('Main website where the affiliate is the owner.'), 'required'=>1)));
         $oForm->addElement(new \PFBC\Element\Email(t('Bank Account:'), 'bank_account', array('description'=>t('Bank Account (PayPal Email Address).'), 'validation'=>new \PFBC\Validation\BankAccount, 'required'=>1)));
