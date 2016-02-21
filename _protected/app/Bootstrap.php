bc6690f96c4d2a66e94c1512d2ed9baa2131ad47
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-02-21 23:53:50 +0000

    Add more details in some useful texts

--- _protected/app/Bootstrap.php
+++ _protected/app/Bootstrap.php
@@ -62,7 +62,9 @@ try
     if (!Server::isRewriteMod())
     {
         $sMsg = '<p class="warning"><a href="' . Framework\Core\Kernel::SOFTWARE_WEBSITE . '">pH7CMS</a> requires Apache "mod_rewrite".</p>
-        <p>Please install it so that pH7CMS can works.<br /> Click <a href="http://ph7cms.com/doc/en/how-to-install-rewrite-module" target="_blank">here</a> if you want to get more information on how to install the rewrite module.<br /><br />
+        <p>It seems you don\'t have it or not installed correctly.<br />
+        Firstly, please be sure the ".htaccess" file has been uploaded to the root directory where pH7CMS is installed. If not, use your FTP client (such as Filezilla) and upload it again from pH7CMS unziped package and try again.<br />
+        Secondly, please install mod_rewrite.<br /> Click <a href="http://ph7cms.com/doc/en/how-to-install-rewrite-module" target="_blank">here</a> if you want to get more information on how to install the rewrite module.<br /><br />
         After doing this, please <a href="' . PH7_URL_ROOT . '">retry</a>.</p>';
 
         echo html_body("Apache's mod_rewrite is required", $sMsg);
