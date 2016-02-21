dd3ce9e0d70c3efe6b0eab1e139c5537fe24afb7
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-02-21 11:01:46 +0000

    Bumped version

--- _protected/framework/Security/Version.class.php
+++ _protected/framework/Security/Version.class.php
@@ -21,9 +21,9 @@ final class Version
 
     /***** Framework Kernel *****/
     const KERNEL_VERSION_NAME = 'p[H]'; // 1.0 and 1.1 branches were "pOH", 1.2 branch was "pOW" and 1.3 is now "p[H]"
-    const KERNEL_VERSION = '1.3.1';
+    const KERNEL_VERSION = '1.3.4';
     const KERNEL_BUILD = '1';
-    const KERNEL_RELASE_DATE = '2016-02-19';
+    const KERNEL_RELASE_DATE = '2016-02-22';
 
     /***** Framework Server *****/
     const KERNEL_TECHNOLOGY_NAME = 'pH7T/1.0.1'; // Ph7 Technology
