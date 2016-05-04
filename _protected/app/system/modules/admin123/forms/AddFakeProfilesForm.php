3e566e3f3dba9d6772ee7945713c3c3653b0654e
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-05-04 20:25:02 +0100

    Update description with more details for profile generator

--- _protected/app/system/modules/admin123/forms/AddFakeProfilesForm.php
+++ _protected/app/system/modules/admin123/forms/AddFakeProfilesForm.php
@@ -26,7 +26,7 @@ class AddFakeProfilesForm
         $oForm->configure(array('action' => '' ));
         $oForm->addElement(new \PFBC\Element\Hidden('submit_add_fake_profiles', 'form_add_fake_profiles'));
         $oForm->addElement(new \PFBC\Element\Token('fake_profiles'));
-        $oForm->addElement(new \PFBC\Element\Select(t('Number:'), 'num', array(1, 5, 10, 15, 25), array('description' => t('Number of fake profiles to add in the same time.'), 'required'=>1)));
+        $oForm->addElement(new \PFBC\Element\Select(t('Number:'), 'num', array(1, 5, 10, 15, 25), array('description' => t('Number of fake profiles to add in the same time. Choosing 15 or 25 profiles might takes a few minutes.'), 'required'=>1)));
         $oForm->addElement(new \PFBC\Element\Button(t('Add Fake Profiles!')));
         $oForm->render();
     }
