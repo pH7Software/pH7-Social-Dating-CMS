d35728ef8d24793f0a7b25fa44793c8e1b58f2e7
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-05-08 18:16:53 +0100

    Change release date

--- _protected/framework/Security/Version.class.php
+++ _protected/framework/Security/Version.class.php
@@ -23,7 +23,7 @@ final class Version
     const KERNEL_VERSION_NAME = 'p[H]'; // 1.0 and 1.1 branches were "pOH", 1.2 branch was "pOW" and 1.3 is now "p[H]"
     const KERNEL_VERSION = '1.3.9';
     const KERNEL_BUILD = '1';
-    const KERNEL_RELASE_DATE = '2016-06-06';
+    const KERNEL_RELASE_DATE = '2016-06-09';
 
     /***** Framework Server *****/
     const KERNEL_TECHNOLOGY_NAME = 'pH7T/1.0.1'; // Ph7 Technology
