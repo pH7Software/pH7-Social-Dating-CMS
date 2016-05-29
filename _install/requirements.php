7bc64cd096d836005b27842e0f8f63b925bb4326
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-05-29 16:40:20 +0100

    Check if also  EXIF extension is is loaded

--- _install/requirements.php
+++ _install/requirements.php
@@ -41,7 +41,7 @@ if (!function_exists('curl_init'))
 if (!extension_loaded('mbstring'))
     $aErrors[] = 'Please install "mbstring" PHP extension.';
 
-if (!function_exists('exif_imagetype'))
+if (!extension_loaded('exif') || !function_exists('exif_imagetype'))
     $aErrors[] = 'Please install "exif" PHP extension.';
 
 $iErrors = (!empty($aErrors)) ? count($aErrors) : 0;
