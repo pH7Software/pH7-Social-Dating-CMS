46eb1c04810a8fb3e2b6590a29cb991aba2f4421
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-06-06 22:41:07 +0100

    Change OPT Tolerance

--- _protected/app/system/modules/two-factor-auth/forms/processing/VerificationCodeFormProcess.php
+++ _protected/app/system/modules/two-factor-auth/forms/processing/VerificationCodeFormProcess.php
@@ -15,6 +15,15 @@ PH7\Framework\Mvc\Router\Uri;
 
 class VerificationCodeFormProcess extends Form
 {
+     /**
+      * Every OPT is valid for 30 sec.
+      * If somebody provides OTP at 29th sec, by the time it reaches the server OTP is expired.
+      * So we can give OTP_TOLERANCE=1, it will check current  & previous OTP.
+      * OTP_TOLERANCE=2, verifies current and last two OTPS
+      * - Text from: http://hayageek.com/two-factor-authentication-with-google-authenticator-php/
+      */
+    const OTP_TOLERANCE = 1;
+
     public function __construct($sMod)
     {
         parent::__construct();
@@ -25,7 +34,7 @@ class VerificationCodeFormProcess extends Form
         $sSecret = (new TwoFactorAuthModel($sMod))->getSecret($iProfileId);
         $sCode = $this->httpRequest->post('verification_code');
 
-        $bCheck = $oAuthenticator->verifyCode($sSecret, $sCode, 0);
+        $bCheck = $oAuthenticator->verifyCode($sSecret, $sCode, self::OTP_TOLERANCE);
 
         if ($bCheck)
         {
