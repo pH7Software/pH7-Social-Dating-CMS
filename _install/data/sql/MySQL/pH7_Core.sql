e99dd60100b396200e139ca5f6737c782cd25343
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-07-01 23:33:45 +0100

    Update the SQL Schema
    Since the update to make it compatible with MySQL Strict Mode

--- _install/data/sql/MySQL/pH7_Core.sql
+++ _install/data/sql/MySQL/pH7_Core.sql
@@ -956,7 +956,7 @@ CREATE TABLE IF NOT EXISTS pH7_Modules (
 
 INSERT INTO pH7_Modules (vendorName, moduleName, version, active) VALUES
 /* Gives the current version of the SQL schema of pH7CMS (this helps to update and shows whether it is necessary or not to update the database as well) */
-('pH7CMS', 'SQL System Schema', '1.2.7', 1);
+('pH7CMS', 'SQL System Schema', '1.2.8', 1);
 
 
 CREATE TABLE IF NOT EXISTS pH7_Report (
