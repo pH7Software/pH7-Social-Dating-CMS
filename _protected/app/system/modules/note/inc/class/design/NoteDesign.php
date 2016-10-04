f0f1330a0fad3496b1933273c835d57426131999
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-10-04 23:15:06 +0100

    Fix thumb image on small devices

--- _protected/app/system/modules/note/inc/class/design/NoteDesign.php
+++ _protected/app/system/modules/note/inc/class/design/NoteDesign.php
@@ -16,13 +16,11 @@ class NoteDesign extends WriteDesignCoreModel
      */
     public static function thumb($oNoteModel)
     {
-        echo '<div class="pic thumb">';
-
+        echo '<p>';
         if (!empty($oNoteModel->thumb))
-            echo '<a href="', Uri::get('note','main','read', $oNoteModel->username . ',' . $oNoteModel->postId), '"><img src="', PH7_URL_DATA_SYS_MOD, 'note/', PH7_IMG, $oNoteModel->username, PH7_SH, $oNoteModel->thumb, '" alt="', $oNoteModel->pageTitle, '" title="', $oNoteModel->pageTitle, '" /></a>';
+            echo '<a href="', Uri::get('note','main','read', $oNoteModel->username . ',' . $oNoteModel->postId), '" class="pic thumb" data-load="ajax"><img src="', PH7_URL_DATA_SYS_MOD, 'note/', PH7_IMG, $oNoteModel->username, PH7_SH, $oNoteModel->thumb, '" alt="', $oNoteModel->pageTitle, '" title="', $oNoteModel->pageTitle, '" /></a>';
         else
             (new AvatarDesignCore)->get($oNoteModel->username, $oNoteModel->firstName, $oNoteModel->sex, 100);
-
-        echo '</div>';
+        echo '</p>';
     }
 }
