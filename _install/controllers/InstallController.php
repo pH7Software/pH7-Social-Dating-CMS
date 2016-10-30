37914d26ac5bbb00f6de4b520918ca7f9aab85e6
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-10-30 18:49:39 +0000

    Remove version in Controller since not necessary

--- _install/controllers/InstallController.php
+++ _install/controllers/InstallController.php
@@ -6,7 +6,6 @@
  * @copyright        (c) 2012-2016, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Install / Controller
- * @version          3.1
  */
 
 namespace PH7;
