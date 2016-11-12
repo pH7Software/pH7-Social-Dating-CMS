98c41ac6a351ae0aed46b1febfc36eef92e1f4d7
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-11-12 02:42:12 +0000

    "Oops" useless here

--- _protected/framework/Geo/Map/Api.class.php
+++ _protected/framework/Geo/Map/Api.class.php
@@ -618,7 +618,7 @@ class Api
         $this->content .= '<script>' . "\n";
 
         if (empty($this->key) || strlen($this->key) <= 10) {
-            $this->content .= 'alert("' . t('Oops! You need to get a Google Maps API key to get it working. Please go to your pH7CMS Admin Panel -> Settings -> General -> API -> Google Maps API Key') . '");' . "\n";
+            $this->content .= 'alert("' . t('You need to get a Google Maps API key to get it working. Please go to your pH7CMS Admin Panel -> Settings -> General -> API -> Google Maps API Key') . '");' . "\n";
         }
 
         $this->content .= 'function addLoadEvent(func) { ' . "\n";
