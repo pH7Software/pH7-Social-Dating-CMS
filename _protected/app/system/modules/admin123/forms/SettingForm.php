1a6ac1ee7566ed7f4423d83e960424599945ab59
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-12-10 23:45:56 +0000

    Change description for Default Module field

--- _protected/app/system/modules/admin123/forms/SettingForm.php
+++ _protected/app/system/modules/admin123/forms/SettingForm.php
@@ -41,7 +41,7 @@ class SettingForm
 
         $oForm->addElement(new \PFBC\Element\Select(t('Theme by default:'), 'default_template', self::getTpls($oFile), array('value' => DbConfig::getSetting('defaultTemplate'), 'required' => 1)));
 
-        $oForm->addElement(new \PFBC\Element\Select(t('Module by default:'), 'default_sys_module', self::getDefMods(), array('description' => t('The default module running by default on the index page. Recommended to keep the "user" module.'), 'value' => DbConfig::getSetting('defaultSysModule'), 'required' => 1)));
+        $oForm->addElement(new \PFBC\Element\Select(t('Module by default:'), 'default_sys_module', self::getDefMods(), array('description' => t('The default module is the one running by default on the homepage (recommended to keep the "user" module).'), 'value' => DbConfig::getSetting('defaultSysModule'), 'required' => 1)));
 
         $oForm->addElement(new \PFBC\Element\Select(t('Language by default:'), 'default_language', self::getLangs($oFile), array('value' => DbConfig::getSetting('defaultLanguage'), 'validation' => new \PFBC\Validation\Str(5,5), 'required' => 1)));
 
