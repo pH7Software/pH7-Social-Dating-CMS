3ec49cb6632caeddc6a3e8ef132d33e9975200f3
Pierre-Henry Soria <hizup.com@hotmail.com>
2016-12-11 11:46:56 +0000

    Never cache html_body page

--- _protected/app/includes/helpers/misc.php
+++ _protected/app/includes/helpers/misc.php
@@ -15,6 +15,8 @@ use PH7\Framework\Registry\Registry;
 
 /**
  * Display a basic HTML body page.
+ * Since it will display a dynamic page,
+ * it will also send headers to not cache the page
  *
  * @param string $sTitle Title of the page.
  * @param string $sMsg Message to display to the page.
@@ -22,6 +24,10 @@ use PH7\Framework\Registry\Registry;
  */
 function html_body($sTitle, $sMsg)
 {
+    // Send headers to not cache the page
+    header('Cache-Control: no-store, no-cache, must-revalidate');
+    header('Expires: Thu, 01 Jan 1970 00:00:00 GMT');
+
     return '<!DOCTYPE html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,maximum-scale=1"><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><title>' . $sTitle . '</title><meta name="author" content="pH7CMS, Pierre-Henry Soria"><meta name="copyright" content="(c) 2012-2016, Pierre-Henry Soria. All Rights Reserved"><meta name="creator" content="pH7CMS, Pierre-Henry Soria"><meta name="designer" content="pH7CMS, Pierre-Henry Soria"><meta name="generator" content="pH7CMS"><style>body{background:#EFEFEF;color:#555;font:normal 12px Arial,Helvetica,sans-serif;margin:0;padding:0}.center{margin-left:auto;margin-right:auto;text-align:center;width:80%}.error,.warning{font-weight:bold;font-size:13px;color:red}.warning{text-transform:uppercase}.italic{font-style:italic}.underline{text-decoration:underline}a{color:#08c;outline-style:none;cursor:pointer}a:link,a:visited{text-decoration:none}a:hover,a:active{color:#F24C9E;text-decoration:underline}</style></head><body><div class="center">' . $sMsg . '</div></body></html>';
 }
 
