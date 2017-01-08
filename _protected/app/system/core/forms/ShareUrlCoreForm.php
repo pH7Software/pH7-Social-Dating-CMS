1dd4b1bc1913152e7f9c69ac0e4b04c1257b4934
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-01-08 11:52:35 +0000

    Add new param for enable/disable Share URL label
    That adds the possibility to disable the default label "Share URL" text on top on the Sharing URL text field

--- _protected/app/system/core/forms/ShareUrlCoreForm.php
+++ _protected/app/system/core/forms/ShareUrlCoreForm.php
@@ -11,21 +11,21 @@ use PH7\Framework\Mvc\Request\Http;
 
 class ShareUrlCoreForm
 {
-
     /**
-     * @param $sUrl The URL to share. If you enter nothing, it will be the current URL. Default: NULL
-     * @param integer $iWidth Width of the form in pixel. Default: NULL
+     * @param $sUrl The URL to share. If you enter nothing, it will be the current URL.
+     * @param integer $iWidth Width of the form in pixel.
+     * @param boolean $bIsShareUrlLabel If TURE, it shows 'Share URL:' label beside the field.
      * @return void
      */
-    public static function display($sUrl = null, $iWidth = null)
+    public static function display($sUrl = null, $iWidth = null, $bIsShareUrlLabel = true)
     {
-        $sUrl = (!empty($sUrl)) ? $sUrl : (new Http)->currentUrl();
+        $sUrl = !empty($sUrl) ? $sUrl : (new Http)->currentUrl();
+        $sLabel = $bIsShareUrlLabel ? t('Share URL:') : '';
 
         $oForm = new \PFBC\Form('form_share_url', $iWidth);
         $oForm->configure(array('class' => 'center'));
-        $oForm->addElement(new \PFBC\Element\Url(t('Share URL:'), 'share', array('value'=>$sUrl, 'readonly'=>'readonly', 'onclick'=>'this.select()')));
+        $oForm->addElement(new \PFBC\Element\Url($sLabel, 'share', array('value'=>$sUrl, 'readonly'=>'readonly', 'onclick'=>'this.select()')));
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<br />'));
         $oForm->render();
     }
-
 }
