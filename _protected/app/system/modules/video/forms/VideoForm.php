c306147f8cb3106496502331f6e7ca72a433569f
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-02-02 14:55:52 +0000

    Multiple video uploader is not possible, fix field name (to singular only)

--- _protected/app/system/modules/video/forms/VideoForm.php
+++ _protected/app/system/modules/video/forms/VideoForm.php
@@ -45,7 +45,7 @@ class VideoForm
         unset($aAlbumName);
 
         $oForm->addElement(new \PFBC\Element\Hidden('album_title', @$iId->name)); // Bad title! Thanks for finding a solution and commit it on http://github.com/pH7Software/pH7-Social-Dating-CMS or send it by email
-        $oForm->addElement(new \PFBC\Element\Textbox(t('Name of your video(s):'), 'title', array('pattern' => $sTitlePattern, 'validation' => new \PFBC\Validation\RegExp($sTitlePattern))));
+        $oForm->addElement(new \PFBC\Element\Textbox(t('Name of your video:'), 'title', array('pattern' => $sTitlePattern, 'validation' => new \PFBC\Validation\RegExp($sTitlePattern))));
         $oForm->addElement(new \PFBC\Element\Select('Video type:', 'type', array(t('Choose...'), 'embed' => t('Embed Code'), 'regular' => t('Regular')), array('id' => 'video-type', 'required'=>1)));
 
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<div class="hidden" id="regular">'));
