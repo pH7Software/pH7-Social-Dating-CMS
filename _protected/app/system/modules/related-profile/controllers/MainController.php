23e15e3a83ea98f3eda98c66771ab3f902ac10d2
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-02-25 14:55:56 +0000

    Bug fixes
    Summary:
    * Get MembersInfo data
    * Add <meta name="robots" content="noindex" since the page shouldnt be indexed by search engines
    * Update copyright year
    * Fix undefined array variable
    * Add phpfoc comment

--- _protected/app/system/modules/related-profile/controllers/MainController.php
+++ _protected/app/system/modules/related-profile/controllers/MainController.php
@@ -1,12 +1,14 @@
 <?php
 /**
  * @author         Pierre-Henry Soria <hello@ph7cms.com>
- * @copyright      (c) 2016-2017, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright      (c) 2017, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package        PH7 / App / System / Module / Related Profile / Controller
  */
 namespace PH7;
 
+
+use PH7\Framework\Layout\Html\Meta;
 use stdClass;
 
 class MainController extends Controller
@@ -24,9 +26,12 @@ class MainController extends Controller
 
     public function index($iProfileId = null)
     {
+        $this->view->header = Meta::NOINDEX;
+
         if (!empty($iProfileId)) {
             $oProfileData = $this->oUserModel->readProfile($iProfileId);
-            $oRelatedProfiles = $this->relatedProdiles($oProfileData);
+            $oProfileFields = $this->oUserModel->getInfoFields($iProfileId);
+            $oRelatedProfiles = $this->relatedProdiles($oProfileData, $oProfileFields);
 
             if (!empty($oRelatedProfiles)) {
                 $this->view->avatarDesign = new AvatarDesignCore; // Avatar Design Class
@@ -36,6 +41,7 @@ class MainController extends Controller
             }
         }
         $this->notFound();
+        $this->output();
     }
 
     protected function notFound()
@@ -44,16 +50,18 @@ class MainController extends Controller
     }
 
     /**
+     * Get related profile data.
+     *
      * @param object $oProfile Profile User Data.
      * @return stdClass Profile data.
      */
-    private function relatedProdiles(stdClass $oProfile)
+    private function relatedProdiles(stdClass $oProfile, stdClass $oProfileFields)
     {
         $aParams = [
-            $aParams[SearchQueryCore::AGE] => $oProfile->birthDate,
-            SearchQueryCore::COUNTRY => $oProfile->country,
-            SearchQueryCore::CITY => $oProfile->city,
-            SearchQueryCore::MATCH_SEX => $oProfile->match_sex
+            SearchQueryCore::AGE => $oProfile->birthDate,
+            SearchQueryCore::MATCH_SEX => $oProfile->matchSex,
+            SearchQueryCore::COUNTRY => $oProfileFields->country,
+            SearchQueryCore::CITY => $oProfileFields->city
         ];
 
         return $this->oUserModel->search($aParams, false, 0, self::MAX_PROFILE);
