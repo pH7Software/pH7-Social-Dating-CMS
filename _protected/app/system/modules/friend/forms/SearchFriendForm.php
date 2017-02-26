6d7efc1629de31868865467354b0f7c1f0a9746a
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-02-26 21:56:44 +0000

    Add Friend module, template + mail template

---
+++ _protected/app/system/modules/friend/forms/SearchFriendForm.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hello@ph7cms.com>
+ * @copyright      (c) 2012-2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package        PH7 / App / System / Module / Friend / Form
+ */
+namespace PH7;
+
+use PH7\Framework\Mvc\Router\Uri, PH7\Framework\Mvc\Request\Http;
+
+class SearchFriendForm
+{
+    public static function display()
+    {
+        $oHttpRequest = new Http;
+        $sUsername = $oHttpRequest->get('username');
+        $sAction = ($oHttpRequest->getExists('action')) ? 'mutual' : 'index';
+        unset($oHttpRequest);
+
+        $oForm = new \PFBC\Form('form_search');
+        $oForm->configure(array('action' => Uri::get('friend', 'main', $sAction, $sUsername) . PH7_SH, 'method' => 'get'));
+        $oForm->addElement(new \PFBC\Element\Search(t('Search a Friend of "%0%"', $sUsername), 'looking', array('description' => t('Enter the First/Last Name, Username, Email or ID of your friend.'))));
+        $oForm->addElement(new \PFBC\Element\Select(t('Browse By:'), 'order', array(SearchCoreModel::USERNAME => t('Username'), SearchCoreModel::FIRST_NAME => t('First Name'), SearchCoreModel::LAST_NAME => t('Last Name'), SearchCoreModel::EMAIL => t('Email'), SearchCoreModel::LATEST => t('Latest'), SearchCoreModel::LAST_ACTIVITY => t('Last Activity'), SearchCoreModel::VIEWS => t('Popular'), SearchCoreModel::RATING => t('Rated'))));
+        $oForm->addElement(new \PFBC\Element\Select(t('Direction:'), 'sort', array(SearchCoreModel::ASC => t('Ascending'), SearchCoreModel::DESC => t('Descending'))));
+        $oForm->addElement(new \PFBC\Element\Button(t('Search'),'submit', array('icon' => 'search')));
+        $oForm->render();
+    }
+}
---
+++ _protected/app/system/modules/friend/models/FriendModel.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hello@ph7cms.com>
+ * @copyright      (c) 2012-2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package        PH7/ App / System / Module / Friend / Model
+ */
+namespace PH7;
+
+use PH7\Framework\Mvc\Model\Engine\Db;
+
+class FriendModel extends FriendCoreModel
+{
+    private $_sStatus;
+
+    /**
+     * Check exists in the friends list
+     *
+     * @param integer $iProfileId = user Id
+     * @param integer $iFriendId friend id
+     * @param mixed (integer or string) $mPending 'all' = select the friends that are approved and pending, 1 = approved or 0 = pending friend requests. Default value is 'all'
+     * @param boolean
+     */
+    public function inList($iProfileId, $iFriendId, $mPending = 'all')
+    {
+        $iProfileId = (int) $iProfileId;
+        $iFriendId = (int) $iFriendId;
+
+        $sSqlPending = ($mPending !== 'all') ? 'AND pending = :pending' : '';
+
+        $rStmt = Db::getInstance()->prepare('SELECT * FROM' . Db::prefix('MembersFriends') .
+          'WHERE profileId = :profileId AND friendId = :friendId ' . $sSqlPending . ' LIMIT 1');
+
+        $rStmt->bindValue(':profileId', $iProfileId, \PDO::PARAM_INT);
+        $rStmt->bindValue(':friendId', $iFriendId, \PDO::PARAM_INT);
+        if ($mPending !== 'all') {
+            $rStmt->bindValue(':pending', $mPending, \PDO::PARAM_INT);
+        }
+        $rStmt->execute();
+        return $rStmt->fetchColumn() > 0;
+    }
+
+    /**
+     * Add a friend
+     *
+     * @param integer $iProfileId = user Id
+     * @param integer $iFriendId friend id
+     * @param string $sRequestDate Date of the Request Friend.
+     * @param integer $iPending  1 = approved or 0 = pending friend requests. Default value is 1
+     * @return string Status in word: 'error', 'id_does_not_exist', 'friend_exists' or 'success'
+     */
+    public function add($iProfileId, $iFriendId, $sRequestDate, $iPending = 1)
+    {
+        $iProfileId = (int) $iProfileId;
+        $iFriendId = (int) $iFriendId;
+
+        // Check if the two existing ID
+        $oExistsModel = new ExistsCoreModel;
+
+        if ($oExistsModel->id($iProfileId, 'Members') && $oExistsModel->id($iFriendId, 'Members')) {
+            if (!$this->inList($iProfileId, $iFriendId)) {
+                $rStmt = Db::getInstance()->prepare('INSERT INTO' . Db::prefix('MembersFriends') .
+                  '(profileId, friendId, pending, requestDate) VALUES (:profileId, :friendId, :pending, :requestDate)');
+
+                $rStmt->bindValue(':profileId', $iProfileId, \PDO::PARAM_INT);
+                $rStmt->bindValue(':friendId', $iFriendId, \PDO::PARAM_INT);
+                $rStmt->bindValue(':pending', $iPending, \PDO::PARAM_INT);
+                $rStmt->bindValue(':requestDate', $sRequestDate, \PDO::PARAM_STR);
+                $oRow = $rStmt->execute();
+                Db::free($rStmt);
+                if (!$oRow) {
+                    $this->_sStatus = 'error';
+                } else {
+                    $this->_sStatus = 'success';
+                }
+            } else {
+                $this->_sStatus = 'friend_exists';
+            }
+        } else {
+            $this->_sStatus = 'id_does_not_exist';
+        }
+
+        unset($oExistsModel); // Destruction of the object
+
+        return $this->_sStatus;
+    }
+
+    /**
+     * Approve friends
+     *
+     * @param integer $iProfileId
+     * @param integer $iFriendId
+     * @return boolean
+     */
+    public function approval($iProfileId, $iFriendId)
+    {
+        $iProfileId = (int) $iProfileId;
+        $iFriendId = (int) $iFriendId;
+
+        $rStmt = Db::getInstance()->prepare('UPDATE'.Db::prefix('MembersFriends') .
+            'SET pending = 0 WHERE profileId = :friendId AND friendId = :profileId');
+        $rStmt->bindValue(':profileId', $iProfileId, \PDO::PARAM_INT);
+        $rStmt->bindValue(':friendId', $iFriendId, \PDO::PARAM_INT);
+        return $rStmt->execute();
+    }
+
+    /**
+     * Delete a friend
+     *
+     * @param integer $iProfileId
+     * @param integer $iFriendId
+     * @return boolean
+     */
+    public function delete($iProfileId, $iFriendId)
+    {
+        $iProfileId = (int) $iProfileId;
+        $iFriendId = (int) $iFriendId;
+
+        $rStmt = Db::getInstance()->prepare('DELETE FROM' . Db::prefix('MembersFriends') .
+            'WHERE (profileId = :profileId AND friendId = :friendId) OR (friendId = :profileId AND profileId = :friendId)');
+        $rStmt->bindValue(':profileId', $iProfileId, \PDO::PARAM_INT);
+        $rStmt->bindValue(':friendId', $iFriendId, \PDO::PARAM_INT);
+        return $rStmt->execute();
+    }
+}
