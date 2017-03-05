ee9e86341012e5be58155dfe479a1b3b336a9d31
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-03-05 08:45:41 +0000

    Cleanup & Import class names

--- _protected/app/system/modules/user/Bootstrap.php
+++ _protected/app/system/modules/user/Bootstrap.php
@@ -6,21 +6,24 @@
  * @package        PH7 / App / System / Module / User
  */
 namespace PH7;
+
 defined('PH7') or die('Restricted access');
 
+use PH7\Framework\Registry\Registry;
+use PH7\Framework\Session\Session;
+use PH7\Framework\Cookie\Cookie;
+use PH7\Framework\Security\Security;
+use PH7\Framework\Mvc\Model\Security as SecurityModel;
+
 // Automatic connection
-if (!UserCore::auth() && Framework\Registry\Registry::getInstance()->action != 'soon')
-{
-    $oCookie = new Framework\Cookie\Cookie;
-    if ($oCookie->exists(array('member_remember', 'member_id')))
-    {
-        if ((new ExistsCoreModel)->id($oCookie->get('member_id')))
-        {
+if (!UserCore::auth() && Registry::getInstance()->action !== 'soon') {
+    $oCookie = new Cookie;
+    if ($oCookie->exists(array('member_remember', 'member_id'))) {
+        if ((new ExistsCoreModel)->id($oCookie->get('member_id'))) {
             $oUserModel = new UserCoreModel;
             $oUser = $oUserModel->readProfile($oCookie->get('member_id'));
-            if ($oCookie->get('member_remember') === Framework\Security\Security::hashCookie($oUser->password))
-            {
-                (new UserCore)->setAuth($oUser, $oUserModel, new Framework\Session\Session, new Framework\Mvc\Model\Security);
+            if ($oCookie->get('member_remember') === Security::hashCookie($oUser->password)) {
+                (new UserCore)->setAuth($oUser, $oUserModel, new Session, new SecurityModel);
             }
         }
     }
