52775d2c059bc2718d0e91c7d209e117c5963bd1
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-03-07 19:36:02 +0000

    Change URL to handle https if website is running with it

--- static/js/cookie_consent/eu-states.js
+++ static/js/cookie_consent/eu-states.js
@@ -3,4 +3,4 @@
  * Copyright:     (c) 2015-2017, Pierre-Henry Soria. All Rights Reserved.
  * License:       GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  */
-﻿document.write('<script src="//static.hizup.com/js/cookieconsent/eustates.js"></script>');
+document.write('<script src="//ph7cms.com/addons/cookieconsent/eustates.js"></script>');
