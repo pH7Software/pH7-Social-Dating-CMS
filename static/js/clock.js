335c8fdab7551ce995db6b2d194d7acaa0a1b7db
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-03-24 21:58:06 +0000

    Remove clock ajax feature
    I won't use it anymore and really useless and old-school style

--- static/js/clock.js
+++
@@ -1,7 +0,0 @@
-/*
- * Author:        Pierre-Henry Soria <ph7software@gmail.com>
- * Copyright:     (c) 2012-2017, Pierre-Henry Soria. All Rights Reserved.
- * License:       GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
- */
-
-function getTime(){$.get(pH7Url.base+"asset/ajax/clock",function(a){$("#clock").html(a)});setTimeout("getTime()",1E3)}getTime();
