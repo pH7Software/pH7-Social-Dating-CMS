353a10bbd46797af9d90a1b1b90270963017a81d
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-03-28 22:48:01 +0100

    Add newline before "namespace"

--- _protected/app/system/modules/payment/inc/class/design/PaymentDesign.php
+++ _protected/app/system/modules/payment/inc/class/design/PaymentDesign.php
@@ -7,6 +7,7 @@
  * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package        PH7 / App / System / Module / Payment / Inc / Class / Design
  */
+
 namespace PH7;
 
 use PH7\Framework\Payment\Gateway\Api\Api as PaymentApi;
