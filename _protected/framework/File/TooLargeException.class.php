f1d73d3c77eb84f20fa2cd410cbe6e5f2961e7f8
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-03-28 19:36:52 +0100

    Add new specific exception classes

---
+++ _protected/framework/File/TooLargeException.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author           Pierre-Henry Soria <ph7software@gmail.com>
+ * @copyright        (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package          PH7 / Framework / File
+ */
+
+namespace PH7\Framework\File;
+
+use PH7\Framework\Error\CException\PH7RuntimeException;
+
+class TooLargeException extends PH7RuntimeException {}
