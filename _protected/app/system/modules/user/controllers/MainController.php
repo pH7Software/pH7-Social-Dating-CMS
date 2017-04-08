728e1e520da238c8e981ce8baa0b1125333add40
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-04-08 12:57:54 +0100

    Quick refactoring, cleanup + PSR

--- _protected/app/system/modules/user/controllers/MainController.php
+++ _protected/app/system/modules/user/controllers/MainController.php
@@ -5,17 +5,23 @@
  * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package        PH7 / App / System / Module / User / Controller
  */
+
 namespace PH7;
 
-use PH7\Framework\Mobile\MobApp, PH7\Framework\Mvc\Model\DbConfig;
+use PH7\Framework\Mobile\MobApp;
+use PH7\Framework\Mvc\Model\DbConfig;
+use PH7\Framework\Error\CException\PH7InvalidArgumentException;
 
 class MainController extends Controller
 {
+    const GUEST_SPLASH_FILE = 'index.guest_splash';
+    const GUEST_FILE = 'index.guest';
 
-    const GUEST_SPLASH_FILE = 'index.guest_splash', GUEST_FILE = 'index.guest';
+    /** @var string */
+    private $_sTitle;
 
-
-    private $_sTitle, $_bIsMobile;
+    /** @var bool */
+    private $_bIsMobile;
 
     /**
      * Displaying the main homepage of the website.
@@ -31,16 +37,14 @@ class MainController extends Controller
         $this->view->userDesign = new UserDesignCore;
 
         // Only visitors
-        if (!UserCore::auth())
-        {
+        if (!UserCore::auth()) {
             // To check if the site is called by a Mobile or Mobile Native App
             $this->_bIsMobile = $this->view->is_mobile = (MobApp::is($this->httpRequest, $this->session) || $this->browser->isMobile());
 
             $this->view->is_users_block = DbConfig::getSetting('usersBlock');
 
             // Background video is used only for the Splash page
-            if ($this->_getGuestTplPage() === static::GUEST_SPLASH_FILE)
-            {
+            if ($this->_getGuestTplPage() === static::GUEST_SPLASH_FILE) {
                 // Enable the Splash Video Background if enabled
                 $bIsBgVideo = (bool) DbConfig::getSetting('bgSplashVideo');
 
@@ -61,9 +65,7 @@ class MainController extends Controller
             $this->view->promo_text = DbConfig::getMetaMain(PH7_LANG_NAME)->promoText;
 
             $this->manualTplInclude($this->_getGuestTplPage() . '.inc.tpl');
-        }
-        elseif (UserCore::auth()) // Only for Members
-        {
+        } elseif (UserCore::auth()) {
             // Set CSS and JS files
             $this->design->addCss(PH7_LAYOUT . PH7_TPL . PH7_TPL_NAME . PH7_SH . PH7_CSS, 'zoomer.css');
             $this->design->addJs(PH7_STATIC . PH7_JS, 'Wall.js');
@@ -73,6 +75,7 @@ class MainController extends Controller
 
             $this->manualTplInclude('index.user.inc.tpl');
         }
+
         $this->output();
     }
 
@@ -118,33 +121,12 @@ class MainController extends Controller
      */
     private function _getGuestTplPage()
     {
-        /**
-         * When you are in the development mode, you can force the guest page by set a "force" GET request with the "splash" or "classic" parameter.
-         * Example: "/?force=splash" or "/?force=classic"
-         */
-        if (isDebug() && $this->httpRequest->getExists('force'))
-        {
-            switch ($this->httpRequest->get('force'))
-            {
-                case 'classic':
-                    $sPage = static::GUEST_FILE;
-                break;
-
-                case 'splash':
-                    $sPage = static::GUEST_SPLASH_FILE;
-                break;
-
-                default:
-                    exit('You can only choose between "classic" or "splash"');
-            }
-        }
-        elseif ($this->_bIsMobile || $this->browser->isMobile())
-        {
+        if (isDebug() && $this->httpRequest->getExists('force')) {
+            $sPage = $this->_getPageForced();
+        } elseif ($this->_bIsMobile || $this->browser->isMobile()) {
             /* 'index.guest.inc.tpl' is not responsive enough for very small screen resolutions, so set to 'index.guest_splash.inc.tpl' by default */
             $sPage = static::GUEST_SPLASH_FILE;
-        }
-        else
-        {
+        } else {
             $bIsSplashPage = (bool) DbConfig::getSetting('splashPage');
             $sPage = ($bIsSplashPage) ? static::GUEST_SPLASH_FILE : static::GUEST_FILE;
         }
@@ -152,4 +134,29 @@ class MainController extends Controller
         return $sPage;
     }
 
+    /**
+     * When you are in the development mode, you can force the guest page by set a "force" GET request with the "splash" or "classic" parameter.
+     * Example: "/?force=splash" or "/?force=classic"
+     *
+     * @throws PH7InvalidArgumentException
+     *
+     * @return string
+     */
+    private function _getPageForced()
+    {
+        switch ($this->httpRequest->get('force')) {
+            case 'classic':
+                $sPage = static::GUEST_FILE;
+            break;
+
+            case 'splash':
+                $sPage = static::GUEST_SPLASH_FILE;
+            break;
+
+            default:
+                throw new PH7InvalidArgumentException('You can only choose between "classic" or "splash"');
+        }
+
+        return $sPage;
+    }
 }
