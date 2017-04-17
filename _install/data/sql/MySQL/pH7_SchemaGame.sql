19b0f49ccd592cd746463692afd6f65fd5a55db4
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-04-17 12:22:38 +0100

    Remove/Update copyright header info

--- _install/data/sql/MySQL/pH7_SchemaGame.sql
+++ _install/data/sql/MySQL/pH7_SchemaGame.sql
@@ -2,11 +2,10 @@
 --
 -- Title:         SQL Schema Game Install File
 --
--- Author:        Pierre-Henry Soria <ph7software@gmail.com>
+-- Author:        Pierre-Henry Soria <hello@ph7cms.com>
 -- Copyright:     (c) 2012-2017, Pierre-Henry Soria. All Rights Reserved.
 -- License:       GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
 -- Package:       PH7 / Install / Data / Sql
--- Version:       1.1
 --
 --
 
