b717babcb3c008dccb1f18734ce4a7d9d9a9b155
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-04-19 18:43:15 +0100

    Import classes + add curly brackets in condition

--- _protected/framework/Ads/Ads.class.php
+++ _protected/framework/Ads/Ads.class.php
@@ -2,21 +2,22 @@
 /**
  * @title            Advertisement Class
  *
- * @author           Pierre-Henry Soria <ph7software@gmail.com>
+ * @author           Pierre-Henry Soria <hello@ph7cms.com>
  * @copyright        (c) 2013-2017, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / Ads
- * @version          1.0
  */
 
 namespace PH7\Framework\Ads;
+
 defined('PH7') or exit('Restricted access');
 
 use PH7\Framework\Mvc;
+use PH7\Framework\Parse\SysVar;
+use PH7\Framework\Analytics\Statistic;
 
 class Ads
 {
-
     const PARAM_URL = 'ad_click';
 
     /**
@@ -29,15 +30,15 @@ class Ads
     public static function output($oData)
     {
         // Stat Advertisement Shows
-        \PH7\Framework\Analytics\Statistic::setView($oData->adsId, 'Ads');
+        Statistic::setView($oData->adsId, 'Ads');
 
         // Advertisement Clicks
         $oHttpRequest = new Mvc\Request\Http;
-        if ($oHttpRequest->getExists(static::PARAM_URL) && $oHttpRequest->get(static::PARAM_URL) == $oData->adsId)
+        if ($oHttpRequest->getExists(static::PARAM_URL) && $oHttpRequest->get(static::PARAM_URL) == $oData->adsId) {
             Mvc\Model\Ads::setClick($oData->adsId);
+        }
         unset($oHttpRequest);
 
-        return (new \PH7\Framework\Parse\SysVar)->parse($oData->code);
+        return (new SysVar)->parse($oData->code);
     }
-
 }
