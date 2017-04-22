7ec786ba702b7b4bb9aa8b9523950c5fa4714d17
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-04-22 18:15:07 +0100

    Increase version number
    Since more changes than inspected have been done

--- _protected/framework/Security/Version.class.php
+++ _protected/framework/Security/Version.class.php
@@ -23,7 +23,7 @@ final class Version
     /***** Framework Kernel *****/
     // 1.0, 1.1 branches were "pOH", 1.2 was "pOW", 1.3, 1.4 were "p[H]", 2.* was "H2O", 3.* was "H3O", 4.* was "HCO", 5.* was "pCO" and 6.* is "WoW"
     const KERNEL_VERSION_NAME = 'WoW';
-    const KERNEL_VERSION = '6.0.10';
+    const KERNEL_VERSION = '6.0.13';
     const KERNEL_BUILD = '1';
     const KERNEL_RELASE_DATE = '2017-05-22';
 
