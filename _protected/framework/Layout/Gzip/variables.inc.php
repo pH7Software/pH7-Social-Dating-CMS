9d1aca27a437af5493b5d8f083cc7564242db23e
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-06-02 20:49:50 +0100

    Add missing header copyright comment + update comment email address

--- _protected/framework/Layout/Gzip/variables.inc.php
+++ _protected/framework/Layout/Gzip/variables.inc.php
@@ -2,12 +2,14 @@
 /**
  * @title            CSS & JS dynamic variables
  *
- * @author           Pierre-Henry Soria <ph7software@gmail.com>
+ * @author           Pierre-Henry Soria <hello@ph7cms.com>
  * @copyright        (c) 2015-2017, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / Layout / Gzip
  */
+
 namespace PH7\Framework\Layout\Gzip;
+
 defined('PH7') or exit('Restricted access');
 
 return array(
