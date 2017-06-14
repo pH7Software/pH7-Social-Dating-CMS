035de2b50dc02951ec169c8656e6ef701f8f93cc
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-06-14 18:34:19 +0100

    Add missing comment

--- _protected/framework/Layout/Tpl/Engine/PH7Tpl/Exception.class.php
+++ _protected/framework/Layout/Tpl/Engine/PH7Tpl/Exception.class.php
@@ -1,3 +1,12 @@
 <?php
+/***************************************************************************
+ * @author           Pierre-Henry Soria <ph7software@gmail.com>
+ * @category         PH7 Template Engine
+ * @package          PH7 / Framework / Layout / Tpl / Engine / PH7Tpl
+ * @copyright        (c) 2012-2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license          CC-BY License - http://creativecommons.org/licenses/by/3.0/
+ *
+ ***************************************************************************/
 namespace PH7\Framework\Layout\Tpl\Engine\PH7Tpl;
+
 class Exception extends \PH7\Framework\Layout\Exception {}
