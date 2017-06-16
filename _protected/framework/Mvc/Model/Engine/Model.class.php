ca16cb566032f040651b988e1ce1de160bf05ea9
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-06-16 18:45:44 +0100

    Import namespaces and remove unnecessary blank newlines

--- _protected/framework/Mvc/Model/Engine/Model.class.php
+++ _protected/framework/Mvc/Model/Engine/Model.class.php
@@ -6,17 +6,17 @@
  * @copyright        (c) 2012-2017, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / Mvc / Model / Engine
- * @version          0.6
  */
 
 namespace PH7\Framework\Mvc\Model\Engine;
+
 defined('PH7') or exit('Restricted access');
 
-use PH7\Framework\Cache\Cache, PH7\Framework\File\File;
+use PH7\Framework\Cache\Cache;
+use PH7\Framework\File\File;
 
 abstract class Model extends Entity
 {
-
     const SQL_FILE_EXT = '.sql';
 
     protected $orm, $cache;
@@ -72,5 +72,4 @@ abstract class Model extends Entity
     {
         unset($this->orm, $this->cache);
     }
-
 }
