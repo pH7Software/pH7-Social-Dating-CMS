2d86e9b6a04941b35199d9e2a12bb13dc928ab76
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-06-27 18:01:57 +0100

    Update contrubiting comment

--- _protected/framework/Cache/Storage/Memcache.class.php
+++ _protected/framework/Cache/Storage/Memcache.class.php
@@ -11,20 +11,22 @@
  */
 
 namespace PH7\Framework\Cache\Storage;
+
 defined('PH7') or exit('Restricted access');
 
 /**
- * Memcache class is still under development, if you are a developer and you want to help us and join our volunteer team of developers to continue development of this class, you're welcome!
- * Please contact us by email: ph7software@gmail.com
+ * Memcache class is still not done. If you are a developer and want to help us and join a wonderful volunteer team of passionate developers
+ * to continue development of this class, you're welcome!
+ *
+ * Our open source Github repo is: https://github.com/pH7Software/pH7-Social-Dating-CMS
  *
  * Thank you,
- * pH7 developers team.
+ * Pierre-Henry Soria
  */
+
 class Memcache
 {
-
     public function __construct()
     {
     }
-
 }
