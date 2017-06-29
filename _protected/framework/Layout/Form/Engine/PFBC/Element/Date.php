27b890e58eda2935172fe0706c7a1fa75fd6b02b
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-06-29 18:40:52 +0100

    Take into account date format from langs/config.ini for "datepicker"

--- _protected/framework/Layout/Form/Engine/PFBC/Element/Date.php
+++ _protected/framework/Layout/Form/Engine/PFBC/Element/Date.php
@@ -1,22 +1,24 @@
 <?php
 /**
- * We made many changes in this code.
- * By pH7 (Pierre-Henry SORIA).
+ * I made changes in this code (by Pierre-Henry SORIA).
  */
+
 namespace PFBC\Element;
+
+use PH7\Framework\Config\Config;
 use PH7\Framework\Mvc\Model\DbConfig;
 
 class Date extends Textbox
 {
-
     public function jQueryDocumentReady()
     {
         parent::jQueryDocumentReady();
         $iCurrentYear = date('Y');
         $iMin = $iCurrentYear - DbConfig::getSetting('maxAgeRegistration');
         $iMax = $iCurrentYear - DbConfig::getSetting('minAgeRegistration');
+        $sDateFormat = Config::getInstance()->values['language.application']['date_time_format'];
 
-        echo 'jQuery("#', $this->attributes['id'], '").datepicker({dateFormat:\'mm/dd/yy\',defaultDate:-9862,changeMonth:true,changeYear:true,yearRange:\''.$iMin.':'.$iMax.'\'});';
+        echo 'jQuery("#', $this->attributes['id'], '").datepicker({dateFormat:\''. $sDateFormat . '\',defaultDate:-9862,changeMonth:true,changeYear:true,yearRange:\''.$iMin.':'.$iMax.'\'});';
     }
 
     public function render()
