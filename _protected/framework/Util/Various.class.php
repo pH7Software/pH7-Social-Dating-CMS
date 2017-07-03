c4c19009294e653ace581cb22ee03e14524aca20
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-03 19:32:29 +0100

    Replace "integer" to "int" phpdoc type

--- _protected/framework/Util/Various.class.php
+++ _protected/framework/Util/Various.class.php
@@ -26,13 +26,13 @@ class Various
      * Generate Random.
      *
      * @param string|null $sStr
-     * @param integer $iLength Default is 40 Characters.
+     * @param int $iLength Default is 40 Characters.
      *
      * @return string
      */
     public static function genRnd($sStr = null, $iLength = self::MAX_LENGTH)
     {
-        $sStr = (!empty($sStr)) ? (string)$sStr : '';
+        $sStr = (!empty($sStr)) ? (string) $sStr : '';
         $sChars = hash('whirlpool', hash('whirlpool', uniqid(mt_rand(), true) . $sStr . Ip::get() . time()) . hash('sha512', (new Browser)->getUserAgent() . microtime(true) * 9999));
         return self::padStr($sChars, $iLength);
     }
@@ -41,7 +41,7 @@ class Various
      * Padding String.
      *
      * @param string $sStr
-     * @param integer $iLength
+     * @param int $iLength
      *
      * @return string
      */
