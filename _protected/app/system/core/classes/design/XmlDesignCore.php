b38d03144b5e329ab3674e3a0e6070b62b7e3f68
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-06 19:25:07 +0100

    Typo in function name

--- _protected/app/system/core/classes/design/XmlDesignCore.php
+++ _protected/app/system/core/classes/design/XmlDesignCore.php
@@ -72,7 +72,7 @@ class XmlDesignCore
         try {
             $aNews = (new NewsFeedCore)->getSoftware($iNum);
 
-            if (cound($aNews) > 0) {
+            if (count($aNews) > 0) {
                 foreach ($aNews as $aItems) {
                     echo '<h4><a href="', $aItems['link'], '" target="_blank">', escape($aItems['title'], true), '</a></h4>';
                     echo '<p>', escape($aItems['description'], true), '</p>';
