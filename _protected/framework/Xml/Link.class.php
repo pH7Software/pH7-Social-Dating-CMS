54f2f83e4a572d76e29cd0941536d25b740dd64e
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-06 18:28:22 +0100

    Remove useless __destruct func, import namespace + cleanup coding style

--- _protected/framework/Xml/Link.class.php
+++ _protected/framework/Xml/Link.class.php
@@ -11,46 +11,50 @@
  */
 
 namespace PH7\Framework\Xml;
+
 defined('PH7') or exit('Restricted access');
+
+use DOMDocument;
 
 class Link
 {
+    /** @var DOMDocument */
+    private $_oXml;
 
-    private $_oXml, $_sPath, $_aRet = array();
+    /** @var string */
+    private $_sPath;
+
+    /** @var array */
+    private $_aRet = array();
 
     /**
      * Constructor with the instance of the DOMDocument object.
      *
      * @param string $sPath The path to the XML file. You can also specify an URL if the "allow_url_fopen" PHP directive is enabled.
-     * @return void
      */
     public function __construct($sPath)
     {
         $this->_sPath = $sPath;
-        // Creating a DOM Object
-        $this->_oXml = new \DOMDocument;
+        $this->_oXml = new DOMDocument;
     }
 
     /**
      * Gets the XML links.
      *
      * @return array The XML tree.
-     * @throws \PH7\Framework\Xml\Exception If the XML file is not found.
+     *
+     * @throws Exception If the XML file is not found.
      */
     public function get()
     {
-        if (!@$this->_oXml->load($this->_sPath))
+        if (!@$this->_oXml->load($this->_sPath)) {
             throw new Exception('The file \'' . $this->_sPath . '\' does not exist or is not a valid XML file.');
+        }
 
-        foreach ($this->_oXml->getElementsByTagName('link') as $oTag)
+        foreach ($this->_oXml->getElementsByTagName('link') as $oTag) {
             $this->_aRet[$oTag->getAttribute('url')] = $oTag->getAttribute('title');
+        }
 
         return $this->_aRet;
     }
-
-    public function __destruct()
-    {
-        unset($this->_oXml, $this->_sPath, $this->_aRet);
-    }
-
 }
