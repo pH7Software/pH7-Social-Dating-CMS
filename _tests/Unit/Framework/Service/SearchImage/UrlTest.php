c7d8b0de1699d6e655d7f01316baeada6e0c5569
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-10 14:19:35 +0100

    Unit test Url Image class

---
+++ _tests/Unit/Framework/Service/SearchImage/UrlTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @author           Pierre-Henry Soria <hello@ph7cms.com>
+ * @copyright        (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package          PH7 / Test / Unit / Framework / Service / SearchImage
+ */
+
+namespace PH7\Test\Unit\Framework\Service\SearchImage;
+
+use PH7\Framework\Service\SearchImage\Url;
+use PHPUnit_Framework_TestCase;
+
+class UrlTest extends PHPUnit_Framework_TestCase
+{
+    public function testValidValue()
+    {
+        $sTestUrl = 'https://ph7cms.com/dating-business-by-steps/';
+        $sUrl = new Url($sTestUrl);
+        $this->assertSame($sTestUrl, $sUrl->getValue());
+    }
+
+    /**
+     * @param string $sUrl
+     *
+     * @dataProvider invalidUrlsProvider
+     *
+     * @expectedException \PH7\Framework\Service\SearchImage\InvalidUrlException
+     */
+    public function testInvalidValue($sUrl)
+    {
+        new Url($sUrl);
+    }
+    /**
+     * @return array
+     */
+    public function invalidUrlsProvider()
+    {
+        return [
+            ['blablabla'],
+            [''],
+            [str_pad('abc', FILTER_VALIDATE_URL * 2, 'abc')]
+        ];
+    }
+}
