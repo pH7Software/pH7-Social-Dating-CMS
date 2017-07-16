8990c9055d118f488c61ddaa74111f0d1eb5f160
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-16 22:06:59 +0100

    File cleanup

--- _protected/framework/Mvc/Model/License.class.php
+++ _protected/framework/Mvc/Model/License.class.php
@@ -7,15 +7,16 @@
  */
 
 namespace PH7\Framework\Mvc\Model;
+
 defined('PH7') or exit('Restricted access');
 
 class License extends Engine\Model
 {
-
     /**
      * Get the license key.
      *
      * @param integer $iId The License ID. Default 1
+     *
      * @return string
      */
     public function get($iId = 1)
@@ -28,11 +29,11 @@ class License extends Engine\Model
      *
      * @param string $sKey
      * @param integer $iId Column ID. Default 1
-     * @return mixed (integer | boolean) Returns the number of rows on success or FALSE on failure.
+     *
+     * @return integer|boolean Returns the number of rows on success or FALSE on failure.
      */
     public function save($sKey, $iId = 1)
     {
         return $this->orm->update('License', 'licenseKey', $sKey, 'licenseId', $iId);
     }
-
 }
