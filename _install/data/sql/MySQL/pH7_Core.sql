9ddb88e5b03fbb7baee9929a8b2403876fcd21b8
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-17 15:20:42 +0100

    CHange default site name

--- _install/data/sql/MySQL/pH7_Core.sql
+++ _install/data/sql/MySQL/pH7_Core.sql
@@ -12,7 +12,7 @@
 --
 -- Set the variables --
 --
-SET @sDefaultSiteName = 'Social Dating App';
+SET @sDefaultSiteName = 'Online Dating Startup';
 SET @sAdminEmail = 'admin@yoursite.com';
 SET @sFeedbackEmail = 'feedback@yoursite.com';
 SET @sNoReplyEmail = 'noreply@yoursite.com';
