008dfa002d61b9ac7f835d5e56f46acc4ad9a731
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-07-24 03:30:21 +0100

    Remove unnecessary quote/space

--- _protected/app/system/modules/two-factor-auth/controllers/MainController.php
+++ _protected/app/system/modules/two-factor-auth/controllers/MainController.php
@@ -93,7 +93,7 @@ class MainController extends Controller
     protected function download($sSecret)
     {
         $sFileName = '2FA-backup-code-' . $this->sMod . '-' . Url::clean($this->registry->site_name) . '.txt';
-        header('Content-Disposition: attachment; filename=" ' . $sFileName . '"');
+        header('Content-Disposition: attachment; filename=' . $sFileName);
 
         echo t('BACKUP VERIFICATION CODE - %site_url% | %0% area', $this->sMod) . "\r\n\r\n";
         echo t('Code: %0%', $this->oAuthenticator->getCode($sSecret)) . "\r\n\r\n";
