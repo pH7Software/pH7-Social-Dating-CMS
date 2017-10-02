5431d0f8801dc8b8ea84a280b5ebfd99a2c56152
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-02 14:54:05 -0700

    Chage dialog popup size

--- templates/system/modules/ph7cms-donation/themes/base/js/donationbox.js
+++ templates/system/modules/ph7cms-donation/themes/base/js/donationbox.js
@@ -7,9 +7,8 @@
 var $donationBox = (function () {
     $.get(pH7Url.base + 'ph7cms-donation/main/donationbox', function (oData) {
         $.colorbox({
-            width: '100%',
             width: '200px',
-            height: '155px',
+            height: '200px',
             speed: 500,
             scrolling: false,
             html: $(oData).find('#box_block')
