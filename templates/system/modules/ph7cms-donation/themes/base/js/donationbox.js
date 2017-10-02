c3f9e5eff4b8b7e7de0e4a6f7da1faad077b23ee
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-02 09:32:11 -0700

    Change size of donation popup

--- templates/system/modules/ph7cms-donation/themes/base/js/donationbox.js
+++ templates/system/modules/ph7cms-donation/themes/base/js/donationbox.js
@@ -8,8 +8,8 @@ var $validationBox = (function () {
     $.get(pH7Url.base + 'ph7cms-donation/main/donationbox', function (oData) {
         $.colorbox({
             width: '100%',
-            maxWidth: '450px',
-            maxHeight: '85%',
+            width: '200px',
+            height: '155px',
             speed: 500,
             scrolling: false,
             html: $(oData).find('#box_block')
