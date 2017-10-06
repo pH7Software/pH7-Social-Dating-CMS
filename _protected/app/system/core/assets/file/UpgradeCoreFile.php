f222b65e5c68d8861da45605c9b834014bfbca92
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-06 16:57:17 -0700

    Bump class version number

--- _protected/app/system/core/assets/file/UpgradeCoreFile.php
+++ _protected/app/system/core/assets/file/UpgradeCoreFile.php
@@ -7,7 +7,7 @@
  * @copyright        (c) 2012-2017, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / App / System / Core / Asset / File
- * @version          1.3
+ * @version          1.5
  */
 
 namespace PH7;
