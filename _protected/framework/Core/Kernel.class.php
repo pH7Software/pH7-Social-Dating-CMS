b6ec322f9a13b44f125894a567256e7e8cc01f61
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-07 01:29:38 -0700

    Already bump copyright year
    So I wont forget :D

--- _protected/framework/Core/Kernel.class.php
+++ _protected/framework/Core/Kernel.class.php
@@ -39,7 +39,7 @@ abstract class Kernel
     const SOFTWARE_AUTHOR = 'Pierre-Henry Soria';
     const SOFTWARE_COMPANY = 'Social Dating CMS | Pierre-Henry Soria';
     const SOFTWARE_LICENSE = 'GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.';
-    const SOFTWARE_COPYRIGHT = '(c) 2011-2017, Pierre-Henry Soria. All Rights Reserved.';
+    const SOFTWARE_COPYRIGHT = '(c) 2011-2018, Pierre-Henry Soria. All Rights Reserved.';
     const SOFTWARE_VERSION_NAME = Version::KERNEL_VERSION_NAME;
     const SOFTWARE_VERSION = Version::KERNEL_VERSION;
     const SOFTWARE_BUILD = Version::KERNEL_BUILD;
