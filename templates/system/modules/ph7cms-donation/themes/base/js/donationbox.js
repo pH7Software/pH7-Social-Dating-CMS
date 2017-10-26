466100e4e513b4e8de75cbe0dce8e8fbca4b895b
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-26 02:17:42 -0700

    Ajust size donationbox + menu link text

--- templates/system/modules/ph7cms-donation/themes/base/js/donationbox.js
+++ templates/system/modules/ph7cms-donation/themes/base/js/donationbox.js
@@ -7,8 +7,8 @@
 var $donationBox = (function () {
     $.get(pH7Url.base + 'ph7cms-donation/main/donationbox', function (oData) {
         $.colorbox({
-            width: '220px',
-            height: '200px',
+            width: '240px',
+            height: '220px',
             speed: 500,
             scrolling: false,
             html: $(oData).find('#box_block')
