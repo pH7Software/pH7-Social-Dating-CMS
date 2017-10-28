9cf3e107d77783cb4dbf2242a042d4d220c69bd1
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-10-27 19:46:42 -0700

    Use short array syntax (PHP >= 5.4)

--- _protected/framework/Layout/Gzip/variables.inc.php
+++ _protected/framework/Layout/Gzip/variables.inc.php
@@ -12,9 +12,9 @@ namespace PH7\Framework\Layout\Gzip;
 
 defined('PH7') or exit('Restricted access');
 
-return array(
+return [
     'url_theme' => PH7_URL_ROOT . PH7_LAYOUT . PH7_TPL, // Replace "[$url_theme]" variable
     'url_def_tpl_css' => PH7_URL_ROOT . PH7_LAYOUT . PH7_TPL . PH7_DEFAULT_THEME . PH7_SH . PH7_CSS, // Replace "[$url_def_tpl_css]" variable
     'url_def_tpl_js' => PH7_URL_ROOT . PH7_LAYOUT . PH7_TPL . PH7_DEFAULT_THEME . PH7_SH . PH7_JS, // Replace "[$url_def_tpl_js]" variable
     'current_tpl_name' => $getCurrentTplName(), // Replace "[$current_tpl_name]" variable
-);
+];
