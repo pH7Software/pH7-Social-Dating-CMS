7ec5819040b1b15608c7b13d17b245848cd00b96
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-06 20:10:23 -0800

    Cleanup class, replace double quote to single ones, remove extra lines

--- _protected/framework/Layout/Form/Engine/PFBC/Element/YesNo.php
+++ _protected/framework/Layout/Form/Engine/PFBC/Element/YesNo.php
@@ -1,4 +1,8 @@
 <?php
+/**
+ * We made this code.
+ * By pH7 (Pierre-Henry SORIA).
+ */
 
 namespace PFBC\Element;
 
@@ -6,15 +10,16 @@ class YesNo extends Radio
 {
     public function __construct($label, $name, array $properties = null)
     {
-        $options = array(
-            "1" => "Yes",
-            "0" => "No"
-        );
+        $options = [
+            '1' => 'Yes',
+            '0' => 'No'
+        ];
 
-        if (!is_array($properties))
-            $properties = array("inline" => 1);
-        elseif (!array_key_exists("inline", $properties))
-            $properties["inline"] = 1;
+        if (!is_array($properties)) {
+            $properties = array('inline' => 1);
+        } elseif (!array_key_exists('inline', $properties)) {
+            $properties['inline'] = 1;
+        }
 
         parent::__construct($label, $name, $options, $properties);
     }
