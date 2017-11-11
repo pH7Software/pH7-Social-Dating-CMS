e35220e7f5b4fa1849611ed242154170f6963975
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-11 01:02:53 -0800

    Create new func to avoid duplicating code

--- _tools/pH7.sh
+++ _tools/pH7.sh
@@ -155,19 +155,13 @@ function file-strict-permissions() {
 # Push the project into GitHub and Bitbucket repos
 function save-code() {
     # Bitbucket repo
-    git remote rm origin
-    git remote add origin git@bitbucket.org:pH_7/ph7cms-social-dating-app-site-builder.git
-    git push
+    _save-git-project-to-repo git@bitbucket.org:pH_7/ph7cms-social-dating-app-site-builder.git
 
     # GitLab repo
-    git remote rm origin
-    git remote add origin git@gitlab.com:pH-7/pH7CMS.git
-    git push
+    _save-git-project-to-repo git@gitlab.com:pH-7/pH7CMS.git
 
     # GitHub repo
-    git remote rm origin
-    git remote add origin git@github.com:pH7Software/pH7-Social-Dating-CMS.git
-    git push
+    _save-git-project-to-repo git@github.com:pH7Software/pH7-Social-Dating-CMS.git
 
     echo "Yaaay! Changes successfully saved into remote repos!"
 }
@@ -234,6 +228,13 @@ function _permissions() {
 function _cache-permissions() {
     sudo chmod 777 -R ./_install/data/caches/*
     sudo chmod 777 -R ./_protected/data/cache/*
+}
+
+# Save a git project to the specified repo (e.g. github, bitbucket)
+function _save-git-project-to-repo() {
+    git remote rm origin
+    git remote add origin $1
+    git push
 }
 
 # Confirmation of orders entered
