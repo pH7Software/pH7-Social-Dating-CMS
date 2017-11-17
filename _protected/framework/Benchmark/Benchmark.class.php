1ce7273e3f490fa4eec417149c8c87c05c09033d
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-17 00:00:46 -0800

    Bump copyright year [forgotten file]

--- _protected/framework/Benchmark/Benchmark.class.php
+++ _protected/framework/Benchmark/Benchmark.class.php
@@ -6,7 +6,7 @@
  *
  * Copyright (c) 2012 Jeremy Perret
  *
- * File Modified by Pierre-Henry Soria, Copyright (c) 2014-2017
+ * File Modified by Pierre-Henry Soria, Copyright (c) 2014-2018
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
