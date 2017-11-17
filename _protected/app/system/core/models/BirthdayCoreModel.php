8b5de5a7253c9d60320786feb6a8ff7709ffb113
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-16 23:47:45 -0800

    Remove useless version info in header doc

--- _protected/app/system/core/models/BirthdayCoreModel.php
+++ _protected/app/system/core/models/BirthdayCoreModel.php
@@ -6,7 +6,6 @@
  * @copyright      (c) 2013-2018, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package        PH7 / App / System / Core / Model
- * @version        1.0
  */
 
 namespace PH7;
