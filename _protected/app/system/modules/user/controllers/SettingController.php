32b0a5c70aaef324ef03e7713c81d7b3046e5921
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-16 23:39:28 -0800

    Dont use underscore for private attributes (Old Pear coding style)

--- _protected/app/system/modules/user/controllers/SettingController.php
+++ _protected/app/system/modules/user/controllers/SettingController.php
@@ -13,22 +13,38 @@ use PH7\Framework\Url\Header;
 
 class SettingController extends Controller
 {
-    private $_sUsername, $_sFirstName, $_sSex, $_sTitle, $_iProfileId, $_bAdminLogged;
+    /** @var string */
+    private $sUsername;
+
+    /** @var string */
+    private $sFirstName;
+
+    /** @var string */
+    private $sSex;
+
+    /** @var string */
+    private $sTitle;
+
+    /** @var int */
+    private $iProfileId;
+
+    /** @var bool */
+    private $bAdminLogged;
 
     public function __construct()
     {
         parent::__construct();
 
-        $this->_bAdminLogged = (AdminCore::auth() && !User::auth());
-        $this->_iProfileId = (int) ($this->_bAdminLogged && $this->httpRequest->getExists('profile_id')) ? $this->httpRequest->get('profile_id') : $this->session->get('member_id');
-        $this->_sUsername = ($this->_bAdminLogged && $this->httpRequest->getExists('username')) ? $this->httpRequest->get('username') : $this->session->get('member_username');
-        $this->_sFirstName = ($this->_bAdminLogged && $this->httpRequest->getExists('first_name')) ? $this->httpRequest->get('first_name') : $this->session->get('member_first_name');
-        $this->_sSex = ($this->_bAdminLogged && $this->httpRequest->getExists('sex')) ? $this->httpRequest->get('sex') : $this->session->get('member_sex');
+        $this->bAdminLogged = (AdminCore::auth() && !User::auth());
+        $this->iProfileId = (int) ($this->bAdminLogged && $this->httpRequest->getExists('profile_id')) ? $this->httpRequest->get('profile_id') : $this->session->get('member_id');
+        $this->sUsername = ($this->bAdminLogged && $this->httpRequest->getExists('username')) ? $this->httpRequest->get('username') : $this->session->get('member_username');
+        $this->sFirstName = ($this->bAdminLogged && $this->httpRequest->getExists('first_name')) ? $this->httpRequest->get('first_name') : $this->session->get('member_first_name');
+        $this->sSex = ($this->bAdminLogged && $this->httpRequest->getExists('sex')) ? $this->httpRequest->get('sex') : $this->session->get('member_sex');
 
         /** For the avatar on the index and avatar page **/
-        $this->view->username = $this->_sUsername;
-        $this->view->first_name = $this->_sFirstName;
-        $this->view->sex = $this->_sSex;
+        $this->view->username = $this->sUsername;
+        $this->view->first_name = $this->sFirstName;
+        $this->view->sex = $this->sSex;
         $this->view->avatarDesign = new AvatarDesignCore; // Avatar Design Class
 
         /** For the wallpaper on the index and design page **/
@@ -43,17 +59,17 @@ class SettingController extends Controller
         // Add Css Style for Tabs
         $this->design->addCss(PH7_LAYOUT . PH7_TPL . PH7_TPL_NAME . PH7_SH . PH7_CSS, 'tabs.css');
 
-        $this->_sTitle = t('Account Settings');
-        $this->view->page_title = $this->_sTitle;
-        $this->view->h2_title = $this->_sTitle;
+        $this->sTitle = t('Account Settings');
+        $this->view->page_title = $this->sTitle;
+        $this->view->h2_title = $this->sTitle;
         $this->output();
     }
 
     public function edit()
     {
-        $this->_sTitle = t('Edit Your Profile');
-        $this->view->page_title = $this->_sTitle;
-        $this->view->h2_title = $this->_sTitle;
+        $this->sTitle = t('Edit Your Profile');
+        $this->view->page_title = $this->sTitle;
+        $this->view->h2_title = $this->sTitle;
         $this->output();
     }
 
@@ -70,9 +86,9 @@ class SettingController extends Controller
 
     public function design()
     {
-        $this->_sTitle = t('Your Wallpaper');
-        $this->view->page_title = $this->_sTitle;
-        $this->view->h2_title = $this->_sTitle;
+        $this->sTitle = t('Your Wallpaper');
+        $this->view->page_title = $this->sTitle;
+        $this->view->h2_title = $this->sTitle;
 
         if ($this->httpRequest->postExists('del'))
             $this->_removeWallpaper();
@@ -82,33 +98,33 @@ class SettingController extends Controller
 
     public function notification()
     {
-        $this->_sTitle = t('Notifications');
-        $this->view->page_title = $this->_sTitle;
-        $this->view->h2_title = $this->_sTitle;
+        $this->sTitle = t('Notifications');
+        $this->view->page_title = $this->sTitle;
+        $this->view->h2_title = $this->sTitle;
         $this->output();
     }
 
     public function privacy()
     {
-        $this->_sTitle = t('Privacy Settings');
-        $this->view->page_title = $this->_sTitle;
-        $this->view->h2_title = $this->_sTitle;
+        $this->sTitle = t('Privacy Settings');
+        $this->view->page_title = $this->sTitle;
+        $this->view->h2_title = $this->sTitle;
         $this->output();
     }
 
     public function password()
     {
-        $this->_sTitle = t('Change Password');
-        $this->view->page_title = $this->_sTitle;
-        $this->view->h2_title = $this->_sTitle;
+        $this->sTitle = t('Change Password');
+        $this->view->page_title = $this->sTitle;
+        $this->view->h2_title = $this->sTitle;
         $this->output();
     }
 
     public function delete()
     {
-        $this->_sTitle = t('Delete Account');
-        $this->view->page_title = $this->_sTitle;
-        $this->view->h2_title = $this->_sTitle;
+        $this->sTitle = t('Delete Account');
+        $this->view->page_title = $this->sTitle;
+        $this->view->h2_title = $this->sTitle;
 
         if ($this->httpRequest->get('delete_status') == 'yesdelete') {
             $this->session->set('yes_delete', 1);
@@ -134,21 +150,28 @@ class SettingController extends Controller
 
     private function _removeAvatar()
     {
-        (new UserCore)->deleteAvatar($this->_iProfileId, $this->_sUsername);
+        (new UserCore)->deleteAvatar($this->iProfileId, $this->sUsername);
+
         Header::redirect(null, t('Profile photo successfully deleted'));
     }
 
     private function _getWallpaper()
     {
-        $sBackground = (new UserModel)->getBackground($this->_iProfileId, 1);
-        return (!empty($sBackground)) ?
-            PH7_URL_DATA_SYS_MOD . 'user/background/img/' . $this->_sUsername . PH7_SH . $sBackground :
-            PH7_URL_TPL . PH7_TPL_NAME . PH7_SH . PH7_IMG . 'icon/none.jpg';
+        $sBackground = (new UserModel)->getBackground($this->iProfileId, 1);
+
+        if (!empty($sBackground)) {
+            $sBgFullPath = PH7_URL_DATA_SYS_MOD . 'user/background/img/' . $this->sUsername . PH7_SH . $sBackground;
+        } else {
+            $sBgFullPath = PH7_URL_TPL . PH7_TPL_NAME . PH7_SH . PH7_IMG . 'icon/none.jpg';
+        }
+
+        return $sBgFullPath;
     }
 
     private function _removeWallpaper()
     {
-        (new UserCore)->deleteBackground($this->_iProfileId, $this->_sUsername);
+        (new UserCore)->deleteBackground($this->iProfileId, $this->sUsername);
+
         Header::redirect(null, t('Your wallpaper has been deleted successfully!'));
     }
 }
