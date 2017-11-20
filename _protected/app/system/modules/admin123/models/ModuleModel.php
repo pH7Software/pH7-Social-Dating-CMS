bdd1b48944f494fbe18ca51e1a01d80dc6ca74bc
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-20 10:30:35 -0800

    Fix indentation

--- _protected/app/system/modules/admin123/models/ModuleModel.php
+++ _protected/app/system/modules/admin123/models/ModuleModel.php
@@ -8,20 +8,20 @@
 
 namespace PH7;
 
+use PH7\Framework\Mvc\Model\Engine\Model;
 use PH7\Framework\Mvc\Model\Engine\Util\Various;
 
-class ModuleModel extends Framework\Mvc\Model\Engine\Model
+class ModuleModel extends Model
 {
-
     /**
      * Executes sql queries for the module of the software.
      *
      * @param string $sSqlModuleFile File SQL
-     * @return mixed (boolean | array) Returns TRUE if there are no errors, otherwise returns an ARRAY of error information.
+     *
+     * @return bool|array Returns TRUE if there are no errors, otherwise returns an ARRAY of error information.
      */
     public function run($sSqlModuleFile)
     {
         return Various::execQueryFile($sSqlModuleFile);
     }
-
 }
