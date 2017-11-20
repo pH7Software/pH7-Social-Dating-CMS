104392a78b9ffceb3b861af3bd13e3884d81242b
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-20 12:12:30 -0800

    PSR-2; Add curly brackets on single-block conditions

--- _protected/framework/Mvc/Model/Module.class.php
+++ _protected/framework/Mvc/Model/Module.class.php
@@ -32,9 +32,11 @@ class Module extends Engine\Model
             $sSqlWhere = ($bIsFolderName) ? 'WHERE folderName = :modName LIMIT 1' : '';
 
             $rStmt = Db::getInstance()->prepare('SELECT ' . $sSelect . ' FROM ' . DB::prefix('SysModsEnabled') . $sSqlWhere);
-            if ($bIsFolderName) $rStmt->bindValue(':modName', $sFolderName, \PDO::PARAM_STR);
+            if ($bIsFolderName) {
+                $rStmt->bindValue(':modName', $sFolderName, \PDO::PARAM_STR);
+            }
             $rStmt->execute();
-            $sFetchMethod = ($bIsFolderName) ? 'fetch' : 'fetchAll';
+            $sFetchMethod = ($bIsFolderName ? 'fetch' : 'fetchAll');
             $oData = $rStmt->$sFetchMethod(\PDO::FETCH_OBJ);
             Db::free($rStmt);
             $this->cache->put($oData);
