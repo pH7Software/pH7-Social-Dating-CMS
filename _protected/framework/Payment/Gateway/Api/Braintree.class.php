f52aae067a6c58cf06bfa63520022724cf5d9fd9
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-20 03:53:13 -0800

    Bump '17 to '18 year from copyright file headers
    "(c) 2017" was forgotten in the previous branch

--- _protected/framework/Payment/Gateway/Api/Braintree.class.php
+++ _protected/framework/Payment/Gateway/Api/Braintree.class.php
@@ -3,7 +3,7 @@
  * @title            Braintree Class
  *
  * @author           Pierre-Henry Soria <hi@ph7.me>
- * @copyright        (c) 2017, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright        (c) 2017-2018, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / Payment / Gateway / Api
  */
