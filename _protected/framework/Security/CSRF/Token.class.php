pH7Software/pH7-Social-Dating-CMS

commit 720e467ae4706cd4bec971b96dfed06941cece45

Use class alias to avoid class name confusion

_protected/framework/Security/CSRF/Token.class.php
@@ -18,7 +18,7 @@ use PH7\AdminCore;
 use PH7\AffiliateCore;
 use PH7\Framework\Ip\Ip;
 use PH7\Framework\Mvc\Model\DbConfig;
-use PH7\Framework\Mvc\Request\Http;
+use PH7\Framework\Mvc\Request\Http as HttpRequest;
 use PH7\Framework\Navigation\Browser;
 use PH7\Framework\Session\Session;
 use PH7\Framework\Util\Various;
@@ -144,7 +144,7 @@ final class Token
      */
     public function checkUrl()
     {
-        $oHttpRequest = new Http;
+        $oHttpRequest = new HttpRequest;
         $bRet = ( ($this->currentSess() === true) || $oHttpRequest->currentUrl() === PH7_URL_ROOT || ($oHttpRequest->get(static::VAR_NAME) === $this->currentSess()) );
         unset($oHttpRequest);
 

commit 0b0fd70e14f85883709e55a62e99f49e82b64776

Http class didnt exist anymore
Caused by 720e467ae4706cd4bec971b96dfed06941cece45 commit. Forgot to rename the other Http declaration

_protected/framework/Security/CSRF/Token.class.php
@@ -100,7 +100,7 @@ final class Token
         $iTime = ($iTime === null) ? DbConfig::getSetting('securityTokenLifetime') : $iTime;
 
         // The default tag name for the security token
-        $sInputToken = (empty($sInputToken)) ? (new Http)->post('security_token') : $sInputToken;
+        $sInputToken = (empty($sInputToken)) ? (new HttpRequest)->post('security_token') : $sInputToken;
 
         $aCheckSession = [
             'security_token_' . $sName,