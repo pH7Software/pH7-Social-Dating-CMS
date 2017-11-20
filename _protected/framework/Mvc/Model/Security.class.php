6372bcfcd3d35f0ff44bfa9cbe682aa217df3200
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-20 12:14:56 -0800

    Import namespaces

--- _protected/framework/Mvc/Model/Security.class.php
+++ _protected/framework/Mvc/Model/Security.class.php
@@ -13,42 +13,54 @@ namespace PH7\Framework\Mvc\Model;
 
 defined('PH7') or exit('Restricted access');
 
+use DateInterval;
+use DateTime;
+use PDO;
+use PH7\Framework\Date\CDateTime;
 use PH7\Framework\Ip\Ip;
+use PH7\Framework\Layout\Tpl\Engine\PH7Tpl\PH7Tpl;
 use PH7\Framework\Mvc\Model\Engine\Db;
 use PH7\Framework\Mvc\Model\Engine\Util\Various;
+use PH7\Security as SecurityCore;
 
 class Security
 {
-    private $_sIp, $_sCurrentTime;
+    /** @var string */
+    private $sIp;
+
+    /** @var string */
+    private $sCurrentTime;
 
     public function __construct()
     {
-        $this->_sIp = Ip::get();
-        $this->_sCurrentTime = (new \PH7\Framework\Date\CDateTime)->get()->dateTime('Y-m-d H:i:s');
+        $this->sIp = Ip::get();
+        $this->sCurrentTime = (new CDateTime)->get()->dateTime('Y-m-d H:i:s');
     }
 
     /**
      * Block user IP
      *
      * @param string $sIp IP address.
-     * @param integer $iExpir Expiration in seconds. Default 86400
-     * @return boolean Returns TRUE if no IP has been found (and the new IP has been added to the block list), otherwise FALSE.
+     * @param int $iExpir Expiration in seconds. Default 86400
+     *
+     * @return bool Returns TRUE if no IP has been found (and the new IP has been added to the block list), otherwise FALSE.
      */
     public function blockIp($sIp, $iExpir = 86400)
     {
         $iExpir = time() + (int)$iExpir;
         $rStmt = Db::getInstance()->prepare('SELECT ip FROM' . Db::prefix('BlockIp') . 'WHERE ip = :ip LIMIT 1');
-        $rStmt->bindValue(':ip', $sIp, \PDO::PARAM_STR);
+        $rStmt->bindValue(':ip', $sIp, PDO::PARAM_STR);
         $rStmt->execute();
 
-        if ($rStmt->rowCount() == 0) // Not Found IP
-        {
+        // If IP is not found
+        if ($rStmt->rowCount() == 0) {
             $rStmt = Db::getInstance()->prepare('INSERT INTO' . Db::prefix('BlockIp') . 'VALUES (:ip, :expiration)');
-            $rStmt->bindValue(':ip', $sIp, \PDO::PARAM_STR);
-            $rStmt->bindValue(':expiration', $iExpir, \PDO::PARAM_INT);
+            $rStmt->bindValue(':ip', $sIp, PDO::PARAM_STR);
+            $rStmt->bindValue(':expiration', $iExpir, PDO::PARAM_INT);
             $rStmt->execute();
             return true;
         }
+
         return false;
     }
 
@@ -69,11 +81,11 @@ class Security
 
         $rStmt = Db::getInstance()->prepare('INSERT INTO' . Db::prefix($sTable . 'LogLogin') . '(email, username, password, status, ip)
         VALUES (:email, :username, :password, :status, :ip)');
-        $rStmt->bindValue(':email', $sEmail, \PDO::PARAM_STR);
-        $rStmt->bindValue(':username', $sUsername, \PDO::PARAM_STR);
-        $rStmt->bindValue(':password', $sPassword, \PDO::PARAM_STR);
-        $rStmt->bindValue(':status', $sStatus, \PDO::PARAM_STR);
-        $rStmt->bindValue(':ip', $this->_sIp, \PDO::PARAM_STR);
+        $rStmt->bindValue(':email', $sEmail, PDO::PARAM_STR);
+        $rStmt->bindValue(':username', $sUsername, PDO::PARAM_STR);
+        $rStmt->bindValue(':password', $sPassword, PDO::PARAM_STR);
+        $rStmt->bindValue(':status', $sStatus, PDO::PARAM_STR);
+        $rStmt->bindValue(':ip', $this->sIp, PDO::PARAM_STR);
         $rStmt->execute();
         Db::free($rStmt);
     }
@@ -81,34 +93,36 @@ class Security
     /**
      * Blocking access to the login page after exceeded login attempts.
      *
-     * @param integer $iMaxAttempts
-     * @param integer $iAttemptTime
+     * @param int $iMaxAttempts
+     * @param int $iAttemptTime
      * @param string $sEmail Email address of member.
-     * @param object \PH7\Framework\Layout\Tpl\Engine\PH7Tpl\PH7Tpl $oView
+     * @param PH7Tpl $oView
      * @param string $sTable Default 'Members'
-     * @return boolean Returns TRUE if attempts are allowed, FALSE otherwise.
+     *
+     * @return bool Returns TRUE if attempts are allowed, FALSE otherwise.
      */
-    public function checkLoginAttempt($iMaxAttempts, $iAttemptTime, $sEmail, \PH7\Framework\Layout\Tpl\Engine\PH7Tpl\PH7Tpl $oView, $sTable = 'Members')
+    public function checkLoginAttempt($iMaxAttempts, $iAttemptTime, $sEmail, PH7Tpl $oView, $sTable = 'Members')
     {
         Various::checkModelTable($sTable);
 
         $rStmt = Db::getInstance()->prepare('SELECT * FROM' . Db::prefix($sTable . 'AttemptsLogin') . 'WHERE ip = :ip LIMIT 1');
-        $rStmt->bindValue(':ip', $this->_sIp, \PDO::PARAM_STR);
+        $rStmt->bindValue(':ip', $this->sIp, PDO::PARAM_STR);
         $rStmt->execute();
 
         if ($rStmt->rowCount() == 1) {
-            $oRow = $rStmt->fetch(\PDO::FETCH_OBJ);
+            $oRow = $rStmt->fetch(PDO::FETCH_OBJ);
 
             if ($oRow->attempts >= $iMaxAttempts) {
-                $sLockoutTime = (new \DateTime($oRow->lastLogin))->add(\DateInterval::createFromDateString("$iAttemptTime minutes"))->format('Y-m-d H:i:s');
+                $sLockoutTime = (new DateTime($oRow->lastLogin))->add(DateInterval::createFromDateString("$iAttemptTime minutes"))->format('Y-m-d H:i:s');
 
-                if ($sLockoutTime > $this->_sCurrentTime) {
+                if ($sLockoutTime > $this->sCurrentTime) {
                     /**
                      * Send email to prevent that someone tries to hack their member account.
                      * We test that the number of attempts equals the number of maximim tantatives to avoid duplication of sending emails.
                      */
-                    if ($oRow->attempts == $iMaxAttempts)
-                        (new \PH7\Security)->sendAlertLoginAttemptsExceeded($iMaxAttempts, $iAttemptTime, $this->_sIp, $sEmail, $oView, $sTable);
+                    if ($oRow->attempts == $iMaxAttempts) {
+                        (new SecurityCore)->sendAlertLoginAttemptsExceeded($iMaxAttempts, $iAttemptTime, $this->sIp, $sEmail, $oView, $sTable);
+                    }
                 } else {
                     // Clear Login Attempts
                     $this->clearLoginAttempts($sTable);
@@ -132,21 +146,21 @@ class Security
         Various::checkModelTable($sTable);
 
         $rStmt = Db::getInstance()->prepare('SELECT * FROM' . Db::prefix($sTable . 'AttemptsLogin') . 'WHERE ip = :ip LIMIT 1');
-        $rStmt->bindValue(':ip', $this->_sIp, \PDO::PARAM_STR);
+        $rStmt->bindValue(':ip', $this->sIp, PDO::PARAM_STR);
         $rStmt->execute();
 
         if ($rStmt->rowCount() == 1) {
-            $oRow = $rStmt->fetch(\PDO::FETCH_OBJ);
+            $oRow = $rStmt->fetch(PDO::FETCH_OBJ);
             $iAttempts = $oRow->attempts + 1;
             $rStmt = Db::getInstance()->prepare('UPDATE' . Db::prefix($sTable . 'AttemptsLogin') . 'SET attempts = :attempts, lastLogin = :currentTime WHERE ip = :ip');
-            $rStmt->bindValue(':ip', $this->_sIp, \PDO::PARAM_STR);
-            $rStmt->bindValue(':attempts', $iAttempts, \PDO::PARAM_INT);
-            $rStmt->bindValue(':currentTime', $this->_sCurrentTime, \PDO::PARAM_STR);
+            $rStmt->bindValue(':ip', $this->sIp, PDO::PARAM_STR);
+            $rStmt->bindValue(':attempts', $iAttempts, PDO::PARAM_INT);
+            $rStmt->bindValue(':currentTime', $this->sCurrentTime, PDO::PARAM_STR);
             $rStmt->execute();
         } else {
             $rStmt = Db::getInstance()->prepare('INSERT INTO' . Db::prefix($sTable . 'AttemptsLogin') . '(ip, attempts, lastLogin) VALUES (:ip, 1, :lastLogin)');
-            $rStmt->bindValue(':ip', $this->_sIp, \PDO::PARAM_STR);
-            $rStmt->bindValue(':lastLogin', $this->_sCurrentTime, \PDO::PARAM_STR);
+            $rStmt->bindValue(':ip', $this->sIp, PDO::PARAM_STR);
+            $rStmt->bindValue(':lastLogin', $this->sCurrentTime, PDO::PARAM_STR);
             $rStmt->execute();
         }
 
@@ -165,7 +179,7 @@ class Security
         Various::checkModelTable($sTable);
 
         $rStmt = Db::getInstance()->prepare('DELETE FROM' . Db::prefix($sTable . 'AttemptsLogin') . 'WHERE ip = :ip');
-        $rStmt->bindValue(':ip', $this->_sIp, \PDO::PARAM_STR);
+        $rStmt->bindValue(':ip', $this->sIp, PDO::PARAM_STR);
         $rStmt->execute();
         Db::free($rStmt);
     }
