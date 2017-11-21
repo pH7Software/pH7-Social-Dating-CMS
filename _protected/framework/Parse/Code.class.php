dfa5ce71dffd6121b1a481fd76b8b188d331c9cd
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-21 00:14:31 -0800

    Store regex pattern into constant

--- _protected/framework/Parse/Code.class.php
+++ _protected/framework/Parse/Code.class.php
@@ -16,9 +16,9 @@ defined('PH7') or exit('Restricted access');
 
 abstract class Code
 {
-    /**
-     * @var string $sText
-     */
+    const REGEX_SCRIPT_BLOCK_PATTERN = '/<script(.*?)>(.*?)<\/script>/is';
+
+    /** @var string */
     protected $sText;
 
     public function __construct()
@@ -33,7 +33,7 @@ abstract class Code
      */
     protected function sanitize()
     {
-        $this->sText = preg_replace('/<script(.*?)>(.*?)<\/script>/is', '', $this->sText);
+        $this->sText = preg_replace(static::REGEX_SCRIPT_BLOCK_PATTERN, '', $this->sText);
         $this->convert();
     }
 
