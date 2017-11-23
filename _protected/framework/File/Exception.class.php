8cdc0a5a5942f9b7039c63e2ef46ca4759d991f9
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-22 23:54:09 -0800

    Import namespace

--- _protected/framework/File/Exception.class.php
+++ _protected/framework/File/Exception.class.php
@@ -8,6 +8,8 @@
 
 namespace PH7\Framework\File;
 
-class Exception extends \PH7\Framework\Error\CException\PH7Exception
+use PH7\Framework\Error\CException\PH7Exception;
+
+class Exception extends PH7Exception
 {
 }
