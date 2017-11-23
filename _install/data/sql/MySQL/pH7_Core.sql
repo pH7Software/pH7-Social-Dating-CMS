2371c29ff412331b3ac7dbdf2f9431b9c3cf3d2a
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-23 14:13:15 -0800

    Specify full package name

--- _install/data/sql/MySQL/pH7_Core.sql
+++ _install/data/sql/MySQL/pH7_Core.sql
@@ -5,7 +5,7 @@
 -- Author:        Pierre-Henry Soria <hello@ph7cms.com>
 -- Copyright:     (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
 -- License:       GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
--- Package:       PH7 / Install / Data / Sql
+-- Package:       PH7 / Install / Data / Sql / MySQL
 --
 --
 
