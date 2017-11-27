4af37543cde8c5bf0b4f58fddc4eddcdbe87558c
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-27 13:08:09 -0800

    Avoid spam contents by checking the number of urls/emails

--- _tests/Unit/Framework/Security/Spam/SpamTest.php
+++ _tests/Unit/Framework/Security/Spam/SpamTest.php
@@ -28,4 +28,32 @@ class SpamTest extends PHPUnit_Framework_TestCase
 
         $this->assertTrue(Spam::detectDuplicate($sText1, $sText2));
     }
+
+    public function testTooManyLinksTest()
+    {
+        $sText = 'Hi, can you go to http://affiliate-link.com this website http://affiliate-link.com/?ref=4933 is so good!';
+
+        $this->assertTrue(Spam::areUrls($sText, 1));
+    }
+
+    public function testCorrectAmountOfLinksTest()
+    {
+        $sText = 'I am nice. I just share one great website because I think it is a great resource: https://wikipedia.com ;)';
+
+        $this->assertFalse(Spam::areUrls($sText, 1));
+    }
+
+    public function testCannotHaveEmails()
+    {
+        $sText = 'Hi! Please email me eva.nice@mail.com!';
+
+        $this->assertTrue(Spam::areEmails($sText, 0));
+    }
+
+    public function testCorrectAmountOfEmails()
+    {
+        $sText = 'Hi! Please email me eva.nice@mail.com!';
+
+        $this->assertFalse(Spam::areEmails($sText, 1));
+    }
 }
