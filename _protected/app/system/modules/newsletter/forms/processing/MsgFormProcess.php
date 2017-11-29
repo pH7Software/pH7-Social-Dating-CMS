e1e9bafb9dd2b3a61fcf4de30bc900ba56710f1b
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-11-28 19:15:59 -0700

    Minor Indentation fix (make length line shorter)
    And fix wrong phpdoc type name

--- _protected/app/system/modules/newsletter/forms/processing/MsgFormProcess.php
+++ _protected/app/system/modules/newsletter/forms/processing/MsgFormProcess.php
@@ -19,7 +19,11 @@ class MsgFormProcess
         if (!$aData['status']) {
             \PFBC\Form::setError('form_msg', Form::errorSendingEmail());
         } else {
-            \PFBC\Form::setSuccess('form_msg', nt('%n% newsletter has been successfully sent', '%n% newsletters were successfully sent!', $aData['nb_mail_sent']));
+            \PFBC\Form::setSuccess(
+                'form_msg',
+                nt('%n% newsletter has been successfully sent', '%n% newsletters were successfully sent!',
+                    $aData['nb_mail_sent'])
+            );
         }
     }
 }
