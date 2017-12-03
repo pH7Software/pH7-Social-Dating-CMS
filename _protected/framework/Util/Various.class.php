1949ef52e2211c34b159d56356427e87db239ae9
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-12-03 11:38:55 -0800

    Minor code cleanup + remove redundant things..

--- _protected/framework/Util/Various.class.php
+++ _protected/framework/Util/Various.class.php
@@ -33,7 +33,7 @@ class Various
     public static function genRnd($sStr = null, $iLength = self::MAX_LENGTH)
     {
         $sPrefix = (string)mt_rand();
-        $sStr = (!empty($sStr)) ? (string)$sStr : '';
+        $sStr = !empty($sStr) ? (string)$sStr : '';
         $sChars = hash('whirlpool', hash('whirlpool', uniqid($sPrefix, true) . $sStr . Ip::get() . time()) . hash('sha512', (new Browser)->getUserAgent() . microtime(true) * 9999));
 
         return self::padStr($sChars, $iLength);
