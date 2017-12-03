5a64e421c1c7b4838c1470decc872de4e772362e
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-12-02 21:16:10 -0800

    Remove extra blank lines + update contributing comment

--- _protected/framework/Search/Search.class.php
+++ _protected/framework/Search/Search.class.php
@@ -10,21 +10,20 @@
  */
 
 namespace PH7\Framework\Search;
+
 defined('PH7') or exit('Restricted access');
 
 /**
- * This class is still under development, if you are a developer and you want to help us and join our volunteer team of developers to continue development of this class, you're welcome!
- * Please use our open source Github repo: https://github.com/pH7Software/pH7-Social-Dating-CMS
+ * This class is still under development. If you are a developer and you want to join me working on pH7CMS
+ * Please use the open source Github repo: https://github.com/pH7Software/pH7-Social-Dating-CMS
  *
- * Thank you,
- * pH7 developers team.
+ * Thank you!
+ * Pierre-Henry Soria
  */
 
 class Search
 {
-
     public function __construct()
     {
     }
-
 }
