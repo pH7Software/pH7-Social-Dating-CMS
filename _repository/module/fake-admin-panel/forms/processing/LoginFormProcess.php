48165dc6c15a971f6c084d4fc52c5e4e3db1c3a9
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-12-07 00:30:54 -0800

    Remove version info from header comment + update email

--- _repository/module/fake-admin-panel/forms/processing/LoginFormProcess.php
+++ _repository/module/fake-admin-panel/forms/processing/LoginFormProcess.php
@@ -2,11 +2,10 @@
 /**
  * @title          Login Form Process
  *
- * @author         Pierre-Henry Soria <ph7software@gmail.com>
+ * @author         Pierre-Henry Soria <hi@ph7.me>
  * @copyright      (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License <http://www.gnu.org/licenses/gpl.html>
  * @package        PH7 / App / Module / Fake Admin Panel / Form / Processing
- * @version        1.1.0
  */
 
 namespace PH7;
--- _repository/module/fake-admin-panel/install/info/in_conclusion.php
+++ _repository/module/fake-admin-panel/install/info/in_conclusion.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @author         Pierre-Henry Soria <ph7software@gmail.com>
+ * @author         Pierre-Henry Soria <hi@ph7.me>
  * @copyright      (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License <http://www.gnu.org/licenses/gpl.html>
  * @package        PH7 / App / Module / Fake Admin Panel / Install / Info
