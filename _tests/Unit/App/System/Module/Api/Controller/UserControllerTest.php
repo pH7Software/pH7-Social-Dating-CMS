1a72317095381f6b116bff093f3ff2a8ab230403
Pierre-Henry Soria <hizup.com@hotmail.com>
2017-12-07 17:01:03 -0800

    PART1: Add tests for API module

---
+++ _tests/Unit/App/System/Module/Api/Controller/UserControllerTest.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author           Pierre-Henry Soria <hello@ph7cms.com>
+ * @copyright        (c) 2017-2018, Pierre-Henry Soria. All Rights Reserved.
+ * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package          PH7 / Test / Unit / App / System / Module / Api / Controller
+ */
+
+namespace PH7\Test\Unit\App\System\Module\Api\Controller;
+
+class UserControllerTest extends MainControllerTest
+{
+    protected function setUp()
+    {
+        parent::setUp();
+    }
+
+    /**
+     * Has still to be implemented and get the DB fixture setup.
+     * Feel free to contribute -> https://github.com/pH7Software/pH7-Social-Dating-CMS
+     */
+}
