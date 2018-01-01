33c7e92da58ddcecfab179cc4510716bdf1016c8
Pierre-Henry Soria <hi@ph7.me>
2018-01-01 20:12:15 +0100

    Update Smarty version to 3.1.32-dev

--- _install/library/Smarty/Autoloader.php
+++ _install/library/Smarty/Autoloader.php
@@ -14,13 +14,13 @@
  *                  require_once '...path/Autoloader.php';
  *                  Smarty_Autoloader::register();
  *             or
- *                  include '...path/bootstarp.php';
+ *                  include '...path/bootstrap.php';
  *
  *                  $smarty = new Smarty();
  */
 class Smarty_Autoloader
 {
-    /**
+   /**
      * Filepath to Smarty root
      *
      * @var string
@@ -58,7 +58,7 @@ class Smarty_Autoloader
             set_include_path(get_include_path() . PATH_SEPARATOR . SMARTY_SYSPLUGINS_DIR) !== false
         ) {
             $registeredAutoLoadFunctions = spl_autoload_functions();
-            if (!isset($registeredAutoLoadFunctions['spl_autoload'])) {
+            if (!isset($registeredAutoLoadFunctions[ 'spl_autoload' ])) {
                 spl_autoload_register();
             }
         } else {
@@ -76,7 +76,7 @@ class Smarty_Autoloader
         self::$SMARTY_DIR = defined('SMARTY_DIR') ? SMARTY_DIR : dirname(__FILE__) . DIRECTORY_SEPARATOR;
         self::$SMARTY_SYSPLUGINS_DIR = defined('SMARTY_SYSPLUGINS_DIR') ? SMARTY_SYSPLUGINS_DIR :
             self::$SMARTY_DIR . 'sysplugins' . DIRECTORY_SEPARATOR;
-        if (version_compare(phpversion(), '5.3.0', '>=')) {
+        if (version_compare(PHP_VERSION, '5.3.0', '>=')) {
             spl_autoload_register(array(__CLASS__, 'autoload'), true, $prepend);
         } else {
             spl_autoload_register(array(__CLASS__, 'autoload'));
@@ -90,12 +90,12 @@ class Smarty_Autoloader
      */
     public static function autoload($class)
     {
-        if ($class[0] !== 'S' && strpos($class, 'Smarty') !== 0) {
+        if ($class[ 0 ] !== 'S' && strpos($class, 'Smarty') !== 0) {
             return;
         }
         $_class = strtolower($class);
-        if (isset(self::$rootClasses[$_class])) {
-            $file = self::$SMARTY_DIR . self::$rootClasses[$_class];
+        if (isset(self::$rootClasses[ $_class ])) {
+            $file = self::$SMARTY_DIR . self::$rootClasses[ $_class ];
             if (is_file($file)) {
                 include $file;
             }
