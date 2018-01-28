3e34c2c6005009b459eae341dc718fe8d35525af
Pierre-Henry Soria <hi@ph7.me>
2018-01-28 17:55:29 +0000

    Bumped SQL schema version

--- _install/data/sql/MySQL/pH7_Core.sql
+++ _install/data/sql/MySQL/pH7_Core.sql
@@ -963,7 +963,7 @@ CREATE TABLE IF NOT EXISTS pH7_Modules (
 
 INSERT INTO pH7_Modules (vendorName, moduleName, version, active) VALUES
 /* Gives the current version of the SQL schema of pH7CMS (this helps to update and shows whether it is necessary or not to update the database as well) */
-('pH7CMS', 'SQL System Schema', '1.4.1', 1);
+('pH7CMS', 'SQL System Schema', '1.4.2', 1);
 
 
 CREATE TABLE IF NOT EXISTS pH7_Report (
