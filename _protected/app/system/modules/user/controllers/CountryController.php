2a4c08082e301ea6ac456df69cb56520bd42c8a4
Pierre-Henry Soria <hi@ph7.me>
2018-01-29 15:33:18 +0000

    Store map zoom & size into constants

--- _protected/app/system/modules/user/controllers/CountryController.php
+++ _protected/app/system/modules/user/controllers/CountryController.php
@@ -16,6 +16,10 @@ use PH7\Framework\Navigation\Page;
 
 class CountryController extends Controller
 {
+    const MAP_ZOOM_LEVEL = 12;
+    const MAP_WIDTH_SIZE = '520px';
+    const MAP_HEIGHT_SIZE = '100%';
+
     const COUNTRY_CODE_LENGTH = 2;
     const MAX_PROFILE_PER_PAGE = 20;
     const MAX_COUNTRY_LENGTH = 50;
@@ -35,9 +39,9 @@ class CountryController extends Controller
             $oMap = new Map;
             $oMap->setKey(DbConfig::getSetting('googleApiKey'));
             $oMap->setCenter($this->registry->country . ' ' . $this->registry->city);
-            $oMap->setSize('100%', '520px');
+            $oMap->setSize(self::MAP_HEIGHT_SIZE, self::MAP_WIDTH_SIZE);
             $oMap->setDivId('country_map');
-            $oMap->setZoom(12);
+            $oMap->setZoom(self::MAP_ZOOM_LEVEL);
             $oMap->addMarkerByAddress($this->registry->country . ' ' . $this->registry->city, t('Meet new people here!'));
             $oMap->generate();
             $this->view->map = $oMap->getMap();
