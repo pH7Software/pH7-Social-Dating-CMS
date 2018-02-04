e437f83d55bc8bed41871e01348588cb51a379b3
Pierre-Henry Soria <hi@ph7.me>
2018-02-04 18:19:17 +0000

    Update phpdoc @package path

--- _protected/app/system/core/models/VisitorCoreModel.php
+++ _protected/app/system/core/models/VisitorCoreModel.php
@@ -5,7 +5,7 @@
  * @author         Pierre-Henry Soria <ph7software@gmail.com>
  * @copyright      (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
- * @package        PH7/ App / System / Module / User / Model
+ * @package        PH7 / App / System / Core / Model
  * @version        1.1
  */
 
