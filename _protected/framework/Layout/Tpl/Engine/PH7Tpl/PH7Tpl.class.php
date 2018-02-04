f54bb0e1bdfb6091b9c0c6ecb555da8898ee8e1f
Pierre-Henry Soria <hi@ph7.me>
2018-02-04 21:18:12 +0000

    Add missing @throws phpdoc tags

--- _protected/framework/Layout/Tpl/Engine/PH7Tpl/PH7Tpl.class.php
+++ _protected/framework/Layout/Tpl/Engine/PH7Tpl/PH7Tpl.class.php
@@ -694,6 +694,8 @@ class PH7Tpl extends Kernel
      *
      * @return void
      *
+     * @throws Exception
+     * @throws \PH7\Framework\File\Exception
      * @throws TplException If the cache file could not be written.
      */
     protected function cache()
