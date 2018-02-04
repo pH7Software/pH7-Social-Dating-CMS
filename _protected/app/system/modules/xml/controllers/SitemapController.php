889c78a115da55745b44f1d52eed3161403b3f5d
Pierre-Henry Soria <hi@ph7.me>
2018-02-04 19:02:01 +0000

    Don't cache sitemap index page since it causes issues when user logged in

--- _protected/app/system/modules/xml/controllers/SitemapController.php
+++ _protected/app/system/modules/xml/controllers/SitemapController.php
@@ -21,6 +21,8 @@ class SitemapController extends MainController implements XmlControllable
 
     public function index()
     {
+        $this->view->setCaching(true);
+
         $this->sTitle = t('Site Map');
         $this->view->page_title = $this->sTitle;
         $this->view->meta_description = t('Map of Website, Free Online Dating Site with Video Chat Rooms, Meet Single People with %site_name%');
