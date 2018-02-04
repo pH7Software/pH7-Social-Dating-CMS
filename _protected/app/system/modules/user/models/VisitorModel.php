b201b2c03bee517a8450297728564bde9509a21c
Pierre-Henry Soria <hi@ph7.me>
2018-02-04 18:22:38 +0000

    Put back local VisitorModel class in user module
    Since Visitor Controller is also part of the "user" module, it make more sense to have also a VisitorModel class in its own module.

---
+++ _protected/app/system/modules/user/models/VisitorModel.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @title          Visitor Model
+ *
+ * @author         Pierre-Henry Soria <ph7software@gmail.com>
+ * @copyright      (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package        PH7/ App / System / Module / User / Model
+ */
+
+
+namespace PH7;
+
+class VisitorModel extends VisitorCoreModel
+{
+}
