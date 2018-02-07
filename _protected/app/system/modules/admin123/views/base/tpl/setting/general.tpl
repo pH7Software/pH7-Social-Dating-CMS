pH7Software/pH7-Social-Dating-CMS

commit 8e98a8d0b6bdee6b58a5f622442b409bd4095d8a

Cleaned up code + minor bug fixed
4 spaces indentation + add curly brackets

_protected/app/system/modules/admin123/views/base/tpl/setting/general.tpl
@@ -1,21 +1,23 @@
 <ol id="toc">
-  <li><a href="#general"><span>{lang 'General Settings'}</span></a></li>
-  <li><a href="#logotype"><span>{lang 'Logo'}</span></a></li>
-  <li><a href="#registration"><span>{lang 'Registration'}</span></a></li>
-  <li><a href="#pic_vid"><span>{lang 'Picture and Video'}</span></a></li>
-  <li><a href="#moderation"><span>{lang 'Moderation'}</span></a></li>
-  <li><a href="#email"><span>{lang 'Email'}</span></a></li>
-  <li><a href="#security"><span>{lang 'Security'}</span></a></li>
-  <li><a href="#spam"><span>{lang 'Spam'}</span></a></li>
-  <li><a href="#api"><span>{lang 'API'}</span></a></li>
-  <li><a href="#automation"><span>{lang 'Automation'}</span></a></li>
+    <li><a href="#general"><span>{lang 'General Settings'}</span></a></li>
+    <li><a href="#logotype"><span>{lang 'Logo'}</span></a></li>
+    <li><a href="#registration"><span>{lang 'Registration'}</span></a></li>
+    <li><a href="#pic_vid"><span>{lang 'Picture and Video'}</span></a></li>
+    <li><a href="#moderation"><span>{lang 'Moderation'}</span></a></li>
+    <li><a href="#email"><span>{lang 'Email'}</span></a></li>
+    <li><a href="#security"><span>{lang 'Security'}</span></a></li>
+    <li><a href="#spam"><span>{lang 'Spam'}</span></a></li>
+    <li><a href="#api"><span>{lang 'API'}</span></a></li>
+    <li><a href="#automation"><span>{lang 'Automation'}</span></a></li>
 </ol>
 {{ SettingForm::display() }}
 
 <script>
-  /* Check if the Setting page is loading from 'p=registration'
-     If so, scroll down to show the "Default Membership Group" first (this is used by the Payment module) */
-  var sHash = location.hash.substr(1);
-  if (sHash == 'p=registration')
-    var $target=$('html,body');$target.animate({scrollTop:$target.height()},1000);
+    /* Check if the Setting page is loading from 'p=registration'
+     * If so, scroll down to show the "Default Membership Group" first (this is used by the Payment module) */
+    var sHash = location.hash.substr(1);
+    if (sHash == 'p=registration') {
+        var $target = $('body');
+        $target.animate({scrollTop:$target.height()}, 1000);
+    }
 </script>

commit 6910eca7a364a7e7bbc25e60126f216c0b050e07

[Bug Fixed] Scroll Downer JS code was broker. "html" element was missing
Caused by https://github.com/pH7Software/pH7-Social-Dating-CMS/commit/8e98a8d0b6bdee6b58a5f622442b409bd4095d8a#diff-0aaf793e3fb1a2121a06c68de7806fb9 commit

_protected/app/system/modules/admin123/views/base/tpl/setting/general.tpl
@@ -17,7 +17,7 @@
      * If so, scroll down to show the "Default Membership Group" first (this is used by the Payment module) */
     var sHash = location.hash.substr(1);
     if (sHash == 'p=registration') {
-        var $target = $('body');
-        $target.animate({scrollTop:$target.height()}, 1000);
+        var $target = $('html, body');
+        $target.animate({scrollTop: $target.height()}, 1000);
     }
 </script>