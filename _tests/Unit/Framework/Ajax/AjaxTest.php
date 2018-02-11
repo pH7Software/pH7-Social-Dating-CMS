dbdc6bffb0204ea2388f9cced36b5735372539e5
Pierre-Henry Soria <hi@ph7.me>
2018-02-11 23:26:16 +0000

    Import namespace

--- _tests/Unit/Framework/Ajax/AjaxTest.php
+++ _tests/Unit/Framework/Ajax/AjaxTest.php
@@ -9,8 +9,9 @@
 namespace PH7\Test\Unit\Framework\Ajax;
 
 use PH7\Framework\Ajax\Ajax;
+use PHPUnit_Framework_TestCase;
 
-class AjaxTest extends \PHPUnit_Framework_TestCase
+class AjaxTest extends PHPUnit_Framework_TestCase
 {
     public function testJsonMsg()
     {
