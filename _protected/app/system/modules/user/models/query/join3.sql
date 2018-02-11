fc07ee71cfd35e3a6c7e49a7e01375ba46647153
Pierre-Henry Soria <hi@ph7.me>
2018-02-11 13:20:39 +0000

    Update table names
    (forgot to update them the the new DB table names!)
    
    More details:
    https://github.com/pH7Software/pH7-Social-Dating-CMS/pull/170
    https://github.com/pH7Software/pH7-Social-Dating-CMS/pull/167

--- _protected/app/system/modules/user/models/query/join3.sql
+++ _protected/app/system/modules/user/models/query/join3.sql
@@ -1,1 +1,1 @@
-UPDATE [DB_PREFIX]MembersInfo SET description = :description WHERE profileId = :profile_id LIMIT 1;
+UPDATE [DB_PREFIX]members_info SET description = :description WHERE profileId = :profile_id LIMIT 1;
