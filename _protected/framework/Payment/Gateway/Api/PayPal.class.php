74bd1c09bebb1dae2ae99279a7f8aa517d2092c8
Pierre-Henry Soria <hi@ph7.me>
2018-02-13 20:54:48 +0000

    Bumped class version

--- _protected/framework/Payment/Gateway/Api/PayPal.class.php
+++ _protected/framework/Payment/Gateway/Api/PayPal.class.php
@@ -6,7 +6,7 @@
  * @copyright        (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / Payment / Gateway / Api
- * @version          1.1
+ * @version          1.3
  */
 
 namespace PH7\Framework\Payment\Gateway\Api;
