26e5e6561bee63ee5590183f63b5232a0b641af3
Pierre-Henry Soria <hi@ph7.me>
2018-02-17 18:36:35 +0100

    Catch exception & show it as a form msg when email address is wrong

--- _protected/app/system/modules/newsletter/forms/processing/MsgFormProcess.php
+++ _protected/app/system/modules/newsletter/forms/processing/MsgFormProcess.php
@@ -8,13 +8,19 @@
 
 namespace PH7;
 
+use Swift_RfcComplianceException;
+
 defined('PH7') or exit('Restricted access');
 
 class MsgFormProcess
 {
     public function __construct()
     {
-        $aData = (new Newsletter)->sendMessages();
+        try {
+            $aData = (new Newsletter)->sendMessages();
+        } catch (Swift_RfcComplianceException $oE) {
+            \PFBC\Form::setError('form_msg', $oE->getMessage());
+        }
 
         if (!$aData['status']) {
             \PFBC\Form::setError('form_msg', Form::errorSendingEmail());
