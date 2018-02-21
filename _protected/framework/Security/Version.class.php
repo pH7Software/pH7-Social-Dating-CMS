d3c344999eb5361cd835a91cb37224ed2e271471
Pierre-Henry Soria <hi@ph7.me>
2018-02-21 20:26:00 +0000

    Bumped to next version :fire:

--- _protected/framework/Security/Version.class.php
+++ _protected/framework/Security/Version.class.php
@@ -31,9 +31,9 @@ final class Version
      * 5.* was "pCO", 6.* was "WoW", 7.*, 8.* were "NaOH", 10.* was "pKa" and 12.* is "PHS"
      */
     const KERNEL_VERSION_NAME = 'PHS';
-    const KERNEL_VERSION = '12.0.0';
+    const KERNEL_VERSION = '12.1.0';
     const KERNEL_BUILD = '1';
-    const KERNEL_RELASE_DATE = '2018-03-01';
+    const KERNEL_RELASE_DATE = '2018-03-17';
 
     /***** Framework Server *****/
     const KERNEL_TECHNOLOGY_NAME = 'pH7T/1.0.1'; // Ph7 Technology
