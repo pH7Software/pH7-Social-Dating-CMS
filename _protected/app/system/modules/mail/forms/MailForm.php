6a9226468c90b57a35975e5891420551e3e69c54
Pierre-Henry Soria <hi@ph7.me>
2018-03-04 14:39:06 +0000

    [Readability] Removed ternary conditional operator inside of array

--- _protected/app/system/modules/mail/forms/MailForm.php
+++ _protected/app/system/modules/mail/forms/MailForm.php
@@ -26,12 +26,17 @@ class MailForm
 
         $oHttpRequest = new HttpRequest; // For Reply Function
 
+        $sSubjectValue = '';
+        if ($oHttpRequest->getExists('title')) {
+            $sSubjectValue = t('RE: ') . str_replace('-', ' ', $oHttpRequest->get('title'));
+        }
+
         $oForm = new \PFBC\Form('form_compose_mail');
         $oForm->configure(array('action' => ''));
         $oForm->addElement(new \PFBC\Element\Hidden('submit_compose_mail', 'form_compose_mail'));
         $oForm->addElement(new \PFBC\Element\Token('compose_mail'));
-        $oForm->addElement(new \PFBC\Element\Textbox(t('Recipient:'), 'recipient', array('id' => 'recipient', 'value' => $oHttpRequest->get('recipient'), 'required' => 1)));
-        $oForm->addElement(new \PFBC\Element\Textbox(t('Subject:'), 'title', array('id' => 'str_title', 'onblur' => 'CValid(this.value,this.id,2,60)', 'value' => ($oHttpRequest->get('title') != '') ? t('RE: ') . str_replace('-', ' ', $oHttpRequest->get('title')) : '', 'validation' => new \PFBC\Validation\Str(2, 60), 'required' => 1)));
+        $oForm->addElement(new \PFBC\Element\Textbox(t('Recipient:'), 'recipient', ['id' => 'recipient', 'value' => $oHttpRequest->get('recipient'), 'required' => 1]));
+        $oForm->addElement(new \PFBC\Element\Textbox(t('Subject:'), 'title', ['id' => 'str_title', 'onblur' => 'CValid(this.value,this.id,2,60)', 'value' => $sSubjectValue, 'validation' => new \PFBC\Validation\Str(2, 60), 'required' => 1]));
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<span class="input_error str_title"></span>'));
         $oForm->addElement(new \PFBC\Element\Textarea(t('Your message:'), 'message', array('id' => 'str_msg', 'onblur' => 'CValid(this.value,this.id,2,2500)', 'value' => $oHttpRequest->get('message'), 'validation' => new \PFBC\Validation\Str(2, 2500), 'basic' => 1, 'required' => 1)));
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<span class="input_error str_msg"></span>'));
