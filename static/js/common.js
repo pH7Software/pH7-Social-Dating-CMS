00a8f50fc8a4dd6522484d89496565f9d944d980
Pierre-Henry Soria <hi@ph7.me>
2018-03-12 19:50:31 +0000

    Remove Yoda Condition since it is not really readable

--- static/js/common.js
+++ static/js/common.js
@@ -61,15 +61,17 @@ function pH7Include(mFile) {
 (function () {
     $('a').click(function () {
         var href = $(this).attr('href');
+
         if (
-            -1 == href.indexOf('ph7cms.com') && -1 == href.indexOf('youtube.com') &&
-            -1 == href.indexOf('youtu.be') && -1 == href.indexOf('vimeo.com') &&
-            -1 == href.indexOf('dailymotion.com') && -1 == href.indexOf('metacafe.com') &&
-            -1 == href.indexOf('gravatar.com') && -1 == href.indexOf('softaculous.com') &&
-            (-1 != href.indexOf('http://') || -1 != href.indexOf('https://'))
+            href.indexOf('ph7cms.com') == -1 && href.indexOf('youtube.com') == -1 &&
+            href.indexOf('youtu.be') == -1 && href.indexOf('vimeo.com') == -1 &&
+            href.indexOf('dailymotion.com') == -1 && href.indexOf('metacafe.com') == -1 &&
+            href.indexOf('gravatar.com') == -1 && href.indexOf('softaculous.com') == -1 &&
+            (href.indexOf('http://') != -1 || href.indexOf('https://') != -1)
+
         ) {
             var host = href.substr(href.indexOf(':') + 3);
-            if (-1 != host.indexOf('/')) {
+            if (host.indexOf('/') != -1) {
                 host = host.substring(0, host.indexOf('/'));
             }
             if (host != window.location.host) {
