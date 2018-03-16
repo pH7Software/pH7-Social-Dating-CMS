4cb55c9d49852fa22219fbe5ae4b637484c9d19d
Pierre-Henry Soria <hi@ph7.me>
2018-03-16 22:47:10 +0100

    Make sure $iProfileId is set
    To avoid Invalid parameter number: number of bound variables does not match number of tokens

--- _protected/app/system/modules/mail/models/MailModel.php
+++ _protected/app/system/modules/mail/models/MailModel.php
@@ -234,15 +234,15 @@ class MailModel extends MailCoreModel
         $sSqlOrder = SearchCoreModel::order($sOrderBy, $iSort);
 
         switch ($sType) {
-            case self::INBOX:
+            case self::INBOX && !empty($iProfileId):
                 $sSql = 'msg.sender = m.profileId WHERE (msg.recipient = :profileId) AND (NOT FIND_IN_SET(\'recipient\', msg.trash)) AND';
                 break;
 
-            case self::OUTBOX:
+            case self::OUTBOX && !empty($iProfileId):
                 $sSql = 'msg.recipient = m.profileId WHERE (msg.sender = :profileId) AND (NOT FIND_IN_SET(\'sender\', msg.toDelete)) AND';
                 break;
 
-            case self::TRASH:
+            case self::TRASH && !empty($iProfileId):
                 $sSql = 'msg.sender = m.profileId WHERE (msg.recipient = :profileId) AND (FIND_IN_SET(\'recipient\', msg.trash)) AND
                 (NOT FIND_IN_SET(\'recipient\', msg.toDelete)) AND';
                 break;
