049daa5d102a057b2df309938e65ac6a58941cce
Pierre-Henry Soria <hi@ph7.me>
2018-04-22 10:54:46 +0100

    Remove unnecessary slash

--- _protected/framework/Core/Kernel.class.php
+++ _protected/framework/Core/Kernel.class.php
@@ -36,7 +36,7 @@ abstract class Kernel
     const SOFTWARE_LICENSE_KEY_URL = 'http://ph7cms.com/memberships';
     const SOFTWARE_DOC_URL = 'http://ph7cms.com/doc';
     const SOFTWARE_GIT_REPO = 'https://github.com/pH7Software/pH7-Social-Dating-CMS';
-    const SOFTWARE_ISSUE_URL = self::SOFTWARE_GIT_REPO . '/issues/';
+    const SOFTWARE_ISSUE_URL = self::SOFTWARE_GIT_REPO . '/issues';
     const SOFTWARE_EMAIL = 'hello@ph7cms.com';
     const SOFTWARE_TWITTER = '@pH7Soft';
     const SOFTWARE_AUTHOR = 'Pierre-Henry Soria';
