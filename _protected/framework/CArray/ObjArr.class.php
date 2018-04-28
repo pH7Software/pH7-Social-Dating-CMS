a5fcd115957853fc3ede696f4948d541bf257f9a
Pierre-Henry Soria <hi@ph7.me>
2018-04-28 08:45:04 +0100

    Remove useless @version detail in class header comment

--- _protected/framework/CArray/ObjArr.class.php
+++ _protected/framework/CArray/ObjArr.class.php
@@ -7,7 +7,6 @@
  * @copyright        (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / CArray
- * @version          1.0
  */
 
 namespace PH7\Framework\CArray;
