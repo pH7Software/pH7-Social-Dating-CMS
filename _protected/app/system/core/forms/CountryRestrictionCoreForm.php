560f8c723f5af04caf8d7dc9cc83db16eb2202c8
Pierre-Henry Soria <hi@ph7.me>
2018-05-26 14:33:23 +0200

    Change description with Country Blocker tool name

--- _protected/app/system/core/forms/CountryRestrictionCoreForm.php
+++ _protected/app/system/core/forms/CountryRestrictionCoreForm.php
@@ -32,7 +32,7 @@ class CountryRestrictionCoreForm
                 t('Countries to be showed on registration forms'),
                 'countries[]',
                 [
-                    'description' => t('You can limit the amount of countries to be displayed on the registration form and user search forms.<br /> If you need to block your entire website to be accessible from a country, please <a href="%0%">go here</a>.', Uri::get(PH7_ADMIN_MOD, 'tool', 'blockcountry')),
+                    'description' => t('You can limit the amount of countries to be displayed on the registration form and user search forms.<br /> If you need to block your entire website to be accessible from a country, please use <a href="%0%">Country Blocker</a> tool.', Uri::get(PH7_ADMIN_MOD, 'tool', 'blockcountry')),
                     'multiple' => 'multiple',
                     'size' => 20,
                     'value' => self::getSelectedCountries($sTable)
