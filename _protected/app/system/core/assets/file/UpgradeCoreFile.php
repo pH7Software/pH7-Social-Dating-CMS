5dee2f35e75c427a3804cde00917ed3de8461bca
Pierre-Henry Soria <hi@ph7.me>
2018-05-28 21:02:24 +0200

    Bump file revision version

--- _protected/app/system/core/assets/file/UpgradeCoreFile.php
+++ _protected/app/system/core/assets/file/UpgradeCoreFile.php
@@ -7,7 +7,7 @@
  * @copyright        (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / App / System / Core / Asset / File
- * @version          1.5
+ * @version          1.8
  */
 
 namespace PH7;
