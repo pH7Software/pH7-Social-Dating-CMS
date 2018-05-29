pH7Software/pH7-Social-Dating-CMS

commit ea381ef8b202c774d79efc64de83d6a255c642f9

[Readability|PSR-2] Rename variable ($sF -> $sFile) + Add curly brackets

_protected/framework/File/File.class.php
@@ -244,20 +244,21 @@ class File
         $sDir = $this->checkExtDir($sDir);
 
         if (is_dir($sDir) && $rHandle = opendir($sDir)) {
-            while (false !== ($sF = readdir($rHandle))) {
-                if ($sF !== '.' && $sF !== '..') {
-                    if (is_dir($sDir . $sF)) {
+            while (false !== ($sFile = readdir($rHandle))) {
+                if ($sFile !== '.' && $sFile !== '..') {
+                    if (is_dir($sDir . $sFile)) {
                         $aTree = array_merge($aTree, $this->getFileList($sDir . $sF, $mExt));
                     } else {
                         if (!empty($mExt)) {
                             $aExt = (array)$mExt;
 
                             foreach ($aExt as $sExt) {
-                                if (substr($sF, -strlen($sExt)) === $sExt)
-                                    $aTree[] = $sDir . $sF;
+                                if (substr($sFile, -strlen($sExt)) === $sExt) {
+                                    $aTree[] = $sDir . $sFile;
+                                }
                             }
                         } else {
-                            $aTree[] = $sDir . $sF;
+                            $aTree[] = $sDir . $sFile;
                         }
                     }
                 }

commit 08c3e6924dae340057de9b33e214ba3e34536f63

:bug: Fix undefined variable
Was caused by ea381ef8b202c774d79efc64de83d6a255c642f9 commit
https://github.com/pH7Software/pH7-Social-Dating-CMS/commit/ea381ef8b202c774d79efc64de83d6a255c642f9#diff-0d0912856fb7d78536cc93d046b31b8f

_protected/framework/File/File.class.php
@@ -247,7 +247,7 @@ class File
             while (false !== ($sFile = readdir($rHandle))) {
                 if ($sFile !== '.' && $sFile !== '..') {
                     if (is_dir($sDir . $sFile)) {
-                        $aTree = array_merge($aTree, $this->getFileList($sDir . $sF, $mExt));
+                        $aTree = array_merge($aTree, $this->getFileList($sDir . $sFile, $mExt));
                     } else {
                         if (!empty($mExt)) {
                             $aExt = (array)$mExt;