c942d0567572edf0293b0b4154d27e92edcdc72b
Pierre-Henry Soria <hi@ph7.me>
2018-06-29 14:11:08 +0200

    Change include to require since make more sense for that file
    Since ajax script wont work if fns/misc.php file isn't found.

--- _install/inc/ajax/_ajax.inc.php
+++ _install/inc/ajax/_ajax.inc.php
@@ -11,7 +11,7 @@ namespace PH7;
 defined('PH7') or exit('Restricted access');
 
 require_once dirname(dirname(__DIR__)) . '/constants.php';
-include_once PH7_ROOT_INSTALL . 'inc/fns/misc.php';
+require_once PH7_ROOT_INSTALL . 'inc/fns/misc.php';
 require_once PH7_ROOT_INSTALL . 'library/Controllable.class.php';
 require_once PH7_ROOT_INSTALL . 'library/Controller.class.php';
 require_once PH7_ROOT_INSTALL . 'library/Language.class.php';
