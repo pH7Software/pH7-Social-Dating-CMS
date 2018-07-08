6aab7d63c165e41bc7f09337634916211627cd0a
Pierre-Henry Soria <hi@ph7.me>
2018-07-09 00:41:47 +0200

    [pH7Tpl] Fix issues with TAL alternative language

--- _protected/framework/Layout/Tpl/Engine/PH7Tpl/Syntax/Tal.class.php
+++ _protected/framework/Layout/Tpl/Engine/PH7Tpl/Syntax/Tal.class.php
@@ -47,44 +47,44 @@ class Tal extends Syntax
             $this->sCode
         );
 
-        /***** <?php echo *****/
+        /***** <?php echo ?> *****/
         $this->sCode = preg_replace(
-            '#<ph:print value=(?:"|\')(.+)(?:"|\') ?/?>#',
-            '<?php echo ',
+            '#<ph:print value=([^\<\>/\n]+) ?/?>#',
+            '<?php echo $1 ?>',
             $this->sCode
         );
 
         /***** if *****/
         $this->sCode = preg_replace(
-            '#<ph:if test=(?:"|\')([^\<\>"\n]+)(?:"|\')>#',
+            '#<ph:if test=(?:"|\')([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php if($1) { ?>',
             $this->sCode
         );
 
         /***** if isset *****/
         $this->sCode = preg_replace(
-            '#<ph:if-set test=(?:"|\')([^\<\>"\n]+)(?:"|\')>#',
+            '#<ph:if-set test=(?:"|\')([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php if(!empty($1)) { ?>',
             $this->sCode
         );
 
         /***** if empty *****/
         $this->sCode = preg_replace(
-            '#<ph:if-empty test=(?:"|\')([^\<\>"\n]+)(?:"|\')>#',
+            '#<ph:if-empty test=(?:"|\')([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php if(empty($1)) { ?>',
             $this->sCode
         );
 
         /***** if equal *****/
         $this->sCode = preg_replace(
-            '#<ph:if-equal test=(?:"|\')([^\{\},"\n]+)(?:"|\'),(?:"|\')([^\{\},"\n]+)(?:"|\')>#',
+            '#<ph:if-equal test=(?:"|\')([^\<\>,"\'\n]+)(?:"|\'),(?:"|\')([^\<\>,"\'\n]+)(?:"|\')>#',
             '<?php if($1 == $2) { ?>',
             $this->sCode
         );
 
         /***** elseif *****/
         $this->sCode = preg_replace(
-            '#<ph:else-if test=(?:"|\')([^\<\>"\n]+)(?:"|\')>#',
+            '#<ph:else-if test=(?:"|\')([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php elseif($1) { ?>',
             $this->sCode
         );
@@ -96,21 +96,21 @@ class Tal extends Syntax
         /*** Example ***/
         /* <ph:for test="$sData in $aData"> <p>Total items: <ph:print value="$sData_total" /><br /> Number: <ph:print value="$sData_i" /><br /> Name: <ph:print value="$sData" /></p> </ph:for> */
         $this->sCode = preg_replace(
-            '#<ph:for test=(?:"|\')([^\<\>"\n]+) in ([^\<\>"\n]+)(?:"|\')>#',
+            '#<ph:for test=(?:"|\')([^\<\>"\'\n]+) in ([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php for($1_i=0,$1_total=count($2);$1_i<$1_total;$1_i++) { $1=$2[$1_i]; ?>',
             $this->sCode
         );
 
         /***** while *****/
         $this->sCode = preg_replace(
-            '#<ph:while test=(?:"|\')([^\<\>"\n]+)(?:"|\')>#',
+            '#<ph:while test=(?:"|\')([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php while($1) { ?>',
             $this->sCode
         );
 
         /***** each (foreach) *****/
         $this->sCode = preg_replace(
-            '#<ph:each test=(?:"|\')([^\<\>"\n]+) in ([^\<\>"\n]+)(?:"|\')>#',
+            '#<ph:each test=(?:"|\')([^\<\>"\'\n]+) in ([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php foreach($2 as $1) { ?>',
             $this->sCode
         );
@@ -124,19 +124,19 @@ class Tal extends Syntax
 
         /***** Escape (htmlspecialchars) *****/
         $this->sCode = preg_replace(
-            '#<ph:escape value=(?:"|\')([^\{\}]+)(?:"|\') ?/?>#',
+            '#<ph:escape value=([^\<\>/\n]+) ?/?>#',
             '<?php this->str->escape($1); ?>',
             $this->sCode
         );
 
         /***** Translate (Gettext) *****/
         $this->sCode = preg_replace(
-            '#<ph:lang value=(?:"|\')([^\{\}]+)(?:"|\') ?/?>#',
+            '#<ph:lang value=([^\<\>/\n]+) ?/?>#',
             '<?php echo t($1); ?>',
             $this->sCode
         );
         $this->sCode = preg_replace(
-            '#<ph:lang>([^\{\}]+)</ph:lang>#',
+            '#<ph:lang>([^\<\>/\n]+)</ph:lang>#',
             '<?php echo t(\'$1\'); ?>',
             $this->sCode
         );
