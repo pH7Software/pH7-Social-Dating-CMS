7001e88b20c6d5e0c45de8300478b9f973f65a98
Pierre-Henry Soria <hi@ph7.me>
2018-07-13 22:48:51 +0200

    [pH7Tpl:Syntax] Refactor TAL class, by splitting parsing code into small methods

--- _protected/framework/Layout/Tpl/Engine/PH7Tpl/Syntax/Tal.class.php
+++ _protected/framework/Layout/Tpl/Engine/PH7Tpl/Syntax/Tal.class.php
@@ -16,7 +16,7 @@ use PH7\Framework\Layout\Tpl\Engine\PH7Tpl\PH7Tpl;
 
 defined('PH7') or exit('Restricted access');
 
-class Tal extends Syntax
+class Tal extends Syntax implements Parsable
 {
     /**
      * Parse XHTML-style syntax.
@@ -32,6 +32,43 @@ class Tal extends Syntax
             );
         }
 
+        $this->includeStatements();
+
+        $this->phpOpeningTag();
+        $this->phpClosingTag();
+        $this->phpOpeningTagWithEchoFunction();
+        $this->phpCode();
+
+        $this->ifStatement();
+        $this->ifSetStatement();
+        $this->ifEmptyStatement();
+        $this->ifEqualStatement();
+
+        $this->elseStatement();
+        $this->elseifStatement();
+
+        /***** Loops *****/
+        $this->forLoopStatement();
+        $this->whileLoopStatement();
+        $this->eachLoopStatement();
+
+        $this->closingBlockStructures();
+
+        $this->designModelFunction();
+
+        $this->escapeFunction();
+
+        $this->langFunctions();
+
+        $this->literalFunction();
+
+        $this->variable();
+
+        $this->clearComment();
+    }
+
+    public function includeStatements()
+    {
         /***** Includes *****/
         $this->sCode = preg_replace(
             '#<ph:auto_include ?/?>#',
@@ -63,115 +100,173 @@ class Tal extends Syntax
             '<?php $this->display($this->getCurrentController() . PH7_DS . \'$1\', $this->registry->path_module_views . PH7_TPL_MOD_NAME . PH7_DS); ?>',
             $this->sCode
         );
+    }
 
-        /***** <?php *****/
+    public function phpOpeningTag()
+    {
         $this->sCode = str_replace('<ph:code>', '<?php ', $this->sCode);
+    }
 
-        /***** ?> *****/
+    public function phpClosingTag()
+    {
         if (!preg_match('#;(?:\s+)?</ph:code>$#', $this->sCode)) {
             $this->sCode = str_replace('</ph:code>', ';?>', $this->sCode);
         } else {
             // Don't put a semicolon if there is already one
             $this->sCode = str_replace('</ph:code>', '?>', $this->sCode);
         }
+    }
 
-        /***** <?php ?> *****/
-        $this->sCode = preg_replace(
-            '#<ph:code value=(?:"|\')(.+)(?:"|\') ?/?>#',
-            '<?php $1 ?>',
-            $this->sCode
-        );
-
-        /***** <?php echo ?> *****/
+    public function phpOpeningTagWithEchoFunction()
+    {
         $this->sCode = preg_replace(
             '#<ph:print value=([^\<\>/\n]+) ?/?>#',
             '<?php echo $1 ?>',
             $this->sCode
         );
+    }
 
-        /***** if *****/
+    public function phpCode()
+    {
+        $this->sCode = preg_replace(
+            '#<ph:code value=(?:"|\')(.+)(?:"|\') ?/?>#',
+            '<?php $1 ?>',
+            $this->sCode
+        );
+    }
+
+    public function ifStatement()
+    {
         $this->sCode = preg_replace(
             '#<ph:if test=(?:"|\')([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php if($1) { ?>',
             $this->sCode
         );
+    }
 
-        /***** if isset *****/
+    public function ifSetStatement()
+    {
         $this->sCode = preg_replace(
             '#<ph:if-set test=(?:"|\')([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php if(!empty($1)) { ?>',
             $this->sCode
         );
+    }
 
-        /***** if empty *****/
+    public function ifEmptyStatement()
+    {
         $this->sCode = preg_replace(
             '#<ph:if-empty test=(?:"|\')([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php if(empty($1)) { ?>',
             $this->sCode
         );
+    }
 
-        /***** if equal *****/
+    public function ifEqualStatement()
+    {
         $this->sCode = preg_replace(
             '#<ph:if-equal test=(?:"|\')([^\<\>,"\'\n]+)(?:"|\'),(?:"|\')([^\<\>,"\'\n]+)(?:"|\')>#',
             '<?php if($1 == $2) { ?>',
             $this->sCode
         );
+    }
 
-        /***** elseif *****/
+    public function elseifStatement()
+    {
         $this->sCode = preg_replace(
             '#<ph:else-if test=(?:"|\')([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php elseif($1) { ?>',
             $this->sCode
         );
+    }
 
-        /***** else *****/
+    public function elseStatement()
+    {
         $this->sCode = str_replace('<ph:else>', '<?php else { ?>', $this->sCode);
+    }
 
-        /***** for *****/
-        /*** Example ***/
+    public function forLoopStatement()
+    {
         /* <ph:for test="$sData in $aData"> <p>Total items: <ph:print value="$sData_total" /><br /> Number: <ph:print value="$sData_i" /><br /> Name: <ph:print value="$sData" /></p> </ph:for> */
         $this->sCode = preg_replace(
             '#<ph:for test=(?:"|\')([^\<\>"\'\n]+) in ([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php for($1_i=0,$1_total=count($2);$1_i<$1_total;$1_i++) { $1=$2[$1_i]; ?>',
             $this->sCode
         );
+    }
 
-        /***** while *****/
+    public function whileLoopStatement()
+    {
         $this->sCode = preg_replace(
             '#<ph:while test=(?:"|\')([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php while($1) { ?>',
             $this->sCode
         );
+    }
 
-        /***** each (foreach) *****/
+    public function eachLoopStatement()
+    {
         $this->sCode = preg_replace(
             '#<ph:each test=(?:"|\')([^\<\>"\'\n]+) in ([^\<\>"\'\n]+)(?:"|\')>#',
             '<?php foreach($2 as $1) { ?>',
             $this->sCode
         );
+    }
 
-        /***** endif | endfor | endwhile | endforeach *****/
+    /**
+     * endif | endfor | endwhile | endforeach
+     *
+     * @return void
+     */
+    public function closingBlockStructures()
+    {
         $this->sCode = str_replace(
             ['</ph:if>', '</ph:else>', '</ph:else-if>', '</ph:for>', '</ph:while>', '</ph:each>', '</ph:if-set>', '</ph:if-empty>', '</ph:if-equal>'],
             '<?php } ?>',
             $this->sCode
         );
+    }
 
-        /***** <ph:designmodel value=[a-z0-9_]+()" /> *****/
+    public function variable()
+    {
+        $this->sCode = preg_replace('#\[\[([a-z0-9_]+)\]\]#i', '<?php echo $$1; ?>', $this->sCode);
+    }
+
+    /**
+     * ph:designmodel value=[a-z0-9_]+()
+     *
+     * @return void
+     */
+    public function designModelFunction()
+    {
         $this->sCode = preg_replace(
             '#<ph:designmodel value=(?:"|\')([a-z0-9_]+)\((.*)\)(?:"|\') ?/?>#i',
             '<?php $this->designModel->$1($2) ?>',
             $this->sCode
         );
+    }
 
-        /***** Escape (htmlspecialchars) *****/
+    /**
+     * Escape (htmlspecialchars).
+     *
+     * @return void
+     */
+    public function escapeFunction()
+    {
         $this->sCode = preg_replace(
             '#<ph:escape value=([^\<\>/\n]+) ?/?>#',
             '<?php this->str->escape($1); ?>',
             $this->sCode
         );
+    }
 
-        /***** Translate (Gettext) *****/
+    /**
+     * Translation (with gettext).
+     *
+     * @return void
+     */
+    public function langFunctions()
+    {
         $this->sCode = preg_replace(
             '#<ph:lang value=([^\<\>/\n]+) ?/?>#',
             '<?php echo t($1); ?>',
@@ -182,14 +277,18 @@ class Tal extends Syntax
             '<?php echo t(\'$1\'); ?>',
             $this->sCode
         );
+    }
 
-        /***** literal JavaScript Code *****/
+    public function literalFunction()
+    {
         $this->sCode = preg_replace('#<ph:literal>(.+)</ph:literal>#sU', '$1', $this->sCode);
+    }
 
-        /***** Variables *****/
-        $this->sCode = preg_replace('#\[\[([a-z0-9_]+)\]\]#i', '<?php echo $$1; ?>', $this->sCode);
-
-        /***** Clears comments: ### comment here ### *****/
+    /**
+     * Clears comments: ### comment here ###
+     */
+    public function clearComment()
+    {
         $this->sCode = preg_replace('/###.+###/sU', null, $this->sCode);
     }
 }
