b1429b0da2f5967328789427a5440906f7a0b016
Pierre-Henry Soria <hi@ph7.me>
2018-07-13 22:00:44 +0200

    [pH7Tpl:Syntax] Refactor CUrly classes, by splitting code into different funcs

--- _protected/framework/Layout/Tpl/Engine/PH7Tpl/Syntax/Curly.class.php
+++ _protected/framework/Layout/Tpl/Engine/PH7Tpl/Syntax/Curly.class.php
@@ -14,7 +14,7 @@ use PH7\Framework\Layout\Tpl\Engine\PH7Tpl\PH7Tpl;
 
 defined('PH7') or exit('Restricted access');
 
-class Curly extends Syntax
+class Curly extends Syntax implements Parsable
 {
     /**
      * {@inheritdoc}
@@ -28,7 +28,38 @@ class Curly extends Syntax
             );
         }
 
-        /***** Includes *****/
+        $this->includeStatements();
+
+        $this->phpOpeningTag();
+        $this->phpClosingTag();
+        $this->phpOpeningTagWithEchoFunction();
+
+        $this->ifStatement();
+        $this->elseStatement();
+        $this->elseifStatement();
+
+        /***** Loops *****/
+        $this->forLoopStatement();
+        $this->whileLoopStatement();
+        $this->eachLoopStatement();
+
+        $this->closingBlockStructures();
+
+        $this->designModelFunction();
+
+        $this->escapeFunction();
+
+        $this->langFunctions();
+
+        $this->literalFunction();
+
+        $this->variable();
+
+        $this->clearComment();
+    }
+
+    public function includeStatements()
+    {
         $this->sCode = str_replace(
             '{auto_include}',
             '<?php $this->display($this->getCurrentController() . PH7_DS . $this->registry->action . \'' . PH7Tpl::TEMPLATE_FILE_EXT . '\', $this->registry->path_module_views . PH7_TPL_MOD_NAME . PH7_DS); ?>',
@@ -59,10 +90,16 @@ class Curly extends Syntax
             '<?php $this->display($this->getCurrentController() . PH7_DS . $1, $this->registry->path_module_views . PH7_TPL_MOD_NAME . PH7_DS); ?>',
             $this->sCode
         );
+    }
 
+    public function phpOpeningTag()
+    {
         /***** <?php *****/
         $this->sCode = str_replace('{{', '<?php ', $this->sCode);
+    }
 
+    public function phpClosingTag()
+    {
         /***** ?> *****/
         if (!preg_match('#(;(?:\s+)?}}|;(?:\s+)?%})#', $this->sCode)) {
             $this->sCode = str_replace(['}}', '%}'], ';?>', $this->sCode);
@@ -70,66 +107,119 @@ class Curly extends Syntax
             // Don't put a semicolon if there is already one
             $this->sCode = str_replace(['}}', '%}'], '?>', $this->sCode);
         }
+    }
 
+    public function phpOpeningTagWithEchoFunction()
+    {
         /***** <?php echo *****/
         $this->sCode = str_replace('{%', '<?php echo ', $this->sCode);
+    }
 
-        /***** if *****/
+    public function ifStatement()
+    {
         $this->sCode = preg_replace('#{if ([^\{\}\n]+)}#', '<?php if($1) { ?>', $this->sCode);
+    }
 
-        /***** elseif *****/
+    public function elseifStatement()
+    {
         $this->sCode = preg_replace('#{elseif ([^\{\}\n]+)}#', '<?php } elseif($1) { ?>', $this->sCode);
+    }
 
-        /***** else *****/
+    public function elseStatement()
+    {
         $this->sCode = str_replace('{else}', '<?php } else { ?>', $this->sCode);
+    }
 
-        /***** for *****/
-        /*** Example ***/
+    public function forLoopStatement()
+    {
         /* {for $sData in $aData} <p>Total items: {% $sData_total %} /><br /> Number: {% $sData_i %}<br /> Name: {% $sData %}</p> {/for} */
         $this->sCode = preg_replace(
             '#{for ([^\{\}\n]+) in ([^\{\}\n]+)}#',
             '<?php for($1_i=0,$1_total=count($2);$1_i<$1_total;$1_i++) { $1=$2[$1_i]; ?>',
             $this->sCode
         );
+    }
 
-        /***** while *****/
+    public function whileLoopStatement()
+    {
         $this->sCode = preg_replace('#{while ([^\{\}\n]+)}#', '<?php while($1) { ?>', $this->sCode);
+    }
 
-        /***** each (foreach) *****/
+    public function eachLoopStatement()
+    {
         $this->sCode = preg_replace(
             '#{each ([^\{\}\n]+) in ([^\{\}\n]+)}#',
             '<?php foreach($2 as $1) { ?>',
             $this->sCode
         );
+    }
 
-        /***** endif | endfor | endwhile | endforeach *****/
+    /**
+     * endif | endfor | endwhile | endforeach
+     *
+     * @return void
+     */
+    public function closingBlockStructures()
+    {
         $this->sCode = str_replace(['{/if}', '{/for}', '{/while}', '{/each}'], '<?php } ?>', $this->sCode);
+    }
 
-        /***** {designModel.[a-z0-9_]+()} *****/
+    public function variable()
+    {
+        $this->sCode = preg_replace('#{([a-z0-9_]+)}#i', '<?php echo $$1; ?>', $this->sCode);
+    }
+
+    /**
+     * {designModel.[a-z0-9_]+()}
+     *
+     * @return void
+     */
+    public function designModelFunction()
+    {
         $this->sCode = preg_replace(
             '#{designModel\.([a-z0-9_]+)\((.*)\)}#i',
             '<?php $this->designModel->$1($2) ?>',
             $this->sCode
         );
+    }
 
-        /***** Escape (htmlspecialchars) *****/
+    /**
+     * Escape (htmlspecialchars).
+     *
+     * @return void
+     */
+    public function escapeFunction()
+    {
         $this->sCode = preg_replace(
             '#{escape ([^\{\}]+)}#',
             '<?php $this->str->escape($1); ?>',
             $this->sCode
         );
+    }
 
-        /***** Language *****/
+    public function langFunctions()
+    {
         $this->sCode = preg_replace('#{lang ([^\{\}]+)}#', '<?php echo t($1); ?>', $this->sCode);
         $this->sCode = preg_replace('#{lang}([^\{\}]+){/lang}#', '<?php echo t(\'$1\'); ?>', $this->sCode);
+    }
 
-        /***** {literal} JavaScript Code {/literal} *****/
+    /**
+     * {literal} JavaScript Code {/literal}
+     *
+     * @return void
+     */
+    public function literalFunction()
+    {
         $this->sCode = preg_replace('#{literal}(.*){/literal}#sU', '$1', $this->sCode);
+    }
 
-        /***** Variables *****/
-        $this->sCode = preg_replace('#{([a-z0-9_]+)}#i', '<?php echo $$1; ?>', $this->sCode);
-
-        /***** Clears comments {* comment *} *****/
+    /**
+     * Clears comments {* comment *}
+     *
+     * @return void
+     */
+    public function clearComment()
+    {
         $this->sCode = preg_replace('#{\*.+\*}#sU', null, $this->sCode);
     }
 }
