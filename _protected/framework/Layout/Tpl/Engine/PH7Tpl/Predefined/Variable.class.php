b6f57b3451501fc8952c587c124bef4921e84f8d
Pierre-Henry Soria <hi@ph7.me>
2018-07-15 11:34:03 +0200

    Remove redundant @version number

--- _protected/framework/Layout/Tpl/Engine/PH7Tpl/Predefined/Variable.class.php
+++ _protected/framework/Layout/Tpl/Engine/PH7Tpl/Predefined/Variable.class.php
@@ -1,14 +1,13 @@
 <?php
 /***************************************************************************
  * @title            PH7 Template Engine
- * @desc             We define variables.
+ * @desc             Define variables.
  *                   Predefined variables can save considerable resources and speeds up the code with respect to variables assigned by through the object's template engine (PH7Tpl).
  *
  * @author           Pierre-Henry Soria <ph7software@gmail.com>
  * @category         PH7 Template Engine
  * @package          PH7 / Framework / Layout / Tpl / Engine / PH7Tpl / Predefined
  * @copyright        (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
- * @version          1.0.2
  * @license          CC-BY License - http://creativecommons.org/licenses/by/3.0/
  ***************************************************************************/
 
