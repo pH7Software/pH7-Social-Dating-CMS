0c44fe6ba3780cb0c34f74843491a1a68e74ff31
Pierre-Henry Soria <hi@ph7.me>
2018-07-15 11:47:53 +0200

    Remove redundant @version tag + fix typos

--- _protected/framework/Layout/Tpl/Engine/PH7Tpl/Predefined/Func.class.php
+++ _protected/framework/Layout/Tpl/Engine/PH7Tpl/Predefined/Func.class.php
@@ -1,14 +1,13 @@
 <?php
 /***************************************************************************
  * @title            PH7 Template Engine
- * @desc             We define functions (and helpers).
- *                   Predefined functions can save considerable resources and speeds up the code with respect to functions in variables assigned by through the object's template engine (PH7Tpl).
+ * @desc             Define functions (and helpers).
+ *                   Predefined functions can save considerable resources and speeds up the code with respect to functions in variables assigned through the object's template engine (PH7Tpl).
  *
  * @author           Pierre-Henry Soria <ph7software@gmail.com>
  * @category         PH7 Template Engine
  * @package          PH7 / Framework / Layout / Tpl / Engine / PH7Tpl / Predefined
  * @copyright        (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
- * @version          1.0.2
  * @license          CC-BY License - http://creativecommons.org/licenses/by/3.0/
  ***************************************************************************/
 
