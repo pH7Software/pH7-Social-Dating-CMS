babe8c9d8c75c57b38664ff82886e7a9f9c5074f
Pierre-Henry Soria <hi@ph7.me>
2018-08-05 21:15:06 +0200

    Bump Image version class

--- _protected/framework/Image/Image.class.php
+++ _protected/framework/Image/Image.class.php
@@ -7,7 +7,7 @@
  * @copyright        (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / Image
- * @version          1.1
+ * @version          1.2
  * @link             http://ph7.me
  * @linkGD           http://php.net/manual/book.image.php
  */
