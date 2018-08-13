84ed46e966d6c5e496546cad25e1e0e03ccc2270
Pierre-Henry Soria <hi@ph7.me>
2018-08-13 11:31:16 +0200

    [Cleanup] Fix indentation; Make line length shorter

--- _protected/app/system/modules/note/forms/processing/EditNoteFormProcess.php
+++ _protected/app/system/modules/note/forms/processing/EditNoteFormProcess.php
@@ -41,7 +41,10 @@ class EditNoteFormProcess extends Form
         }
 
         if (!$this->updateCategories($iNoteId, $iProfileId, $oPost, $oNoteModel)) {
-            \PFBC\Form::setError('form_edit_note', t('You cannot select more than %0% categories.', Note::MAX_CATEGORY_ALLOWED));
+            \PFBC\Form::setError(
+                'form_edit_note',
+                t('You cannot select more than %0% categories.', Note::MAX_CATEGORY_ALLOWED)
+            );
             return; // Stop execution of the method
         }
 
