f7bc764536262ec7a4896d8aeba4f1e012ed354a
Pierre-Henry Soria <hi@ph7.me>
2018-09-14 12:53:53 +1200

    Update class

--- _protected/app/system/modules/milestone-celebration/inc/class/MilestoneNotifier.php
+++ _protected/app/system/modules/milestone-celebration/inc/class/MilestoneNotifier.php
@@ -10,25 +10,46 @@ namespace PH7;
 
 class MilestoneNotifier
 {
-    const MAIL_TEMPLATE_FILENAME = '/global/mail/mod/milestone-celebration/admin-notifier.tpl';
+    const MAIL_TEMPLATE_FILE = '/tpl/mail/sys/core/milestone-celebration/admin-notifier.tpl';
 
     private $oUserModel;
 
     private $oMail;
 
-     private $oView;
+    private $oView;
+
+    /**
+     * @return int
+     *
+     * @throws Framework\File\Exception
+     * @throws Framework\Layout\Tpl\Engine\PH7Tpl\Exception
+     */
+    private function sendMessage()
+    {
+        $this->oView->body = $this->getNotifierMessage();
+        $sRecipientEmail = $this->getEmail();
+        $sMessageHtml = $this->oView->parseMail(
+            PH7_PATH_SYS . 'global/' . PH7_VIEWS . PH7_TPL_MAIL_NAME . self::MAIL_TEMPLATE_FILE,
+            $sRecipientEmail
+        );
+        $aInfo = [
+            'to' => $sRecipientEmail,
+            'subject' => $this->getNotifierSubject()
+        ];
+        return $this->oMail->send($aInfo, $sMessageHtml, Mailable::HTML_FORMAT);
+    }
 
 
-      public function __construct(UserCoreModel $oUserModel, Mailable $oMail, PH7Tpl $oView)
-      {
-          $this->oMail = $oMail;
-          $this->oUserModel = $oUserModel;
-          $this->oView = $oView;
-      }
+    public function __construct(UserCoreModel $oUserModel, Mailable $oMail, PH7Tpl $oView)
+    {
+        $this->oMail = $oMail;
+        $this->oUserModel = $oUserModel;
+        $this->oView = $oView;
+    }
 
-      public function sendEmailToAdmin()
-      {
-          $this->oView->subject
+    public function sendEmailToAdmin()
+    {
+        $this->oView->subject
 
 
           $aInfo = [
