ee0feba80783eb562ed42e909fab8e3cd0e2db03
Pierre-Henry Soria <hi@ph7.me>
2018-09-14 17:14:31 +1200

    Improving the Milestone's service that sends emails to admin

--- _protected/app/system/modules/milestone-celebration/inc/class/MilestoneNotifier.php
+++ _protected/app/system/modules/milestone-celebration/inc/class/MilestoneNotifier.php
@@ -8,37 +8,20 @@
 
 namespace PH7;
 
+use PH7\Framework\Layout\Tpl\Engine\PH7Tpl\PH7Tpl;
+
 class MilestoneNotifier
 {
-    const MAIL_TEMPLATE_FILE = '/tpl/mail/sys/core/milestone-celebration/admin-notifier.tpl';
+    const MAIL_TEMPLATE_FILE_PATH = '/tpl/mail/sys/core/milestone-celebration/admin-notifier.tpl';
 
+    /** @var UserCoreModel */
     private $oUserModel;
 
+    /** @var Mailable */
     private $oMail;
 
+    /** @var PH7Tpl */
     private $oView;
-
-    /**
-     * @return int
-     *
-     * @throws Framework\File\Exception
-     * @throws Framework\Layout\Tpl\Engine\PH7Tpl\Exception
-     */
-    private function sendMessage()
-    {
-        $this->oView->body = $this->getNotifierMessage();
-        $sRecipientEmail = $this->getEmail();
-        $sMessageHtml = $this->oView->parseMail(
-            PH7_PATH_SYS . 'global/' . PH7_VIEWS . PH7_TPL_MAIL_NAME . self::MAIL_TEMPLATE_FILE,
-            $sRecipientEmail
-        );
-        $aInfo = [
-            'to' => $sRecipientEmail,
-            'subject' => $this->getNotifierSubject()
-        ];
-        return $this->oMail->send($aInfo, $sMessageHtml, Mailable::HTML_FORMAT);
-    }
-
 
     public function __construct(UserCoreModel $oUserModel, Mailable $oMail, PH7Tpl $oView)
     {
@@ -47,16 +30,29 @@ class MilestoneNotifier
         $this->oView = $oView;
     }
 
+    /**
+     * @return int
+     *
+     * @throws Framework\Layout\Tpl\Engine\PH7Tpl\Exception
+     */
     public function sendEmailToAdmin()
     {
-        $this->oView->subject
+        $this->oView->body = $this->getNotifierMessage();
+        $sAdminEmail = DbConfig::getSetting('adminEmail');
 
+        $sMessageHtml = $this->oView->parseMail(
+            PH7_PATH_SYS . 'global/' . PH7_VIEWS . PH7_TPL_MAIL_NAME . self::MAIL_TEMPLATE_FILE_PATH,
+            $sAdminEmail
+        );
+        $aInfo = [
+            'to' => $sAdminEmail,
+            'subject' => t('Your Website Reached % 0 % users!🍾', $this->oUserModel->total())
+        ];
 
-          $aInfo = [
-              ‘subject’ => t('Your Website Reached % 0 % users!🍾', $this->oUserModel->total())
-
-          ];
-
-           return $this->oMail->send($aInfo, $mailContents, Mailable::HTML_FORMAT);
+        return $this->oMail->send(
+            $aInfo,
+            $sMessageHtml,
+            Mailable::HTML_FORMAT
+        );
     }
 }
