01bf9c1a85395edd651f93499a138879d7e29fab
Pierre-Henry Soria <hi@ph7.me>
2018-09-14 12:55:22 +1200

    Update wrong header copyright date

--- _protected/framework/Mail/InvalidEmailException.class.php
+++ _protected/framework/Mail/InvalidEmailException.class.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author           Pierre-Henry Soria <hi@ph7.me>
- * @copyright        (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright        (c) 2018, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / Mail
  */
