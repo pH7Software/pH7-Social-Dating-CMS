cf463ade6aee2d57c7fe4800413b1cf25f4889ab
Pierre-Henry Soria <hi@ph7.me>
2018-09-18 21:04:02 +1200

    #301 [UpgradePatch] Remove street fields

--- _repository/upgrade/14.0.0-14.3.0/data/sql/PostgreSQL/upgrade.sql
+++ _repository/upgrade/14.0.0-14.3.0/data/sql/PostgreSQL/upgrade.sql
@@ -6,6 +6,8 @@
 
 -- Remove field that is no longer used
 ALTER TABLE ph7_memberships DROP COLUMN orderId;
+ALTER TABLE ph7_members_info DROP COLUMN street;
+ALTER TABLE ph7_affiliates_info DROP COLUMN street;
 
 
 -- Update pH7CMS's SQL schema version
