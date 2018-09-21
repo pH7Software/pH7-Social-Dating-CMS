fb87bf9df2d62d49b91d200a0e2e4ac3c0c5dde8
Pierre-Henry Soria <hi@ph7.me>
2018-09-22 09:51:08 +1200

    [Cleanup][PFBC BankForm class] Fix indentation

--- _protected/app/system/modules/affiliate/forms/BankForm.php
+++ _protected/app/system/modules/affiliate/forms/BankForm.php
@@ -33,12 +33,34 @@ class BankForm
         $oForm->addElement(new \PFBC\Element\Token('bank_account'));
 
         if (self::isAdminLogged() && $oHttpRequest->getExists('profile_id')) {
-            $oForm->addElement(new \PFBC\Element\HTMLExternal('<p class="center"><a class="bold btn btn-default btn-md" href="' . Uri::get('affiliate', 'admin', 'browse') . '">' . t('Back to Browse Affiliates') . '</a></p>'));
+            $oForm->addElement(
+                new \PFBC\Element\HTMLExternal(
+                    '<p class="center"><a class="bold btn btn-default btn-md" href="' . Uri::get('affiliate', 'admin', 'browse') . '">' . t('Back to Browse Affiliates') . '</a></p>'
+                )
+            );
         }
 
-        $oForm->addElement(new \PFBC\Element\HTMLExternal('<h2 class="underline">' . t('Bank Information:') . '</h2>'));
+        $oForm->addElement(
+            new \PFBC\Element\HTMLExternal(
+                '<h2 class="underline">' . t('Bank Information:') . '</h2>'
+            )
+        );
+
         $sHtmlPayPalIcon = '<a href="https://www.paypal.com" rel="noopener" target="_blank"><img src="' . PH7_URL_STATIC . PH7_IMG . 'icon/paypal-small.svg" alt="PayPal" title="PayPal"></a><br />';
-        $oForm->addElement(new \PFBC\Element\Email($sHtmlPayPalIcon . t('Your Bank Account:'), 'bank_account', ['id' => 'email_paypal', 'onblur' => 'CValid(this.value,this.id)', 'description' => t('Your Bank Account (PayPal Email Address).'), 'value' => self::getAffiliateBankAccount($oHttpRequest), 'validation' => new \PFBC\Validation\BankAccount, 'required' => 1]));
+        $oForm->addElement(
+            new \PFBC\Element\Email(
+                $sHtmlPayPalIcon . t('Your Bank Account:'),
+                'bank_account',
+                [
+                    'id' => 'email_paypal',
+                    'onblur' => 'CValid(this.value,this.id)',
+                    'description' => t('Your Bank Account (PayPal Email Address).'),
+                    'value' => self::getAffiliateBankAccount($oHttpRequest),
+                    'validation' => new \PFBC\Validation\BankAccount,
+                    'required' => 1
+                ]
+            )
+        );
         $oForm->addElement(new \PFBC\Element\HtmlExternal('<span class="input_error email_paypal"></span>'));
         $oForm->addElement(new \PFBC\Element\Button);
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<script src="' . PH7_URL_STATIC . PH7_JS . 'validate.js"></script>'));
