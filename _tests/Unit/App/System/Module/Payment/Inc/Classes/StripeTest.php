05a8c97491689ab4cb17dc22603a082c35d23562
Pierre-Henry Soria <hi@ph7.me>
2018-09-23 12:58:04 +1200

    Add unit tests

---
+++ _tests/Unit/App/System/Module/Payment/Inc/Classes/StripeTest.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @author           Pierre-Henry Soria <hello@ph7cms.com>
+ * @copyright        (c) 2018, Pierre-Henry Soria. All Rights Reserved.
+ * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package          PH7 / Test / Unit / App / System / Module / Payment / Inc / Classes
+ */
+
+namespace PH7\Test\Unit\App\System\Module\Payment\Inc\Classes;
+
+require_once PH7_PATH_SYS . 'core/models/UserCoreModel.php';
+require_once PH7_PATH_SYS_MOD . 'payment/inc/class/Api.php';
+require_once PH7_PATH_SYS_MOD . 'payment/inc/class/Stripe.php';
+
+use PH7\Stripe;
+use PHPUnit_Framework_TestCase;
+
+class StripeTest extends PHPUnit_Framework_TestCase
+{
+    public function testAmount()
+    {
+        $iAmount = Stripe::getAmount('19.99');
+
+        $this->assertSame('1999', $iAmount);
+    }
+}
