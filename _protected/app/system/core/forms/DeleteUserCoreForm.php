4fd7059b0cba67c1262cc206dbaf03c31866615f
Pierre-Henry Soria <hi@ph7.me>
2018-09-25 21:45:53 +1200

    Add live Captcha/description ajax validation

--- _protected/app/system/core/forms/DeleteUserCoreForm.php
+++ _protected/app/system/core/forms/DeleteUserCoreForm.php
@@ -30,10 +30,13 @@ class DeleteUserCoreForm
         $oForm->addElement(new \PFBC\Element\Hidden('submit_delete_account', 'form_delete_account'));
         $oForm->addElement(new \PFBC\Element\Token('delete_account'));
         $oForm->addElement(new \PFBC\Element\Password(t('Your Password:'), 'password', ['required' => 1]));
-        $oForm->addElement(new \PFBC\Element\Textarea(t('Your Reason:'), 'message', ['description' => t('Please be specific why you want to leave us.') . '<br />' . t('It will hep us to improve our service and make it the best one for you!'), 'required' => 1, 'validation' => new \PFBC\Validation\Str(5, 500)]));
+        $oForm->addElement(new \PFBC\Element\Textarea(t('Your Reason:'), 'message', ['id' => 'str_reason', 'onblur' => 'CValid(this.value, this.id,5,500)', 'description' => t('Please be specific why you want to leave us.') . '<br />' . t('It will hep us to improve our service and make it the best one for you!'), 'required' => 1, 'validation' => new \PFBC\Validation\Str(5, 500)]));
+        $oForm->addElement(new \PFBC\Element\HTMLExternal('<span class="input_error str_reason"></span>'));
         $oForm->addElement(new \PFBC\Element\Radio(t('Why:'), 'why_delete', [t("I'm not happy with the service."), t('I met someone.'), t('Other. I said the reason above.')], ['required' => 1]));
-        $oForm->addElement(new \PFBC\Element\CCaptcha(t('Captcha'), 'captcha', ['description' => t('Enter the below code:')]));
+        $oForm->addElement(new \PFBC\Element\CCaptcha(t('Captcha'), 'captcha', ['id' => 'ccaptcha', 'onkeyup' => 'CValid(this.value, this.id)', 'description' => t('Enter the below code:')]));
+        $oForm->addElement(new \PFBC\Element\HTMLExternal('<span class="input_error ccaptcha"></span>'));
         $oForm->addElement(new \PFBC\Element\Button);
+        $oForm->addElement(new \PFBC\Element\HTMLExternal('<script src="' . PH7_URL_STATIC . PH7_JS . 'validate.js"></script>'));
         $oForm->render();
     }
 }
