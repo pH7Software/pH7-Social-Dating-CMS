88ce283880e7d4a2b79c3ed4b69457554af32095
Pierre-Henry Soria <hi@ph7.me>
2018-09-26 18:19:10 +1200

    GitHub #314 Update field desc + remove saledatingprofiles website

--- _protected/app/system/modules/admin123/forms/ImportUserForm.php
+++ _protected/app/system/modules/admin123/forms/ImportUserForm.php
@@ -26,7 +26,7 @@ class ImportUserForm
 
         $oForm = new \PFBC\Form('form_import_user');
         $oForm->configure(['action' => '']);
-        $oForm->addElement(new \PFBC\Element\HTMLExternal('<p>' . t('Import members into your site from a <strong><abbr title="Comma Separated Values">CSV</abbr></strong> file. You can find database users <strong><a href="%0%">here</a></strong>.', 'http://www.saledatingprofiles.com/?from=ph7cms.com') . '</p>'));
+        $oForm->addElement(new \PFBC\Element\HTMLExternal('<p>' . t('Import members into your site from a <strong><abbr title="Comma Separated Values">CSV</abbr></strong> file. You should be able to find websites that offer dating profiles database. Make sure they offer CSV format.') . '</p>'));
         $oForm->addElement(new \PFBC\Element\Hidden('submit_import_user', 'form_import_user'));
         $oForm->addElement(new \PFBC\Element\Token('import_user'));
         $oForm->addElement(new \PFBC\Element\File(t('CSV file:'), 'csv_file', ['accept' => '.csv', 'required' => 1]));
