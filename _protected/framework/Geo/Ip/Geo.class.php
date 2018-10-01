e59c996583a8c4b4fe92c4aedf6438bf88d152a8
Pierre-Henry Soria <hi@ph7.me>
2018-10-01 18:14:15 +1300

    Remove redundant @version phpdoc's tag

--- _protected/framework/Geo/Ip/Geo.class.php
+++ _protected/framework/Geo/Ip/Geo.class.php
@@ -6,7 +6,6 @@
  * @copyright        (c) 2012-2018, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / Geo / Ip
- * @version          1.1
  */
 
 namespace PH7\Framework\Geo\Ip;
