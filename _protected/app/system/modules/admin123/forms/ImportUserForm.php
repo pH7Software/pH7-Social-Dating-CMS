aeec81840f1547084349aa6b8d96e67e57364294
Pierre-Henry Soria <hi@ph7.me>
2018-10-01 21:58:41 +1300

    [#314] Add details about what CSV import can do and cannot
    Avoid confusion and limit the amount of new issues. Mention that the CSV import cannot handle user images at the moment.
    
    Ref:  https://github.com/pH7Software/pH7-Social-Dating-CMS/issues/314#issuecomment-424627398

--- _protected/app/system/modules/admin123/forms/ImportUserForm.php
+++ _protected/app/system/modules/admin123/forms/ImportUserForm.php
@@ -10,6 +10,7 @@
 
 namespace PH7;
 
+use PH7\Framework\Mvc\Router\Uri;
 use PH7\Framework\Url\Header;
 
 class ImportUserForm
@@ -27,6 +28,7 @@ class ImportUserForm
         $oForm = new \PFBC\Form('form_import_user');
         $oForm->configure(['action' => '']);
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<p>' . t('Import members into your site from a <strong><abbr title="Comma Separated Values">CSV</abbr></strong> file. You should be able to find websites that offer dating profiles database. Make sure they offer CSV format.') . '</p>'));
+        $oForm->addElement(new \PFBC\Element\HTMLExternal('<p class="small">' . t('Note: This tool cannot import profile photos. Please use the <a href="%0%">Fake Profile Generator</a> if you need profiles with photos', Uri::get(PH7_ADMIN_MOD, 'user', 'addfakeprofiles')) . '</p>'));
         $oForm->addElement(new \PFBC\Element\Hidden('submit_import_user', 'form_import_user'));
         $oForm->addElement(new \PFBC\Element\Token('import_user'));
         $oForm->addElement(new \PFBC\Element\File(t('CSV file:'), 'csv_file', ['accept' => '.csv', 'required' => 1]));
