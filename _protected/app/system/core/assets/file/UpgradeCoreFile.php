6287fc0569d2e77597e9aea85a525ba7115a4cbb
Pierre-Henry Soria <hi@ph7.me>
2018-10-04 19:26:57 +1300

    Rename variable name with type name as prefix
    More info: https://ph7cms.com/doc/en/code-convention

--- _protected/app/system/core/assets/file/UpgradeCoreFile.php
+++ _protected/app/system/core/assets/file/UpgradeCoreFile.php
@@ -488,7 +488,7 @@ class UpgradeCore
     {
         try {
             return F\Import::file(PH7_PATH_REPOSITORY . static::DIR . PH7_DS . $this->sUpgradesDirUpgradeFolder . static::INFO_DIR . PH7_DS . $sInstFile);
-        } catch (F\IOException $e) {
+        } catch (F\IOException $oExcept) {
             return '<p class="error">' . t('Instruction file not found!') . '</p>';
         }
     }
