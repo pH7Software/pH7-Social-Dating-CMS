867f5dc193f242df72c2bd246004062bcec267c6
Pierre-Henry Soria <hi@ph7.me>
2018-10-12 18:26:51 +1300

    HTML Indentation fix

--- _protected/framework/Layout/Html/Design.class.php
+++ _protected/framework/Layout/Html/Design.class.php
@@ -770,12 +770,12 @@ class Design
         if ((bool)DbConfig::getSetting('socialMediaWidgets')) {
             $sHtml = <<<HTML
 <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
-<a class="a2a_dd" href="https://www.addtoany.com/share" rel="nofollow"></a>
-<a class="a2a_button_facebook"></a>
-<a class="a2a_button_twitter"></a>
-<a class="a2a_button_pinterest"></a>
-<a class="a2a_button_facebook_messenger"></a>
-<a class="a2a_button_linkedin"></a>
+    <a class="a2a_dd" href="https://www.addtoany.com/share" rel="nofollow"></a>
+    <a class="a2a_button_facebook"></a>
+    <a class="a2a_button_twitter"></a>
+    <a class="a2a_button_pinterest"></a>
+    <a class="a2a_button_facebook_messenger"></a>
+    <a class="a2a_button_linkedin"></a>
 </div>
 HTML;
             echo $sHtml;
@@ -794,10 +794,10 @@ HTML;
         if ((bool)DbConfig::getSetting('socialMediaWidgets')) {
             $sHtml = <<<HTML
 <div class="a2a_kit a2a_kit_size_32 a2a_default_style">
-<a class="a2a_dd" href="https://www.addtoany.com/share" rel="nofollow"></a>
-<a class="a2a_button_facebook"></a>
-<a class="a2a_button_twitter"></a>
-<a class="a2a_button_pinterest"></a>
+    <a class="a2a_dd" href="https://www.addtoany.com/share" rel="nofollow"></a>
+    <a class="a2a_button_facebook"></a>
+    <a class="a2a_button_twitter"></a>
+    <a class="a2a_button_pinterest"></a>
 </div>
 HTML;
             echo $sHtml;
