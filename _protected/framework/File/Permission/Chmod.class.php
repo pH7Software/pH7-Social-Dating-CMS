2f5f8e7cf0375b7eff26c4d6020998323fc6bf1f
Pierre-Henry Soria <hi@ph7.me>
2018-10-17 23:34:29 +1300

    Fix wrong @package value

--- _protected/framework/File/Permission/Chmod.class.php
+++ _protected/framework/File/Permission/Chmod.class.php
@@ -3,7 +3,7 @@
  * @author           Pierre-Henry Soria <hello@ph7cms.com>
  * @copyright        (c) 2018, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
- * @package          PH7 / Framework / File
+ * @package          PH7 / Framework / File / Permission
  */
 
 namespace PH7\Framework\File\Permission;
--- _protected/framework/File/Permission/PermissionException.class.php
+++ _protected/framework/File/Permission/PermissionException.class.php
@@ -3,7 +3,7 @@
  * @author           Pierre-Henry Soria <ph7software@gmail.com>
  * @copyright        (c) 2017-2018, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
- * @package          PH7 / Framework / File / Transfer
+ * @package          PH7 / Framework / File / Permission
  */
 
 namespace PH7\Framework\File\Permission;
