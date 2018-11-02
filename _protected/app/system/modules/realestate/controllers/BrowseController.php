93118dbe3f79fde4b3b56e76b4bb0ba195adf88c
Pierre-Henry Soria <hi@ph7.me>
2018-11-03 10:34:54 +1300

    [Search:Browse] Add seller action (for seller profiles)

--- _protected/app/system/modules/realestate/controllers/BrowseController.php
+++ _protected/app/system/modules/realestate/controllers/BrowseController.php
@@ -58,10 +58,44 @@ class BrowseController extends Controller
             // We can put HTML tags in the <title> tag as our template engine will remove all HTML tags present in the title tag, ...
             $this->view->page_title = t('Browse Members');
             $this->view->h1_title = '<span class="pH1">' . t('Browse Members') . '</span>';
-            $this->view->h3_title = t('Meet new People with %0%', '<span class="pH0">' . $this->registry->site_name . '</span>');
+            $this->view->h3_title = t('Find Buyers with %0%', '<span class="pH0">' . $this->registry->site_name . '</span>');
             $this->view->meta_description = t('Meet new People and Friends near you with %site_name% - Browse Members');
             $this->view->avatarDesign = new AvatarDesignCore;
             $this->view->users = $oUsers;
+
+            $this->output();
+        }
+    }
+
+    public function seller()
+    {
+        $this->iTotalUsers = $this->oUserModel->search($_GET, true, null, null);
+        $this->view->total_pages = $this->oPage->getTotalPages(
+            $this->iTotalUsers, self::MAX_PROFILE_PER_PAGE
+        );
+        $this->view->current_page = $this->oPage->getCurrentPage();
+        $oUsers = $this->oUserModel->search(
+            $_GET,
+            false,
+            $this->oPage->getFirstItem(),
+            $this->oPage->getNbItemsPerPage()
+        );
+
+        if (empty($oUsers)) {
+            Header::redirect(
+                Uri::get('realestate', 'search', 'seller'),
+                t('No results. Please try again with wider or new search criteria.'),
+                Design::WARNING_TYPE
+            );
+        } else {
+            // We can put HTML tags in the <title> tag as our template engine will remove all HTML tags present in the title tag, ...
+            $this->view->page_title = t('Browse Members');
+            $this->view->h1_title = '<span class="pH1">' . t('Browse Members') . '</span>';
+            $this->view->h3_title = t('Find Sellers with %0%', '<span class="pH0">' . $this->registry->site_name . '</span>');
+            $this->view->meta_description = t('Meet new People and Friends near you with %site_name% - Browse Members');
+            $this->view->avatarDesign = new AvatarDesignCore;
+            $this->view->users = $oUsers;
+
             $this->output();
         }
     }
