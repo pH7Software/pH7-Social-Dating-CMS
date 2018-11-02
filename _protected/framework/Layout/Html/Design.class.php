7ae398ce5b6b32f38781d2255626fb74325ff97d
Pierre-Henry Soria <hi@ph7.me>
2018-11-03 11:12:02 +1300

    Update homepage default URL

--- _protected/framework/Layout/Html/Design.class.php
+++ _protected/framework/Layout/Html/Design.class.php
@@ -288,7 +288,7 @@ class Design
             if (SysMod::isEnabled('user-dashboard')) {
                 $this->url('user-dashboard', 'main', 'index');
             } else {
-                $this->url('realestate', 'browse', 'index');
+                $this->url('realestate', 'main', 'index');
             }
         } elseif (AdminCore::auth()) {
             $this->url(PH7_ADMIN_MOD, 'main', 'index');
