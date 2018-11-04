49b1dec602d45677b22740ada0c39eb18aec8c03
Pierre-Henry Soria <hi@ph7.me>
2018-11-04 16:15:54 +1300

    Make website name shorter

--- _install/data/sql/MySQL/pH7_Core.sql
+++ _install/data/sql/MySQL/pH7_Core.sql
@@ -12,7 +12,7 @@
 --
 -- Set the variables --
 --
-SET @sDefaultSiteName = 'Acquireutah: Real Estate, community website';
+SET @sDefaultSiteName = 'Acquireutah: Real Estate';
 SET @sAdminEmail = 'admin@yoursite.com';
 SET @sFeedbackEmail = 'feedback@yoursite.com';
 SET @sNoReplyEmail = 'noreply@yoursite.com';
