823ffdcb32d5bb9320c6c5be949f010c57d531dc
Pierre-Henry Soria <hi@ph7.me>
2018-11-16 19:04:11 +1300

    Add a.k.a when mentioning profiles' usernames

--- _protected/app/system/modules/user/controllers/SignupController.php
+++ _protected/app/system/modules/user/controllers/SignupController.php
@@ -59,7 +59,7 @@ class SignupController extends Controller
         $this->view->page_title = ($bUserRef) ? t('Register for free to meet %0% on %site_name%. The Real Social Dating app!', $sFirstName) : t('Free Sign Up to Meet Lovely People!');
 
         if ($bUserRef) {
-            $sH1Txt = t('Register for Free to Meet <span class="pink2">%0%</span> (<span class="pink1">%1%</span>) on <span class="pink2">%site_name%</span>!', $sFirstName, $this->str->upperFirst($sUsername));
+            $sH1Txt = t('Register for Free to Meet <span class="pink2">%0%</span> (a.k.a <span class="pink1">%1%</span>) on <span class="pink2">%site_name%</span>!', $sFirstName, $this->str->upperFirst($sUsername));
         } else {
             $sH1Txt = t('Sign Up on %site_name%!');
         }
