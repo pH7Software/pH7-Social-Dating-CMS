54a306071b0fd9dc54154e434382d22c2e9ace60
Pierre-Henry Soria <hi@ph7.me>
2018-11-18 23:26:17 +1300

    [PWA Mod:Revert] At this time, dont cache anything

--- service-worker.js
+++ service-worker.js
@@ -1,15 +1,3 @@
-var CACHE_NAME = 'homepage';
-
-const CACHED_FILES = [
-    '/',
-    // TODO: Add more URLs to be cached here
-];
-self.addEventListener('install', function (event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(function (cache) {
-                console.info('[sw.js] cached the files.');
-                return cache.addAll(CACHED_FILES);
-            })
-    );
-});
+/*
+ TODO: Implement the Service Worker to cache some static data, html, images, ...
+ */
