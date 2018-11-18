0024313b67da6b85e4bda8ba27e0695dd93db557
Pierre-Henry Soria <hi@ph7.me>
2018-11-19 01:34:14 +1300

    Remove "comma" from last array element

--- _protected/framework/Mvc/Controller/Controller.class.php
+++ _protected/framework/Mvc/Controller/Controller.class.php
@@ -255,7 +255,7 @@ abstract class Controller extends Core implements Controllable
             'is_birthday_enabled' => SysMod::isEnabled('birthday'),
             'is_friend_enabled' => SysMod::isEnabled('friend'),
             'is_webcam_enabled' => SysMod::isEnabled('webcam'),
-            'is_pwa_enabled' => SysMod::isEnabled('pwa'),
+            'is_pwa_enabled' => SysMod::isEnabled('pwa')
         ];
 
         $this->view->assigns($aModsEnabled);
