0d1a43794013a5bda14504ea8009e6aacb9b75f6
Pierre-Henry Soria <hi@ph7.me>
2018-11-21 22:27:02 +1300

    [Installation Wizard] Update language keys

--- _install/langs/en/install.lang.php
+++ _install/langs/en/install.lang.php
@@ -63,7 +63,7 @@ $LANG = array(
     'db_password' => 'Database password',
     'db_prefix' => 'Table name prefix',
     'desc_db_prefix' => 'This option is useful when you have multiple installations of pH7CMS on the same database.
-        We recommend that you change the default values ​​in order to increase the security of your website.',
+        We also recommend that you change the default value ​​in order to increase the security of your website.',
     'db_encoding' => 'Encoding',
     'desc_db_encoding' => 'Database encoding. Usually UTF8 encoding for international.',
     'db_port' => 'Database host port number',
