8e46d485934df4a14a86d3dce145682c87b51ad0
Pierre-Henry Soria <hi@ph7.me>
2018-11-23 13:18:14 +1300

    [Understandability] Mention where "sw-register.js" is located

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
 /*
+ @see /templates/system/modules/pwa/themes/base/js/sw-register.js
+
  TODO: Implement the Service Worker to cache some static data, html pages, images, etc.
  More info: https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
  🚀 Feel free to fork the project: https://github.com/pH7Software/pH7-Social-Dating-CMS
