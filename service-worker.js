39053b68e9a047bb887a1f987389e0676a1d8c7c
Pierre-Henry Soria <hi@ph7.me>
2018-11-23 13:14:41 +1300

    :tada: Make it more ATTRACTIVE for contributing to pH7Builder :rocket:

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 /*
- TODO: Implement the Service Worker to cache some static data, html, images, ...
+ TODO: Implement the Service Worker to cache some static data, html pages, images, etc.
  More info: https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
- Feel free to fork the project https://github.com/pH7Software/pH7-Social-Dating-CMS
- Commit your changes, then create a Pull Request for submitting your awesome changes.
+ 🚀 Feel free to fork the project: https://github.com/pH7Software/pH7-Social-Dating-CMS
+ Commit your changes, then open a Pull Request for submitting your awesome changes 🎉
  */
