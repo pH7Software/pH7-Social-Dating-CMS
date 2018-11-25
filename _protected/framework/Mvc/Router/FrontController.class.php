2418a8856e295423c66cf62a2a09902a7cb2d370
Pierre-Henry Soria <hi@ph7.me>
2018-11-26 12:32:53 +1300

    FrontController: Update comments

--- _protected/framework/Mvc/Router/FrontController.class.php
+++ _protected/framework/Mvc/Router/FrontController.class.php
@@ -147,7 +147,8 @@ final class FrontController
                     $this->notFound('The <b>' . $this->oRegistry->module .
                         '</b> system module is not found.<br />File: <b>' . PH7_PATH_APP . $sPathModule . $this->oRegistry->module . PH7_DS .
                         '</b><br /> or the <b>' . PH7_CONFIG_FILE . '</b> file is not found.<br />File: <b>' . PH7_PATH_APP . $sPathModule . $this->oRegistry->module . PH7_DS . PH7_CONFIG . PH7_CONFIG_FILE . '</b>');
-                    // It reloads the config.ini file for the new module "error"
+
+                    // Reload the config.ini file for the new "error" module
                     $this->oConfig->load(PH7_PATH_MOD . $this->oRegistry->module . PH7_DS . PH7_CONFIG . PH7_CONFIG_FILE);
                 }
 
@@ -199,7 +200,7 @@ final class FrontController
             $this->notFound('The <b>' . $this->oRegistry->module . '</b> system module is not found.<br />File: <b>' . PH7_PATH_SYS . PH7_MOD . $this->oRegistry->module . PH7_DS .
                 '</b><br /> or the <b>' . PH7_CONFIG_FILE . '</b> file is not found.<br />File: <b>' . PH7_PATH_SYS . PH7_MOD . $this->oRegistry->module . PH7_DS . PH7_CONFIG . PH7_CONFIG_FILE . '</b>');
 
-            // It reloads the config.ini file for the new module "error"
+            // Reload the config.ini file for the new "error" module
             $this->oConfig->load(PH7_PATH_SYS . PH7_MOD . $this->oRegistry->module . PH7_DS . PH7_CONFIG . PH7_CONFIG_FILE);
         }
 
@@ -258,7 +259,8 @@ final class FrontController
         if (!$this->oConfig->load(PH7_PATH_MOD . $this->oRegistry->module . PH7_DS . PH7_CONFIG . PH7_CONFIG_FILE)) {
             $this->notFound('The <b>' . $this->oRegistry->module . '</b> module is not found.<br />File: <b>' . PH7_PATH_MOD . $this->oRegistry->module . PH7_DS .
                 '</b><br /> or the <b>' . PH7_CONFIG_FILE . '</b> file is not found.<br />File: <b>' . PH7_PATH_MOD . $this->oRegistry->module . PH7_DS . PH7_CONFIG . PH7_CONFIG_FILE . '</b>');
-            // It reloads the config.ini file for the new module "error"
+
+            // Reload the config.ini file for the new "error" module
             $this->oConfig->load(PH7_PATH_MOD . $this->oRegistry->module . PH7_DS . PH7_CONFIG . PH7_CONFIG_FILE);
         }
 
