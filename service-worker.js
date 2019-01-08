a304ac686fb8bfd2a98414a2bfb763a3e3e1185b
Pierre-Henry Soria <hi@ph7.me>
2019-01-08 18:10:50 +1300

    [PWA Mod][WIP:Alpha] Continue ServiceWorker.js

--- service-worker.js
+++ service-worker.js
@@ -6,3 +6,44 @@
  🚀 Feel free to fork the project: https://github.com/pH7Software/pH7-Social-Dating-CMS
  Commit your changes, then open a Pull Request for submitting your awesome changes 🎉
  */
+
+const STATIC_CACHE_NAME = 'sta_homepage';
+const DYNAMIC_CACHE_NAME = 'dyn_homepage';
+
+const CACHED_FILES = [
+    '/',
+    // TODO: Add more URLs to be cached here
+];
+
+self.addEventListener('install', async event => {
+    const cache = await caches.open(STATIC_CACHE_NAME);
+    cache.addAll(CACHED_FILES);
+});
+
+self.addEventListener('fetch', event => {
+    const {request} = event;
+    const url = new URL(request.url);
+
+    if (url.origin === location.origin) {
+        event.respondWith(cacheData(request));
+    } else {
+        event.respondWith(networkFirst(request));
+    }
+});
+
+async function cacheData(request) {
+    const cachedResponse = await caches.match(request);
+    return cachedResponse || fetch(request);
+}
+
+async function networkFirst(request) {
+    const cache = await caches.open(DYNAMIC_CACHE_NAME);
+
+    try {
+        const response = await fetch(request);
+        cache.put(request, response.clone());
+        return response;
+    } catch (error) {
+        return await cache.match(request);
+    }
+}
