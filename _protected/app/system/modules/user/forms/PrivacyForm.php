fc554254d6f5c14fa2d77f828427611bd103d133
Pierre-Henry Soria <hi@ph7.me>
2019-01-20 13:45:47 +1300

    Refactoring: Use PrivacyCore constants + Better Indentation + Make "Offline" translatable

--- _protected/app/system/modules/user/forms/PrivacyForm.php
+++ _protected/app/system/modules/user/forms/PrivacyForm.php
@@ -34,13 +34,70 @@ class PrivacyForm
         $oForm->addElement(new \PFBC\Element\Hidden('submit_privacy_account', 'form_privacy_account'));
         $oForm->addElement(new \PFBC\Element\Token('privacy_account'));
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<h3><u>' . t('Profile:') . '</u></h3>'));
-        $oForm->addElement(new \PFBC\Element\Radio(t('Who can view your profile?'), 'privacy_profile', ['all' => t('Everyone (including people who are not %0% members).', Registry::getInstance()->site_name), 'only_members' => t('Only %0% members who are logged in.', Registry::getInstance()->site_name), 'only_me' => t('Only me.')], ['value' => $oPrivacy->privacyProfile, 'required' => 1]));
+        $oForm->addElement(
+            new \PFBC\Element\Radio(
+                t('Who can view your profile?'),
+                'privacy_profile',
+                [
+                    PrivacyCore::ALL => t('Everyone (including people who are not %0% members).', Registry::getInstance()->site_name),
+                    PrivacyCore::ONLY_USERS => t('Only %0% members who are logged in.', Registry::getInstance()->site_name),
+                    PrivacyCore::ONLY_ME => t('Only me.')
+                ],
+                [
+                    'value' => $oPrivacy->privacyProfile,
+                    'required' => 1
+                ]
+            )
+        );
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<h3><u>' . t('Web search engine:') . '</u></h3>'));
-        $oForm->addElement(new \PFBC\Element\Radio(t('Do you want to be included in search results?'), 'search_profile', ['yes' => t("Yes, include my profile in search results (%site_name%'s search, Google, Bing, Yahoo, etc.)."), 'no' => t('No, do not include my profile in search results.')], ['value' => $oPrivacy->searchProfile, 'required' => 1]));
+        $oForm->addElement(
+            new \PFBC\Element\Radio(
+                t('Do you want to be included in search results?'),
+                'search_profile',
+                [
+                    PrivacyCore::YES => t("Yes, include my profile in search results (%site_name%'s search, Google, Bing, Yahoo, etc.)."),
+                    PrivacyCore::NO => t('No, do not include my profile in search results.')
+                ],
+                [
+                    'value' => $oPrivacy->searchProfile,
+                    'required' => 1
+                ]
+            )
+        );
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<h3><u>' . t('Show profile visitors:') . '</u></h3>'));
-        $oForm->addElement(new \PFBC\Element\Radio(t('Would you like to display members that have viewed your profile?'), 'user_save_views', ['yes' => t('Yes, display members who viewed my profile (Selecting this option will allow other members to see that you visited their profile).'), 'no' => t('No, don\'t display members who viewed my profile. (Selecting this option will prevent you from seeing who visited your profile).')], ['value' => $oPrivacy->userSaveViews, 'required' => 1]));
+        $oForm->addElement(
+            new \PFBC\Element\Radio(
+                t('Would you like to display members that have viewed your profile?'),
+                'user_save_views',
+                [
+                    PrivacyCore::YES => t('Yes, display members who viewed my profile (Selecting this option will allow other members to see that you visited their profile).'),
+                    PrivacyCore::NO => t('No, don\'t display members who viewed my profile. (Selecting this option will prevent you from seeing who visited your profile).')
+                ],
+                [
+                    'value' => $oPrivacy->userSaveViews,
+                    'required' => 1
+                ]
+            )
+        );
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<h3><u>' . t('Presence:') . '</u></h3>'));
-        $oForm->addElement(new \PFBC\Element\Select(t('Your status <div class="user_status right" id="status_div"></div>'), 'user_status', [UserModel::ONLINE_STATUS => t('Online'), UserModel::BUSY_STATUS => t('Busy'), UserModel::AWAY_STATUS => t('Away'), UserModel::OFFLINE_STATUS => 'Offline'], ['id' => 'status', 'onchange' => 'init_status()', 'value' => $oUserModel->getUserStatus($iProfileId), 'required' => 1]));
+        $oForm->addElement(
+            new \PFBC\Element\Select(
+                t('Your status <div class="user_status right" id="status_div"></div>'),
+                'user_status',
+                [
+                    UserModel::ONLINE_STATUS => t('Online'),
+                    UserModel::BUSY_STATUS => t('Busy'),
+                    UserModel::AWAY_STATUS => t('Away'),
+                    UserModel::OFFLINE_STATUS => t('Offline')
+                ],
+                [
+                    'id' => 'status',
+                    'onchange' => 'init_status()',
+                    'value' => $oUserModel->getUserStatus($iProfileId),
+                    'required' => 1
+                ]
+            )
+        );
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<script>$(function(){ init_status() });</script>'));
         $oForm->addElement(new \PFBC\Element\Button);
         $oForm->render();
