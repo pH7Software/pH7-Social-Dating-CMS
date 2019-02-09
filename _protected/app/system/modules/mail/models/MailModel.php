876e20aeae89c89afc120f01728d4e06e040e179
Pierre-Henry Soria <hi@ph7.me>
2019-02-09 14:52:32 +1300

    Use MySQL SET datatype's delimiter constant

--- _protected/app/system/modules/mail/models/MailModel.php
+++ _protected/app/system/modules/mail/models/MailModel.php
@@ -194,9 +194,9 @@ class MailModel extends MailCoreModel
         $oData = $this->getMsg($iMessageId);
         $sFieldId = $oData->sender === $iProfileId ? self::SENDER_DB_FIELD : self::RECIPIENT_DB_FIELD;
         if ($sMode === self::RESTOR_MODE) {
-            $sTrashVal = str_replace([$sFieldId, ','], '', $oData->trash);
+            $sTrashVal = str_replace([$sFieldId, Db::SET_DELIMITER], '', $oData->trash);
         } else {
-            $sTrashVal = ($oData->sender === $oData->recipient) ? 'sender,recipient' : $sFieldId . (!empty($oData->trash) ? ',' . $oData->trash : '');
+            $sTrashVal = ($oData->sender === $oData->recipient) ? 'sender,recipient' : $sFieldId . (!empty($oData->trash) ? Db::SET_DELIMITER . $oData->trash : '');
         }
         unset($oData);
 
