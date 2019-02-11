71ed5394537e9ee2cc36033cdb675d53ffc42a93
Pierre-Henry Soria <hi@ph7.me>
2019-02-11 18:23:03 +1300

    Minor update for sharing tweet message

--- _protected/app/system/modules/admin123/inc/class/TweetSharing.php
+++ _protected/app/system/modules/admin123/inc/class/TweetSharing.php
@@ -14,7 +14,7 @@ use PH7\Framework\Url\Url;
 final class TweetSharing
 {
     const TWITTER_TWEET_URL = 'https://twitter.com/intent/tweet?text=';
-    const TWITTER_TWEET_MSG = "I built my Social #DatingWebApp with #pH7CMS 😍, #DatingSoftware -> %0% \n%1% 🚀";
+    const TWITTER_TWEET_MSG = "I built my Social #DatingWebApp with #pH7CMS 😍\n#DatingSoftware -> %0% => %1% 🚀";
 
     /**
      * @return string
