b9d2f887687aac9ccdc74a207950d77da0305590
Pierre-Henry Soria <hi@ph7.me>
2019-02-17 10:14:47 +1300

    [CleanCode] Indentation Improved

--- _protected/app/system/modules/user/forms/processing/LoginFormProcess.php
+++ _protected/app/system/modules/user/forms/processing/LoginFormProcess.php
@@ -101,7 +101,10 @@ class LoginFormProcess extends Form implements LoginableForm
             if ($this->httpRequest->postExists('remember')) {
                 // We hash again the password
                 (new Framework\Cookie\Cookie)->set(
-                    ['member_remember' => Security::hashCookie($oUserData->password), 'member_id' => $oUserData->profileId]
+                    [
+                        'member_remember' => Security::hashCookie($oUserData->password),
+                        'member_id' => $oUserData->profileId
+                    ]
                 );
             }
 
@@ -126,7 +129,12 @@ class LoginFormProcess extends Form implements LoginableForm
                         )
                     );
                 } else {
-                    $oUser->setAuth($oUserData, $this->oUserModel, $this->session, $oSecurityModel);
+                    $oUser->setAuth(
+                        $oUserData,
+                        $this->oUserModel,
+                        $this->session,
+                        $oSecurityModel
+                    );
 
                     Header::redirect(
                         $sUrlRelocateAfterLogin,
