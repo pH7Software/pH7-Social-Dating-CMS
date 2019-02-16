8c876295a51195b07c17241679f5db69b0600588
Pierre-Henry Soria <hi@ph7.me>
2019-02-17 09:42:36 +1300

    Store useful constants in SmsVerification for sms-verification

---
+++ _protected/app/system/core/classes/SmsVerificationCore.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hello@ph7cms.com>
+ * @copyright      (c) 2019, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package        PH7 / App / System / Core / Class
+ */
+
+namespace PH7;
+
+class SmsVerificationCore
+{
+    const PROFILE_ID_SESS_NAME = 'sms_verification_profile_id';
+    const PHONE_NUMBER_SESS_NAME = 'user_phone_number';
+}
