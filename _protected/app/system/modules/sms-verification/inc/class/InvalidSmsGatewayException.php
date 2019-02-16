3aa755ace3f5e99077416c5a58982bc4116dccc2
Pierre-Henry Soria <hi@ph7.me>
2019-02-17 08:38:32 +1300

    SMSVerification: Add Provider + API class files

---
+++ _protected/app/system/modules/sms-verification/inc/class/InvalidSmsGatewayException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hello@ph7cms.com>
+ * @copyright      (c) 2019, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package        PH7 / App / System / Module / SMS Verification / Inc / Class
+ */
+
+namespace PH7;
+
+use PH7\Framework\Error\CException\PH7InvalidArgumentException;
+
+class InvalidSmsGatewayException extends PH7InvalidArgumentException
+{
+}
