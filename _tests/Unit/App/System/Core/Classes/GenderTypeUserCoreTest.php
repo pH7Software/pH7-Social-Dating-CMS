07b411c8c999dc405d8188afea00d331683ddf59
Pierre-Henry Soria <hi@ph7.me>
2019-02-17 16:52:18 +1300

    Rename GenderTypeUserCoreModelTest -> GenderTypeUserCoreTest + update references in there

--- _tests/Unit/App/System/Core/Classes/GenderTypeUserCoreTest.php
+++ _tests/Unit/App/System/Core/Classes/GenderTypeUserCoreTest.php
@@ -3,17 +3,17 @@
  * @author           Pierre-Henry Soria <hello@ph7cms.com>
  * @copyright        (c) 2018, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
- * @package          PH7 / Test / Unit / App / System / Core / Models
+ * @package          PH7 / Test / Unit / App / System / Core / Classes
  */
 
-namespace PH7\Test\Unit\App\System\Core\Models;
+namespace PH7\Test\Unit\App\System\Core\Classes;
 
 use PH7\GenderTypeUserCore;
 use PHPUnit_Framework_TestCase;
 
-require_once PH7_PATH_SYS . 'core/models/GenderTypeUserCoreModel.php';
+require_once PH7_PATH_SYS . 'core/classes/GenderTypeUserCore.php';
 
-class GenderTypeUserCoreModelTest extends PHPUnit_Framework_TestCase
+class GenderTypeUserCoreTest extends PHPUnit_Framework_TestCase
 {
     /**
      * @param string $sGender
