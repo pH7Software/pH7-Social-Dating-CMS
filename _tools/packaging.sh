1938253a359d51bb0adf5c09fb46af5adb2f843d
Pierre-Henry Soria <hi@ph7.me>
2019-02-18 14:02:18 +1300

    [#441] Update default permissions when packaging pH7CMS

--- _tools/packaging.sh
+++ _tools/packaging.sh
@@ -15,8 +15,21 @@ function run() {
     if [ $? -eq 1 ]; then
         _confirm "Have you made a copy of it before?"
         if [ $? -eq 1 ]; then
-            ## Permission
-            sudo chmod -R 777 .
+
+            ## Permissions
+            find . -type f -print0 | sudo xargs -0 chmod 666 # for files
+            find . -type d -print0 | sudo xargs -0 chmod 755 # for folders
+
+            sudo chmod -R 777 ./_install/data/logs/
+            sudo chmod -R 777 ./_install/data/caches/
+            sudo chmod -R 777 ./data/system/modules/*
+            sudo chmod -R 777 ./_repository/module/*
+            sudo chmod -R 777 ./_repository/upgrade/*
+            sudo chmod -R 777 ./_protected/app/configs/*
+            sudo chmod -R 777 ./_protected/data/cache/*
+            sudo chmod -R 777 ./_protected/data/backup/*
+            sudo chmod -R 777 ./_protected/data/tmp/*
+            sudo chmod -R 777 ./_protected/data/log/*
 
             ## TMP files
             find . -type f \( -name '*~' -or -name '*.log' -or -name '*.tmp' -or -name '*.swp' -or -name '.directory' -or -name '._*' -or -name '.DS_Store*' -or -name 'Thumbs.db' \) -exec rm {} \;
