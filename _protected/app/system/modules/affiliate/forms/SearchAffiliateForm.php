45b62c12c0a4a46d21386963abb0ee2ae541a641
Pierre-Henry Soria <hi@ph7.me>
2019-02-24 11:56:44 +1300

    [CleanUp][PFBC Forms] Improve indentation

--- _protected/app/system/modules/affiliate/forms/SearchAffiliateForm.php
+++ _protected/app/system/modules/affiliate/forms/SearchAffiliateForm.php
@@ -16,9 +16,41 @@ class SearchAffiliateForm
     {
         $oForm = new \PFBC\Form('form_search');
         $oForm->configure(['action' => Uri::get('affiliate', 'admin', 'browse') . PH7_SH, 'method' => 'get']);
-        $oForm->addElement(new \PFBC\Element\Search(t('Search an Affiliate:'), 'looking', ['description' => t('Enter their ID, First Name, Last Name, Username, Email, Bank Account, Sex or IP address.')]));
-        $oForm->addElement(new \PFBC\Element\Select(t('Browse By:'), 'order', [SearchCoreModel::USERNAME => t('Username'), SearchCoreModel::FIRST_NAME => t('First Name'), SearchCoreModel::LAST_NAME => t('Last Name'), SearchCoreModel::EMAIL => t('Email'), SearchCoreModel::PENDING_APPROVAL => t('Pending approval'), SearchCoreModel::LATEST => t('Latest'), SearchCoreModel::LAST_ACTIVITY => t('Last Activity'), SearchCoreModel::LAST_EDIT => t('Last Account Edit')]));
-        $oForm->addElement(new \PFBC\Element\Select(t('Direction:'), 'sort', [SearchCoreModel::ASC => t('Ascending'), SearchCoreModel::DESC => t('Descending')]));
+        $oForm->addElement(
+            new \PFBC\Element\Search(
+                t('Search an Affiliate:'),
+                'looking',
+                [
+                    'description' => t('Enter their ID, First Name, Last Name, Username, Email, Bank Account, Sex or IP address.')
+                ]
+            )
+        );
+        $oForm->addElement(
+            new \PFBC\Element\Select(
+                t('Browse By:'),
+                'order',
+                [
+                    SearchCoreModel::USERNAME => t('Username'),
+                    SearchCoreModel::FIRST_NAME => t('First Name'),
+                    SearchCoreModel::LAST_NAME => t('Last Name'),
+                    SearchCoreModel::EMAIL => t('Email'),
+                    SearchCoreModel::PENDING_APPROVAL => t('Pending approval'),
+                    SearchCoreModel::LATEST => t('Latest'),
+                    SearchCoreModel::LAST_ACTIVITY => t('Last Activity'),
+                    SearchCoreModel::LAST_EDIT => t('Last Account Edit')
+                ]
+            )
+        );
+        $oForm->addElement(
+            new \PFBC\Element\Select(
+                t('Direction:'),
+                'sort',
+                [
+                    SearchCoreModel::ASC => t('Ascending'),
+                    SearchCoreModel::DESC => t('Descending')
+                ]
+            )
+        );
         $oForm->addElement(new \PFBC\Element\Button(t('Search'), 'submit', ['icon' => 'search']));
         $oForm->render();
     }
