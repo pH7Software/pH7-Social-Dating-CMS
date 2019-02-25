6c4223b3aa31cb629afd01f256459299e0d2b321
Pierre-Henry Soria <hi@ph7.me>
2019-02-25 19:49:30 +1300

    [Upgrade][SQL] Fix wrong comments + Add wysiwygEditorForum option

--- _repository/upgrade/14.8.0-15.0.0/data/sql/PostgreSQL/upgrade.sql
+++ _repository/upgrade/14.8.0-15.0.0/data/sql/PostgreSQL/upgrade.sql
@@ -4,9 +4,13 @@
 -- License:       GNU General Public License
 --
 
--- Add the ability to enable/disave Session IP Check
+-- Add user text verification module
 INSERT INTO ph7_sys_mods_enabled (moduleTitle, folderName, premiumMod, enabled) VALUES
 ('SMS Verification', 'sms-verification', '0', '0');
+
+-- Allow to enable the WYSIWYG editor (CKEditor) for the forum posts
+INSERT INTO ph7_settings (settingName, settingValue, description, settingGroup) VALUES
+('wysiwygEditorForum', 0, 'Enable or not the WYSIWYG. 0 = Disable | 1 = Enable', 'general');
 
 
 -- Update pH7CMS's SQL schema version
