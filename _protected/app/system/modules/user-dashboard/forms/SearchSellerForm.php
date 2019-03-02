08160c76752f076248f19eb0670a2db75979b547
Pierre-Henry Soria <hi@ph7.me>
2019-03-02 16:13:05 +1300

    Add Only Listings from Date on UserDashboard Search form

--- _protected/app/system/modules/user-dashboard/forms/SearchSellerForm.php
+++ _protected/app/system/modules/user-dashboard/forms/SearchSellerForm.php
@@ -101,6 +101,12 @@ class SearchSellerForm
                 ['value' => 0, 'min' => 0]
             )
         );
+        $oForm->addElement(
+            new \PFBC\Element\Date(
+                t('Only show listings from this date'),
+                SearchQueryCore::FROM_DATE
+            )
+        );
         //$oForm->addElement(new \PFBC\Element\Select(t('Browse By:'), 'order', [SearchCoreModel::LATEST => t('Latest Members'), SearchCoreModel::LAST_ACTIVITY => t('Last Activity'), SearchCoreModel::VIEWS => t('Most Popular'), SearchCoreModel::RATING => t('Top Rated'), SearchCoreModel::USERNAME => t('Username'), SearchCoreModel::FIRST_NAME => t('First Name'), SearchCoreModel::LAST_NAME => t('Last Name'), SearchCoreModel::EMAIL => t('Email')]));
         $oForm->addElement(new \PFBC\Element\Select(t('Search Direction:'), 'sort', [SearchCoreModel::DESC => t('Descending'), SearchCoreModel::ASC => t('Ascending')]));
         $oForm->addElement(new \PFBC\Element\Button(t('Search'), 'submit', ['icon' => 'search']));
