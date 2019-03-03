3d0193347a4ed6f89c8af7ab3e986f56d03defbe
Pierre-Henry Soria <hi@ph7.me>
2019-03-03 13:24:51 +1300

    Registration: Add Registration Update Seller Info Field

---
+++ _protected/app/system/modules/realestate/models/query/join2.sql
@@ -0,0 +1,14 @@
+UPDATE [DB_PREFIX]members_info
+SET
+propertyPrice = :price
+propertyBedrooms = :bedrooms,
+propertyBathrooms = :bathrooms,
+propertySize = :house_size,
+propertyYearBuilt = :year_built,
+propertyHomeType = :home_type,
+propertyHomeStyle = :home_style,
+propertySquareFeet = :square_ft,
+propertyLotSize = :lot_size,
+propertyGarageSpaces = :garage_spaces,
+propertyCarportSpaces = :carport_spaces
+WHERE profileId = :profile_id LIMIT 1;
