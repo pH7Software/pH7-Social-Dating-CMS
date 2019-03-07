496b8fd6e3670a067dd861a33f1cf64da8fdf07c
Pierre-Henry Soria <hi@ph7.me>
2019-03-07 19:28:00 +1300

    Remove upgrade patches (not needed for this project)

--- _repository/upgrade/0.1.2-0.1.3/data/sql/MySQL/upgrade.sql
+++
@@ -1,7 +0,0 @@
---
--- Author:        Pierre-Henry Soria <ph7software@gmail.com>
--- Copyright:     (c) 2013-2018, Pierre-Henry Soria. All Rights Reserved.
--- License:       GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
---
-
-ALTER TABLE `pH7_Messages` MODIFY `sendDate` datetime NOT NULL DEFAULT '0000-00-00 00:00:00';
--- _repository/upgrade/1.0.0-1.0.1/data/sql/MySQL/upgrade.sql
+++
@@ -1,9 +0,0 @@
---
--- Author:        Pierre-Henry Soria <ph7software@gmail.com>
--- Copyright:     (c) 2013-2018, Pierre-Henry Soria. All Rights Reserved.
--- License:       GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
---
-
-ALTER TABLE pH7_AdminsAttemptsLogin ADD UNIQUE KEY (ip);
-ALTER TABLE pH7_MembersAttemptsLogin ADD UNIQUE KEY (ip);
-ALTER TABLE pH7_AffiliatesLogLogin ADD UNIQUE KEY (ip);
