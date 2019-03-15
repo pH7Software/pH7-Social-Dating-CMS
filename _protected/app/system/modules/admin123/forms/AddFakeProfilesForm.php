01bf180165a255b0c9a4f2120a4c461dfe46b6c1
Pierre-Henry Soria <hi@ph7.me>
2019-03-15 19:43:12 +1300

    Improve PDBC param indentation

--- _protected/app/system/modules/admin123/forms/AddFakeProfilesForm.php
+++ _protected/app/system/modules/admin123/forms/AddFakeProfilesForm.php
@@ -28,9 +28,41 @@ class AddFakeProfilesForm
         $oForm->configure(['action' => '']);
         $oForm->addElement(new \PFBC\Element\Hidden('submit_add_fake_profiles', 'form_add_fake_profiles'));
         $oForm->addElement(new \PFBC\Element\Token('fake_profiles'));
-        $oForm->addElement(new \PFBC\Element\Select(t('Number of Profile:'), 'num', [1, 5, 10, 15, 25], ['description' => t('Number of fake profiles to add in the same time. Choosing 15 or 25 profiles might takes a few minutes.'), 'required' => 1]));
-        $oForm->addElement(new \PFBC\Element\Select(t('Gender:'), 'sex', ['both' => t('Gentlemen &amp; Ladies'), 'male' => t('Only Gentlemen'), 'female' => t('Only Ladies')], ['required' => 1]));
-        $oForm->addElement(new \PFBC\Element\Select(t('Nationality:'), 'nat', static::getNationalities(), ['required' => 1]));
+        $oForm->addElement(
+            new \PFBC\Element\Select(
+                t('Number of Profile:'),
+                'num',
+                [1, 5, 10, 15, 25],
+                [
+                    'description' => t('Number of fake profiles to add in the same time. Choosing 15 or 25 profiles might takes a few minutes.'),
+                    'required' => 1
+                ]
+            )
+        );
+        $oForm->addElement(
+            new \PFBC\Element\Select(
+                t('Gender:'),
+                'sex',
+                [
+                    'both' => t('Gentlemen &amp; Ladies'),
+                    'male' => t('Only Gentlemen'),
+                    'female' => t('Only Ladies')
+                ],
+                [
+                    'required' => 1
+                ]
+            )
+        );
+        $oForm->addElement(
+            new \PFBC\Element\Select(
+                t('Nationality:'),
+                'nat',
+                static::getNationalities(),
+                [
+                    'required' => 1
+                ]
+            )
+        );
         $oForm->addElement(new \PFBC\Element\Button(t('Add Fake Profiles!')));
         $oForm->render();
     }
