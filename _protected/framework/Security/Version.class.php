e0399cba8e9f0b91a8ffcf56f17ba5017d10d017
Pierre-Henry Soria <hi@ph7.me>
2019-05-08 21:18:12 +1200

    Update Release Date :date:

--- _protected/framework/Security/Version.class.php
+++ _protected/framework/Security/Version.class.php
@@ -52,7 +52,7 @@ final class Version
      */
     const KERNEL_VERSION = '15.1.0';
     const KERNEL_BUILD = '1';
-    const KERNEL_RELEASE_DATE = '2019-04-14';
+    const KERNEL_RELEASE_DATE = '2019-05-13';
 
     /***** Framework Server *****/
     const KERNEL_TECHNOLOGY_NAME = 'pH7CMS.com';
