bcc9276858de768cdd5c8afe2f82771d64f7fa8a
Pierre-Henry Soria <hi@ph7.me>
2019-05-10 22:25:13 +1200

    [phpdoc]Remove unnecessary @version/@title tags

--- _install/index.php
+++ _install/index.php
@@ -1,13 +1,10 @@
 <?php
 /**
- * @title            Index File
- *
  * @author           Pierre-Henry Soria <hello@ph7cms.com>
  * @copyright        (c) 2012-2019, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
- * @link             http://ph7cms.com
+ * @link             https://ph7cms.com
  * @package          PH7 / Install
- * @version          1.3
  */
 
 define('PH7', 1);
