186e748ff77dadf31ac5cded88a343de89a301a0
Pierre-Henry Soria <hi@ph7.me>
2019-05-23 10:49:51 +0200

    Add InvalidAlgorithmException class :smiley:

---
+++ _protected/framework/Security/InvalidAlgorithmException.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @author           Pierre-Henry Soria <hello@ph7cms.com>
+ * @copyright        (c) 2019, Pierre-Henry Soria. All Rights Reserved.
+ * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ * @package          PH7 / Framework / Security
+ */
+
+namespace PH7\Framework\Security;
+
+use PH7\Framework\Error\CException\PH7InvalidArgumentException;
+
+class InvalidAlgorithmException extends PH7InvalidArgumentException
+{
+}
