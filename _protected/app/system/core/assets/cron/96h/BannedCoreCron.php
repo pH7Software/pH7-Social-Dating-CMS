e6b3cef3a2e076f89dd089b7b251d8c1e3ba411f
Pierre-Henry Soria <hi@ph7.me>
2019-07-27 21:56:17 +0200

    Chmod to 777 ip.txt (in case it may work)

--- _protected/app/system/core/assets/cron/96h/BannedCoreCron.php
+++ _protected/app/system/core/assets/cron/96h/BannedCoreCron.php
@@ -16,6 +16,7 @@ defined('PH7') or exit('Restricted access');
 use Exception;
 use GuzzleHttp\Client;
 use PH7\Framework\Error\Logger;
+use PH7\Framework\File\Permission\Chmod;
 use PH7\Framework\Security\Ban\Ban;
 
 /** Reset time limit and increase memory **/
@@ -252,6 +253,8 @@ class BannedCoreCron extends Cron
         if ($this->invalidNewIp()) {
             return false;
         }
+
+        $this->file->chmod(self::BANNED_IP_FILE_PATH, Chmod::MODE_ALL_EXEC);
 
         foreach ($this->aNewIps as $sIp) {
             $this->addIp($sIp);
