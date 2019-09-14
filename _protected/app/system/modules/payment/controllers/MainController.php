7e47b8768194d7e2ef8665ee04de4edd7f5f8ee2
Pierre-Henry Soria <hi@ph7.me>
2019-09-15 02:15:28 +0900

    [Payment Module] Catch exceptions from Skeerel Pay library

--- _protected/app/system/modules/payment/controllers/MainController.php
+++ _protected/app/system/modules/payment/controllers/MainController.php
@@ -14,6 +14,7 @@ namespace PH7;
 use Braintree_Transaction;
 use DateInterval;
 use DateTime;
+use Exception;
 use PH7\Framework\Cache\Cache;
 use PH7\Framework\Core\Kernel;
 use PH7\Framework\File\File;
@@ -386,31 +387,35 @@ class MainController extends Controller
 
     private function skeerelHandler()
     {
-        if (\Skeerel\Skeerel::verifyAndRemoveSessionStateParameter(
-            $this->httpRequest->get('state')
-        )) {
-            $oSkeerel = new \Skeerel\Skeerel(
-                $this->config->values['module.setting']['skeerel.website_id'],
-                $this->config->values['module.setting']['skeerel.website_secret']
-            );
-            $oResult = $oSkeerel->getData($this->httpRequest->get('token'));
+        try {
+            if (\Skeerel\Skeerel::verifyAndRemoveSessionStateParameter(
+                $this->httpRequest->get('state')
+            )) {
+                $oSkeerel = new \Skeerel\Skeerel(
+                    $this->config->values['module.setting']['skeerel.website_id'],
+                    $this->config->values['module.setting']['skeerel.website_secret']
+                );
+                $oResult = $oSkeerel->getData($this->httpRequest->get('token'));
 
-            if ($oResult->status) {
-                $iItemNumber = $this->httpRequest->post('item_number');
-                if ($this->oUserModel->updateMembership(
-                    $iItemNumber,
-                    $this->iProfileId,
-                    $this->dateTime->get()->dateTime(UserCoreModel::DATETIME_FORMAT)
-                )) {
-                    $this->bStatus = true; // Status is OK
-                    $this->updateUserGroupId($iItemNumber);
-                    $this->notification(Skeerel::class, $iItemNumber);
+                if ($oResult->status) {
+                    $iItemNumber = $this->httpRequest->post('item_number');
+                    if ($this->oUserModel->updateMembership(
+                        $iItemNumber,
+                        $this->iProfileId,
+                        $this->dateTime->get()->dateTime(UserCoreModel::DATETIME_FORMAT)
+                    )) {
+                        $this->bStatus = true; // Status is OK
+                        $this->updateUserGroupId($iItemNumber);
+                        $this->notification(Skeerel::class, $iItemNumber);
+                    }
                 }
+            } else {
+                $this->design->setMessage(
+                    t('Payment state parameter cannot be verified.')
+                );
             }
-        } else {
-            $this->design->setMessage(
-                t('Error occurred while processing transaction.')
-            );
+        } catch (Exception $oE) {
+            $this->design->setMessage($this->str->escape($oE->getMessage(), true));
         }
     }
 
