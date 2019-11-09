21057a91a29a403b653cc47a4d845f3eb7c1a34c
Pierre-Henry Soria <hi@ph7.me>
2019-11-10 05:04:54 +1100

    Remove extra blank line in method's body

--- _protected/app/system/modules/admin123/controllers/SettingController.php
+++ _protected/app/system/modules/admin123/controllers/SettingController.php
@@ -77,7 +77,6 @@ class SettingController extends Controller
         $this->view->page_title = $this->view->h1_title = t('Banner Settings');
         $this->view->h4_title = nt('%n% Banner', '%n% Banners', $iTotalAds);
         $this->output();
-
     }
 
     public function addAds()
