56075cb3608b13d83e305529238e9614318d5e33
Pierre-Henry Soria <hi@ph7.me>
2019-11-14 06:25:20 +1300

    Update email address in file header comment

--- _protected/framework/Payment/Gateway/Api/PayPal.class.php
+++ _protected/framework/Payment/Gateway/Api/PayPal.class.php
@@ -2,11 +2,11 @@
 /**
  * @title            PayPal Class
  *
- * @author           Pierre-Henry Soria <ph7software@gmail.com>
+ * @author           Pierre-Henry Soria <hello@ph7cms.com>
  * @copyright        (c) 2012-2019, Pierre-Henry Soria. All Rights Reserved.
  * @license          GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Framework / Payment / Gateway / Api
- * @version          1.3
+ * @version          1.4
  */
 
 namespace PH7\Framework\Payment\Gateway\Api;
