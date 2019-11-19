ff4e640aa3e3bf6657abe45c6a4396a67e2f530a
Pierre-Henry Soria <hi@ph7.me>
2019-11-20 04:41:54 +1100

    Refactoring condition into clear/readable name

--- _protected/app/system/modules/admin123/forms/DisableModuleForm.php
+++ _protected/app/system/modules/admin123/forms/DisableModuleForm.php
@@ -12,10 +12,11 @@ use PH7\Framework\Mvc\Model\DbConfig;
 use PH7\Framework\Mvc\Model\Module as ModuleModel;
 use PH7\Framework\Mvc\Router\Uri;
 use PH7\Framework\Url\Header;
+use stdClass;
 
 class DisableModuleForm
 {
-    const DEV_STAGE_MODS = [
+    const DEV_STAGE_MODULES = [
         'connect'
     ];
 
@@ -49,7 +50,7 @@ class DisableModuleForm
                 $sAdditionalText .= ' • <a class="small" href="' . Uri::get(PH7_ADMIN_MOD, 'setting', 'general') . '#p=api">' . t('Change the default Chat by yours') . '</a>';
             }
 
-            if (in_array($oData->folderName, self::DEV_STAGE_MODS, true)) {
+            if (self::isModuleInDevStage($oData)) {
                 $sAdditionalText .= ' • <span class="small red">' . t('Only for development purpose to test it before <a href="%0%">opening a PR</a>. <a href="%1%">Social APIs</a> have to be updated.', 'https://github.com/pH7Software/pH7-Social-Dating-CMS/pulls', 'https://github.com/pH7Software/pH7-Social-Dating-CMS/blob/master/_protected/app/system/modules/connect/inc/class/') . '</span>';
             }
 
@@ -64,5 +65,15 @@ class DisableModuleForm
         $oForm->addElement(new \PFBC\Element\Checkbox('', 'module_id', $aModuleNames, ['value' => $aSelectedMods]));
         $oForm->addElement(new \PFBC\Element\Button(t('Save'), 'submit', ['icon' => 'check']));
         $oForm->render();
+    }
+
+    /**
+     * @param stdClass $oData
+     *
+     * @return bool
+     */
+    private static function isModuleInDevStage(stdClass $oData)
+    {
+        return in_array($oData->folderName, self::DEV_STAGE_MODULES, true);
     }
 }
