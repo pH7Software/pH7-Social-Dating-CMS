e8aa25feb22abab9a37de41bb6d825dac805c27e
Pierre-Henry Soria <hi@ph7.me>
2019-11-21 14:53:45 +0100

    [Report Form] Fix indentation; Shorter line length

--- _protected/app/system/modules/report/forms/ReportForm.php
+++ _protected/app/system/modules/report/forms/ReportForm.php
@@ -34,8 +34,36 @@ class ReportForm
         $oForm->addElement(new \PFBC\Element\Hidden('spammer', $oHttpRequest->get('spammer'), ['required' => 1]));
         $oForm->addElement(new \PFBC\Element\Hidden('url', $oHttpRequest->get('url'), ['validation' => new \PFBC\Validation\Url]));
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<h3 class="center">' . t('Do your want to report this?') . '</h4>'));
-        $oForm->addElement(new \PFBC\Element\Select(t('Type the Content'), 'type', ['user' => t('Profile'), 'avatar' => t('Avatar'), 'mail' => t('Message'), 'comment' => t('Comment'), 'picture' => t('Photo'), 'video' => t('Video'), 'forum' => t('Forum'), 'note' => t('Note')], ['value' => $oHttpRequest->get('type'), 'required' => 1]));
-        $oForm->addElement(new \PFBC\Element\Textarea(t('Comment:'), 'desc', ['title' => t('Please tell us why you want to report this content (scam, illegal content, adult content, etc.). Help us to eliminate scams, fake profiles, spam ... Thank you'), 'required' => 1]));
+        $oForm->addElement(
+            new \PFBC\Element\Select(
+                t('Type the Content'),
+                'type',
+                [
+                    'user' => t('Profile'),
+                    'avatar' => t('Avatar'),
+                    'mail' => t('Message'),
+                    'comment' => t('Comment'),
+                    'picture' => t('Photo'),
+                    'video' => t('Video'),
+                    'forum' => t('Forum'),
+                    'note' => t('Note')
+                ],
+                [
+                    'value' => $oHttpRequest->get('type'),
+                    'required' => 1
+                ]
+            )
+        );
+        $oForm->addElement(
+            new \PFBC\Element\Textarea(
+                t('Comment:'),
+                'desc',
+                [
+                    'title' => t('Please tell us why you want to report this content (scam, illegal content, adult content, etc.). Help us to eliminate scams, fake profiles, spam ... Thank you'),
+                    'required' => 1
+                ]
+            )
+        );
         $oForm->addElement(new \PFBC\Element\Button(t('Report It'), 'submit', ['icon' => 'check']));
         $oForm->addElement(new \PFBC\Element\Button(t('Cancel'), 'cancel', ['onclick' => 'parent.$.colorbox.close();return false', 'icon' => 'cancel']));
         $oForm->addElement(new \PFBC\Element\HTMLExternal('<script src="' . PH7_URL_STATIC . PH7_JS . 'str.js"></script>'));
