49306e70f51f9db898277f2671c09023051d29e6
Pierre-Henry Soria <hi@ph7.me>
2019-12-07 23:40:43 +0100

    [Installation Wizard] Add emoji to make a sentence more attractive

--- _install/langs/en/install.lang.php
+++ _install/langs/en/install.lang.php
@@ -134,7 +134,7 @@ $LANG = array(
     'note_able_to_change_niche_settings_later' => 'Please note that you will be able to change the template and enable/disable the modules later in your admin panel.',
     'will_you_make_donation' => '😇 Will you help me to maintain &amp; improve the software?',
     'donate_here' => 'Subscribe now to be a patron 🏆',
-    'or_paypal_donation' => 'And/Or, you can also contribute through PayPal 🤩',
+    'or_paypal_donation' => '💸 And/Or, you can also contribute through PayPal 🤩',
     'warning_no_js' => 'JavaScript is disabled on your Web browser!<br />
         Please enable JavaScript via the options of your Web browser in order to use this website.',
     'admin_url' => 'Admin Panel URL',
