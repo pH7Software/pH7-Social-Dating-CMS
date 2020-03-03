6ac2072ccf7ee494b3817f2fd6263a4c4c492b92
♚ PH⑦ de Soria™♛ <hi@ph7.me>
2020-03-03 20:14:51 +1100

    Remove unused default Codeigniter framework setting variable

--- _protected/framework/Security/Validate/config_filter.inc.php
+++ _protected/framework/Security/Validate/config_filter.inc.php
@@ -42,7 +42,6 @@ $config['cookie_secure'] = true;
 | 'csrf_cookie_name' = The cookie name
 | 'csrf_expire' = The number in seconds the token should expire.
 */
-$config['csrf_protection'] = true;
 $config['csrf_token_name'] = 'csrf_test_name';
 $config['csrf_cookie_name'] = 'csrf_cookie_name';
 $config['csrf_expire'] = 7200;
