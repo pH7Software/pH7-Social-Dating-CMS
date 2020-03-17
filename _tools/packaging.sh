7108e1927dce07cf8a8bccf6fc535b2ba2b99c7c
Pierre-Henry Soria <hi@ph7.me>
2020-03-17 10:49:32 +1000

    Bump Copyright Year in Tool Bash files

--- _tools/packaging.sh
+++ _tools/packaging.sh
@@ -7,7 +7,7 @@
 #                  (e.g., you@you:/path/to/root-project$ bash _tools/pH7.sh).
 #
 # Author:          Pierre-Henry Soria <hello@ph7cms.com>
-# Copyright:       (c) 2014-2019, Pierre-Henry Soria. All Rights Reserved.
+# Copyright:       (c) 2014-2020, Pierre-Henry Soria. All Rights Reserved.
 # License:         GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
 ##
 
