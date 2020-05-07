abc09d523b6368afd2a52502adf8fdbdaec5827a
Pierre-Henry Soria <hi@ph7.me>
2020-05-07 19:22:49 +1000

    Update gettext language strings

--- _protected/app/system/core/classes/SecurityCore.php
+++ _protected/app/system/core/classes/SecurityCore.php
@@ -34,12 +34,11 @@ class SecurityCore
         $sFirstName = $this->getUserFirstNameFromEmail($sTo, $sTable);
         $sForgotPwdLink = Uri::get('lost-password', 'main', 'forgot', Various::convertTableToMod($sTable));
 
-        $oView->content = t('Dear, %0%', $sFirstName) . '<br />' .
+        $oView->content = t('Hi %0%', $sFirstName) . '<br />' .
             t('Someone tried to login more than %0% times with the IP address: "%1%".', $iMaxAttempts, $sIp) . '<br />' .
             t('For safety and security reasons, we have blocked access to this person for a delay of %0% minutes.', $iAttemptTime) . '<br /><ol><li>' .
             t('If it was you who tried to login to your account, we suggest to <a href="%1%">request a new password</a> in %0% minutes.', $iAttemptTime, $sForgotPwdLink) . '</li><li>' .
-            t('If you do not know the person who made the login attempts, you should be very careful and change your password to a new one more complicated.') . '<br />' .
-            t('We also recommend that you change the password of your emailbox, because it is with this emalbox we send a potential new password in case you forget it.') . '</li></ol><br /><hr />' .
+            t('If you do not know the person who made the login attempts, you should be very careful and change your password to a new complex one.') . '</li></ol><br /><hr />' .
             t('Have a nice day!');
 
         $sMessageHtml = $oView->parseMail(
