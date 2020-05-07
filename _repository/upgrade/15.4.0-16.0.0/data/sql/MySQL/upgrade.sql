8c74fbf3c5ecc71039e3d93853c8e3b934e4afed
Pierre-Henry Soria <hi@ph7.me>
2020-05-07 15:20:24 +1000

    [v16.0.0] Add migration SQL queries

---
+++ _repository/upgrade/15.4.0-16.0.0/data/sql/MySQL/upgrade.sql
@@ -0,0 +1,44 @@
+--
+-- Author:        Pierre-Henry Soria <hello@ph7cms.com>
+-- Copyright:     (c) 2020, Pierre-Henry Soria. All Rights Reserved.
+-- License:       GNU General Public License
+--
+
+-- Remove outdated fields
+ALTER TABLE ph7_admins_log_sess
+    DROP COLUMN sessionHash,
+    DROP COLUMN idHash,
+    DROP COLUMN lastActivity,
+    DROP COLUMN location,
+    DROP COLUMN password;
+
+ALTER TABLE ph7_members_log_sess
+    DROP COLUMN sessionHash,
+    DROP COLUMN idHash,
+    DROP COLUMN lastActivity,
+    DROP COLUMN location,
+    DROP COLUMN password;
+
+ALTER TABLE ph7_affiliates_log_sess
+    DROP COLUMN sessionHash,
+    DROP COLUMN idHash,
+    DROP COLUMN lastActivity,
+    DROP COLUMN location,
+    DROP COLUMN password;
+
+-- Remove outdated keys
+ALTER TABLE ph7_admins_log_sess
+    DROP INDEX sessionHash,
+    DROP INDEX lastActivity;
+
+ALTER TABLE ph7_members_log_sess
+    DROP INDEX sessionHash,
+    DROP INDEX lastActivity;
+
+ALTER TABLE ph7_affiliates_log_sess
+    DROP INDEX sessionHash,
+    DROP INDEX lastActivity;
+
+
+-- Update pH7CMS's SQL schema version
+UPDATE ph7_modules SET version = '1.5.9' WHERE vendorName = 'pH7CMS';
---
+++ _repository/upgrade/15.4.0-16.0.0/info/conclusion.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @author         Pierre-Henry Soria <hello@ph7cms.com>
+ * @copyright      (c) 2020, Pierre-Henry Soria. All Rights Reserved.
+ * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
+ */
+
+namespace PH7;
+
+defined('PH7') or exit('Restricted access');
+
+// Default contents value
+$sHtml = '';
+
+/*** Begin Contents ***/
+
+$sHtml .= t("Hooray! It's all done by now! :)");
+
+/*** End Contents ***/
+
+// Output!
+return $sHtml;
