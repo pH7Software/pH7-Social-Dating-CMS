55f55e0a2ac1928ffeb2486b7e90b5c9c5c922b3
Pierre-Henry Soria <hi@ph7.me>
2020-05-07 23:52:29 +1000

    Make the SQL migration script simpler
    *_log_sess tables werent used before. So, can just be dropped and be recreated. Will be much simpler and avoid potential SQL issues.

--- _repository/upgrade/15.4.0-16.0.0/data/sql/MySQL/upgrade.sql
+++ _repository/upgrade/15.4.0-16.0.0/data/sql/MySQL/upgrade.sql
@@ -4,46 +4,53 @@
 -- License:       GNU General Public License
 --
 
--- Remove outdated fields
-ALTER TABLE ph7_admins_log_sess
-    DROP COLUMN sessionHash,
-    DROP COLUMN idHash,
-    DROP COLUMN userAgent,
-    DROP COLUMN lastActivity,
-    DROP COLUMN location,
-    DROP COLUMN password,
-    DROP COLUMN guest;
+-- Remove outdated tables
+DROP TABLE ph7_admins_log_sess;
+DROP TABLE ph7_members_log_sess;
+DROP TABLE ph7_affiliates_log_sess;
 
-ALTER TABLE ph7_members_log_sess
-    DROP COLUMN sessionHash,
-    DROP COLUMN idHash,
-    DROP COLUMN userAgent,
-    DROP COLUMN lastActivity,
-    DROP COLUMN location,
-    DROP COLUMN password,
-    DROP COLUMN guest;
 
-ALTER TABLE ph7_affiliates_log_sess
-    DROP COLUMN sessionHash,
-    DROP COLUMN idHash,
-    DROP COLUMN userAgent,
-    DROP COLUMN lastActivity,
-    DROP COLUMN location,
-    DROP COLUMN password,
-    DROP COLUMN guest;
 
--- Remove outdated keys
-ALTER TABLE ph7_admins_log_sess
-    DROP INDEX sessionHash,
-    DROP INDEX lastActivity;
+CREATE TABLE IF NOT EXISTS ph7_admins_log_sess (
+  sessionId int(10) unsigned NOT NULL AUTO_INCREMENT,
+  profileId tinyint(3) unsigned NOT NULL,
+  username varchar(40) DEFAULT NULL,
+  email varchar(120) DEFAULT NULL,
+  firstName varchar(50) DEFAULT NULL,
+  lastName varchar(50) DEFAULT NULL,
+  ip varchar(45) NOT NULL DEFAULT '127.0.0.1',
+  dateTime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (sessionId),
+  FOREIGN KEY (profileId) REFERENCES ph7_admins(profileId)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE ph7_members_log_sess
-    DROP INDEX sessionHash,
-    DROP INDEX lastActivity;
 
-ALTER TABLE ph7_affiliates_log_sess
-    DROP INDEX sessionHash,
-    DROP INDEX lastActivity;
+CREATE TABLE IF NOT EXISTS ph7_members_log_sess (
+  sessionId int(10) unsigned NOT NULL AUTO_INCREMENT,
+  profileId int(10) unsigned NOT NULL,
+  username varchar(40) DEFAULT NULL,
+  email varchar(120) DEFAULT NULL,
+  firstName varchar(50) DEFAULT NULL,
+  lastName varchar(50) DEFAULT NULL,
+  ip varchar(45) NOT NULL DEFAULT '127.0.0.1',
+  dateTime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (sessionId),
+  FOREIGN KEY (profileId) REFERENCES ph7_members(profileId)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE IF NOT EXISTS ph7_affiliates_log_sess (
+  sessionId int(10) unsigned NOT NULL AUTO_INCREMENT,
+  profileId int(10) unsigned NOT NULL,
+  username varchar(40) DEFAULT NULL,
+  email varchar(120) DEFAULT NULL,
+  firstName varchar(50) DEFAULT NULL,
+  lastName varchar(50) DEFAULT NULL,
+  ip varchar(45) NOT NULL DEFAULT '127.0.0.1',
+  dateTime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (sessionId),
+  FOREIGN KEY (profileId) REFERENCES ph7_affiliates(profileId)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 -- Update pH7CMS's SQL schema version
