16e6dbf701bd9ebc1b871f2a53c00e0c49869741
Pierre-Henry Soria <hi@ph7.me>
2020-06-08 11:43:57 +0930

    [Refactoring] Changed Disclaimer functions to a class
    And use localStorage instead of traditional cookies

--- static/js/disclaimer.js
+++ static/js/disclaimer.js
@@ -5,18 +5,19 @@
  */
 
 const disagreedRedirectLink = "https://google.com";
-const cookieName = 'agreed18';
-let dialogStatus = 0;
+const localKeyName = 'agreed18';
 
 $(document).ready(function () {
-    if (!doesCookieExist(cookieName)) {
-        centerPopup();
-        loadDisclaimer();
+    let oDisclaimer = new Disclaimer();
+
+    if (!oDisclaimer.isAccepted()) {
+        oDisclaimer.centerDialog();
+        oDisclaimer.load();
     }
 
     $('#agree-over18').on('click', function () {
-        disableDisclaimer();
-        document.cookie = cookieName + '=true';
+        oDisclaimer.disable();
+        oDisclaimer.setAccepted();
     });
 
     $('#disagree-under18').on('click', function () {
@@ -24,51 +25,63 @@ $(document).ready(function () {
     });
 });
 
-function loadDisclaimer() {
-    if (dialogStatus == 0) {
-        $('#disclaimer-background').css({
-            'opacity': 0.95
+class Disclaimer {
+    constructor() {
+        this.dialogStatus = 0;
+    }
+
+    load() {
+        if (this.dialogStatus == 0) {
+            $('#disclaimer-background').css({
+                'opacity': 0.95
+            });
+            $('#disclaimer-background').fadeIn('slow');
+            $('#disclaimer-window').fadeIn('slow');
+            this.dialogStatus = 1;
+        }
+    }
+
+    disable() {
+        if (this.dialogStatus == 1) {
+            $('#disclaimer-window').fadeOut('slow');
+            $('#disclaimer-background').fadeOut('slow');
+            this.dialogStatus = 0;
+        }
+    }
+
+    centerDialog() {
+        const windowWidth = document.documentElement.clientWidth;
+        const windowHeight = document.documentElement.clientHeight;
+
+        const popupHeight = $('#disclaimer-window').height();
+        const popupWidth = $('#disclaimer-window').width();
+
+        $('#disclaimer-window').css({
+            "position": "absolute",
+            "top": windowHeight / 2 - popupHeight / 2,
+            "left": windowWidth / 2 - popupWidth / 2
         });
-        $('#disclaimer-background').fadeIn('slow');
-        $('#disclaimer-window').fadeIn('slow');
-        dialogStatus = 1;
+
+        $('#disclaimer-background').css({
+            "height": windowHeight
+        });
     }
-}
 
-function disableDisclaimer() {
-    if (dialogStatus == 1) {
-        $('#disclaimer-window').fadeOut('slow');
-        $('#disclaimer-background').fadeOut('slow');
-        dialogStatus = 0;
+    isAccepted() {
+        try {
+            return localStorage.getItem(localKeyName);
+        } catch (e) {
+            console.log('Cannot use localStorage', e);
+        }
+
+        return null;
     }
-}
 
-function centerPopup() {
-    const windowWidth = document.documentElement.clientWidth;
-    const windowHeight = document.documentElement.clientHeight;
-
-    const popupHeight = $('#disclaimer-window').height();
-    const popupWidth = $('#disclaimer-window').width();
-
-    $('#disclaimer-window').css({
-        "position": "absolute",
-        "top": windowHeight / 2 - popupHeight / 2,
-        "left": windowWidth / 2 - popupWidth / 2
-    });
-
-    $('#disclaimer-background').css({
-        "height": windowHeight
-    });
-}
-
-function doesCookieExist(name) {
-    const nameEQ = name + "=";
-    const ca = document.cookie.split(';');
-
-    for (let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    setAccepted() {
+        try {
+            localStorage.setItem(localKeyName, '1');
+        } catch (e) {
+            console.log('Cannot use localStorage', e);
+        }
     }
-    return null;
 }
