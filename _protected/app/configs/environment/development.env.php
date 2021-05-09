157da6b46fee8860e722f657d14447e9229838fa
Pierre-Henry Soria <hi@ph7.me>
2021-05-10 00:51:52 +0930

    [Cleanup] Env error reporting option files

--- _protected/app/configs/environment/development.env.php
+++ _protected/app/configs/environment/development.env.php
@@ -4,7 +4,7 @@
  *
  * @author         Pierre-Henry Soria <hello@ph7cms.com>
  * @link           http://ph7cms.com
- * @copyright      (c) 2012-2019, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright      (c) 2012-2021, Pierre-Henry Soria. All Rights Reserved.
  * @license        GNU General Public License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package        PH7 / App / Config / Environment
  */
@@ -13,11 +13,13 @@ namespace PH7;
 
 defined('PH7') or exit('Restricted access');
 
-// If php.ini is inadequate, we fix it.
+define('PH7_ENV_ENABLED', 'On');
+
+// If php.ini is inadequate, let's fix it
 error_reporting(E_ALL); // Since PHP 5.4 E_STRICT became part of E_ALL
-ini_set('display_errors', 'On');
-ini_set('display_startup_errors', 'On');
-ini_set('track_errors', 'On');
-ini_set('html_errors', 'On');
+ini_set('display_errors', PH7_ENV_ENABLED);
+ini_set('display_startup_errors', PH7_ENV_ENABLED);
+ini_set('track_errors', PH7_ENV_ENABLED);
+ini_set('html_errors', PH7_ENV_ENABLED);
 ini_set('docref_root', 'http://php.net/manual/');
 ini_set('memory_limit', '356M');
