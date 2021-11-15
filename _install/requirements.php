7f4628320fed3a4f8cb1c2c86175170280ab98d3
Pierre-Henry Soria <hi@ph7.me>
2021-11-16 07:07:46 +1030

    Refactor bool condition into readable function
    As `version_compare()` can is not always very readable when reading the code quickly. That way, it will help a dev reading and understand the code even quicker

--- _install/requirements.php
+++ _install/requirements.php
@@ -25,7 +25,7 @@ define('DIRECTIVE_KEY', 'directive');
 
 $aErrors = array();
 
-if (version_compare(PHP_VERSION, PH7_REQUIRED_SERVER_VERSION, '<')) {
+if (does_meet_minimum_required_php_version()) {
     $aErrors[] = 'Your current PHP version is ' . PHP_VERSION . '. pH7CMS requires PHP ' . PH7_REQUIRED_SERVER_VERSION . ' or newer.<br /> Please ask your Web host to upgrade PHP to ' . PH7_REQUIRED_SERVER_VERSION . ' or newer.';
 }
 
@@ -102,6 +102,10 @@ if ($iErrors > 0) {
     exit(1);
 }
 
+function does_meet_minimum_required_php_version()
+{
+    return version_compare(PHP_VERSION, PH7_REQUIRED_SERVER_VERSION, '<');
+}
 
 function display_html_header($sPageTitle)
 {
