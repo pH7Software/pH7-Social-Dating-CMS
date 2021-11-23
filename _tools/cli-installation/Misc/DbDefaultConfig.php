a5ae9c082136afef74a2658f69ac69193f54f225
Pierre-Henry Soria <hi@ph7.me>
2021-11-24 08:09:28 +1030

    Add SQL default values + SQL Queries

---
+++ _tools/cli-installation/Misc/DbDefaultConfig.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace PH7\Cli\Installer\Misc;
+
+final class DbDefaultConfig
+{
+    const HOSTNAME = 'localhost';
+    const USERNAME = 'root';
+    const NAME = 'ph7cms';
+    const PREFIX = 'ph7_';
+    const PORT = '3306';
+    const CHARSET = 'utf8mb4';
+}
---
+++ _tools/cli-installation/Misc/SqlQuery.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace PH7\Cli\Installer\Misc;
+
+final class SqlQuery
+{
+    public const ADD_ADMIN = 'INSERT INTO %s (profileId , username, password, email, firstName, lastName, joinDate, lastActivity, ip)
+    VALUES (1, :username, :password, :email, :firstName, :lastName, :joinDate, :lastActivity, :ip)';
+
+    public const UPDATE_SITE_NAME = "UPDATE %s SET settingValue = :siteName WHERE settingName = 'siteName' OR settingName = 'watermarkTextImage' OR settingName = 'emailName'";
+
+    public const UPDATE_ADMIN_EMAIL = "UPDATE %s SET settingValue = :adminEmail WHERE settingName = 'adminEmail' LIMIT 1";
+
+    public const UPDATE_FEEDBACK_EMAIL = "UPDATE %s SET settingValue = :feedbackEmail WHERE settingName = 'feedbackEmail' LIMIT 1";
+
+    public const UPDATE_RETURN_EMAIL = "UPDATE %s SET settingValue = :returnEmail WHERE settingName = 'returnEmail' LIMIT 1";
+
+    public const UPDATE_THEME = "UPDATE %s SET settingValue = :theme WHERE settingName = :setting LIMIT 1";
+
+    public const UPDATE_SYS_MODULE = "UPDATE %s SET enabled = :status WHERE folderName = :modName LIMIT 1";
+}
