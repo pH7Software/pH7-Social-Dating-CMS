032ba5ae7fd396d6a706d7432368d75a817535ce
Pierre-Henry Soria <hi@ph7.me>
2022-01-12 07:58:32 +1030

    [SecurityCheck] Update paragraph message

--- _protected/app/configs/environment/production.env.php
+++ _protected/app/configs/environment/production.env.php
@@ -30,8 +30,8 @@ ini_set('html_errors', PH7_ENV_DISABLED);
 if (is_dir(PH7_PATH_ROOT . '_install/')) {
     $sMsg = '<p class="warning">Security Alert – <a href="' . Framework\Core\Kernel::SOFTWARE_WEBSITE . '">pH7Builder</a></p>
      <p class="error">Please remove "_install/" folder from your server before continuing.</p>
-     <p>You can delete this folder using an FTP client (FileZilla or other).</p>
-     <p>You can also delete this folder with an SSH connection if your host allows. Below is the UNIX command to do this is:<br />
+     <p>You can delete this folder using an FTP client (FileZilla or other) or through cPanel File Manager.</p>
+     <p>You can also delete this folder with an SSH connection if your host allows. Below is the UNIX command for doing this:<br />
      <code>sudo rm -rf /YOUR-PUBLIC-SERVER-PATH/YOUR-WEBSITE/_install/</code></p>
      <p>→ Once done, please <a href="' . PH7_URL_ROOT . '">reload pH7Builder</a></p>';
 
