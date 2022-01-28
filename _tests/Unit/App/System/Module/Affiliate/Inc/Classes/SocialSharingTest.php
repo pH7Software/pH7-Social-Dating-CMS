93d9b5991bb71690b7e28e11c691536158246da9
Pierre-Henry Soria <hi@ph7.me>
2022-01-28 21:21:07 +1030

    [UnitTest] Fix missing class import

--- _tests/Unit/App/System/Module/Affiliate/Inc/Classes/SocialSharingTest.php
+++ _tests/Unit/App/System/Module/Affiliate/Inc/Classes/SocialSharingTest.php
@@ -13,8 +13,9 @@ namespace PH7\Test\Unit\App\System\Module\Affiliate\Inc\Classes;
 require_once PH7_PATH_SYS_MOD . 'affiliate/inc/class/SocialSharing.php';
 
 use PH7\SocialSharing;
+use PHPUnit\Framework\TestCase;
 
-class SocialSharingTest extends TestCaset
+class SocialSharingTest extends TestCase
 {
     public function testGetMessage(): void
     {
