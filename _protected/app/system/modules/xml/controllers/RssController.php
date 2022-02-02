pH7Software/pH7-Social-Dating-CMS

commit 04e25143a526bc357c51dfe37dec694c40cc8118

GH-946 Remove "game" module from "xnl" module

_protected/app/system/modules/xml/controllers/RssController.php
@@ -79,11 +79,6 @@ class RssController extends MainController implements XmlControllable
             case 'comment-note':
             case 'comment-picture':
             case 'comment-video':
-            case 'comment-game':
-                $this->view->setCaching(false); // We disable the cache since they are dynamic pages managed by the router
-                $this->sAction = 'comment.inc';
-                break;
-
             case 'forum-post':
                 if ($this->isParamValid($mParam)) {
                     $this->view->setCaching(false); // We disable the cache since they are dynamic pages managed by the router

_protected/app/system/modules/xml/controllers/SitemapController.php
@@ -80,19 +80,11 @@ class SitemapController extends MainController implements XmlControllable
             case 'comment':
             case 'picture':
             case 'video':
-            case 'game':
-                $this->sAction = $sAction;
-                break;
-
             case 'comment-profile':
             case 'comment-blog':
             case 'comment-note':
             case 'comment-picture':
             case 'comment-video':
-            case 'comment-game':
-                $this->view->setCaching(false); // Disable the cache since they are dynamic pages managed by the router
-                $this->sAction = 'comment.inc';
-                break;
 
             default:
                 $this->displayPageNotFound(t('Sitemap Not Found!'));

_install/data/sql/MySQL/pH7_DataGame.sql
@@ -1,83 +1,0 @@
---
---
--- Title:         SQL Data Game Install File
---
--- Author:        Pierre-Henry Soria <hello@ph7cms.com>
--- Copyright:     (c) 2012-2019, Pierre-Henry Soria. All Rights Reserved.
--- License:       MIT License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
--- Package:       PH7 / Install / Data / Sql / MySQL
---
---
-
-SET @sCurrentDate = CURRENT_TIMESTAMP;
-
-
-INSERT INTO ph7_games_categories (categoryId, name) VALUES
-(1, 'Action'),
-(2, 'Arcade'),
-(3, 'Casino'),
-(4, 'Drawing'),
-(5, 'Fighting'),
-(6, 'Other'),
-(7, 'Puzzle'),
-(8, 'Racing'),
-(9, 'Retro'),
-(10, 'Shooting'),
-(11, 'Sports');
-
-
-INSERT INTO ph7_games (gameId, name, title, description, keywords, thumb, file, categoryId, addedDate, downloads, votes, score, views) VALUES
--- Action (1)
-(4, '', 'Flashblox', 'Tetris clone', '', 'tetris_clone.png', 'tetris_clone.swf', 1, @sCurrentDate, 0, 0, 0, 0),
-(5, '', 'Bell Boys', 'Help the bell boy to deliver their orders to the right floor by controling the elevators', '', 'bell_boys.png', 'bell_boys.swf', 1, @sCurrentDate, 0, 0, 0, 0),
-(6, '', 'Frogger', 'This is a real classic simple and addictive. Just help the frog to cross the street with heavy traffic', '', 'frogger.png', 'frogger.swf', 1, @sCurrentDate, 0, 0, 0, 0),
-
--- Arcade (2)
-(1, '', 'America Fights Back', 'Think you can do better in the war? Well heres your chance.Use your mouse and click to fire your guns! Watch your energy level to make sure you dont die. There are no second chances or extra', 'America Fights Back', 'america_strikes_back.png', 'america_strikes_back.swf', 2, @sCurrentDate, 0, 0, 0, 0),
-(2, '', 'Altex', 'Be fast and shoot them all!', 'Altex', 'altex.png', 'altex.swf', 2, @sCurrentDate, 0, 0, 0, 0),
-(3, '', 'Alpine Escape', 'Catch the brides falling from the airship but don''t shoot it down - shoot down the fighter plane in', 'Alpine Escape', 'alpine_escape.png', 'alpine_escape.swf', 2, @sCurrentDate, 0, 0, 0, 0),
-
--- Casino (3)
-(7, '', 'Blackjack', 'This is a nice Blackjack games that allow playing upto 5 hands at once', '', 'blackjack.png', 'blackjack.swf', 3, @sCurrentDate, 0, 0, 0, 0),
-(8, '', 'The Blackjack Casino', 'One of the most popular Casino games', '', 'blackjack_casino.png', 'blackjack_casino.swf', 3, @sCurrentDate, 0, 0, 0, 0),
-(9, '', 'Casino - Let It Ride', 'Click on the chip amount you wish to bet. Press DEAL button to get cards. Determine if your hand is worth keeping or dropping one of your bets.', '', 'let_ride.png', 'let_ride.swf', 3, @sCurrentDate, 0, 0, 0, 0),
-
--- Drawing (4)
-(10, '', 'Boeing', 'NA', 'Boeing', 'boeing.jpg', 'boeing.swf', 4, @sCurrentDate, 0, 0, 0, 0),
-(11, '', 'Book', 'NA', 'Book', 'book.jpg', 'book.swf', 4, @sCurrentDate, 0, 0, 0, 0),
-(12, '', 'Burj_al_Arab', 'NA', 'Burj_al_Arab', 'burj_al_arab.jpg', 'burj_al_arab.swf', 4, @sCurrentDate, 0, 0, 0, 0),
-
--- Fighting (5)
-(13, '', 'Asteroid Field+', 'Hit as many asteriods as possible in the asteroid field', '', 'asteroid.png', 'asteroid.swf', 5, @sCurrentDate, 0, 0, 0, 0),
-(14, '', 'Brighton Bounty Hunter', 'Shoot all that you can see on screen', '', 'brighton_bounty_hunter.png', 'brighton_bounty_hunter.swf', 5, @sCurrentDate, 0, 0, 0, 0),
-(15, '', 'Sheepteroids', 'It is Sheep-teroids that we are shooting!', '', 'sheepteroids.png', 'sheepteroids.swf', 5, @sCurrentDate, 0, 0, 0, 0),
-
--- Other (6)
-(16, '', 'Blackjack Fever', 'Blackjack Las Vegas style - Double Down Split and More - Get your practice in before making your t', 'Blackjack Fever', 'bjfever.png', 'bjfever.swf', 6, @sCurrentDate, 0, 0, 0, 0),
-(17, '', 'Beermat', 'A game about fliking beermats.', 'Beermat', 'beermat.png', 'beermat.swf', 6, @sCurrentDate, 0, 0, 0, 0),
-(18, '', 'Beeku Big Adventure Ch1', 'Run around as Beeku blast away bugs and critters with your machine gun.', 'Beeku''s Big Adventure Ch1', 'beeku_big_adventure.png', 'beeku_big_adventure.swf', 6, @sCurrentDate, 0, 0, 0, 0),
-
--- Puzzle (7)
-(19, '', 'Addem Up', 'Place the next tile from the queue on the board. If all surrounding tiles add up to that number they', 'Addem Up', 'addem_up.png', 'addem_up.swf', 7, @sCurrentDate, 0, 0, 0, 0),
-(20, '', 'Aga Maze', 'A mazing game', 'Aga Maze', 'aga_maze.png', 'aga_maze.swf', 7, @sCurrentDate, 0, 0, 0, 0),
-(21, '', 'Air Heads', 'Fly your balloon across building but avoid the smog above.', 'Air Heads', 'air_heads.png', 'air_heads.swf', 7, @sCurrentDate, 0, 0, 0, 0),
-
--- Racing (8)
-(22, '', 'Stai Ruere', 'Protesters have gathered around a barracks area! It''s your duty to control them and maintain peace a', 'Stai Ruere', 'stai_ruere.png', 'stai_ruere.swf', 8, @sCurrentDate, 0, 0, 0, 0),
-(23, '', 'Stay The Distance', 'Great horse racing game. Easy but a bit harsh.', 'Stay The Distance', 'stay_the_distance.png', 'stay_the_distance.swf', 8, @sCurrentDate, 0, 0, 0, 0),
-(24, '', 'TGFG Racing', 'Race around the track smashing into your opponents to knock them off and picking up turbo so you can', 'TGFG Racing', 'tgfg_racing.png', 'tgfg_racing.swf', 8, @sCurrentDate, 0, 0, 0, 0),
-
--- Retro (9)
-(25, '', '12 Puzzle', 'Fit the puzzle pieces together to solve the puzzle!', '12 Puzzle', '12_puzzle.png', '12_puzzle.swf', 9, @sCurrentDate, 0, 0, 0, 0),
-(26, '', '24 Puzzle', 'Align the 24 numbers in order from 1-24 in this 3D puzzle.', '24 Puzzle', '24_puzzle.png', '24_puzzle.swf', 9, @sCurrentDate, 0, 0, 0, 0),
-(27, '', '3D Frogger', 'Frogger takes on a new look in this 3 dimensional version of the classic game. Make your way across', '3D Frogger', '3d_frogger.png', '3d_frogger.swf', 9, @sCurrentDate, 0, 0, 0, 0),
-
--- Shooting (10)
-(28, '', 'Amok Madman', 'Get tought by an expert in this fun shooting game.', 'Amok Madman', 'amok.png', 'amok.swf', 10, @sCurrentDate, 0, 0, 0, 0),
-(29, '', 'Aderans Forest', 'Aderans Forest is under attack. Shoot all the enemies before they destroy it! Click the mouse to shoot.', 'Aderans Forest', 'aderans_forest.png', 'aderans_forest.swf', 10, @sCurrentDate, 0, 0, 0, 0),
-(30, '', 'AO-War On Iraq', 'Shoot down Iraqi Terrorists Jeeps Grenaders and hostile villagers using AK Machine guns.', 'AO-War On Iraq', 'war_on_iraq.png', 'war_on_iraq.swf', 10, @sCurrentDate, 0, 0, 0, 0),
-
--- Sports (11)
-(31, '', 'Bullseye', 'Play this classic darts game.', 'Bullseye', 'bullseye.png', 'bullseye.swf', 11, @sCurrentDate, 0, 0, 0, 0),
-(32, '', 'Billiards', 'Billiards Flash Game. So famous, you have to play with our Cue Sports game right now. Finally, our Billiards flash game is simple to use, smart and modern.', 'Cue Sports, Cuesports, Billiard, Billiards, Snooker, poll, Billiards balls, Pool Balls', 'billiards.png', 'billiards.swf', 11, @sCurrentDate, 0, 0, 0, 0),
-(33, '', 'Bowling', 'Here you are. A superb bowling game. Turn your speakers up!Throw the ball when you see the sign. In order to throw the ball push the go botton. Keep it pressed until the yellow field at t', 'Bowling', 'bowling.png', 'bowling.swf', 11, @sCurrentDate, 0, 0, 0, 0);

_install/data/sql/MySQL/pH7_SchemaGame.sql
@@ -1,35 +1,0 @@
---
---
--- Title:         SQL Schema Game Install File
---
--- Author:        Pierre-Henry Soria <hello@ph7cms.com>
--- Copyright:     (c) 2012-2019, Pierre-Henry Soria. All Rights Reserved.
--- License:       MIT License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
--- Package:       PH7 / Install / Data / Sql / MySQL
---
---
-
-CREATE TABLE IF NOT EXISTS ph7_games (
-  gameId int(10) unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(120) DEFAULT '',
-  title varchar(120) NOT NULL,
-  description varchar(191) NOT NULL,
-  keywords varchar(191) DEFAULT '',
-  thumb varchar(191) NOT NULL,
-  file varchar(191) NOT NULL,
-  categoryId tinyint(4) unsigned NOT NULL DEFAULT '0',
-  addedDate timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  downloads int(9) unsigned DEFAULT '0',
-  votes int(9) unsigned DEFAULT '0',
-  score float unsigned DEFAULT '0',
-  views int(10) unsigned DEFAULT '0',
-  PRIMARY KEY (gameId)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1;
-
-
-CREATE TABLE IF NOT EXISTS ph7_games_categories (
-  categoryId smallint(4) unsigned NOT NULL AUTO_INCREMENT,
-  name varchar(40) NOT NULL DEFAULT '',
-  PRIMARY KEY (categoryId),
-  UNIQUE KEY (name)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1;

_install/data/sql/PostgreSQL/pH7_DataGame.sql
@@ -1,83 +1,0 @@
---
---
--- Title:         SQL Data Game Install File
---
--- Author:        Pierre-Henry Soria <hello@ph7cms.com>
--- Copyright:     (c) 2017-2019, Pierre-Henry Soria. All Rights Reserved.
--- License:       MIT License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
--- Package:       PH7 / Install / Data / Sql / PostgreSQL
---
---
-
-@sCurrentDate := CURRENT_TIMESTAMP;
-
-
-INSERT INTO ph7_games_categories (categoryId, name) VALUES
-(1, 'Action'),
-(2, 'Arcade'),
-(3, 'Casino'),
-(4, 'Drawing'),
-(5, 'Fighting'),
-(6, 'Other'),
-(7, 'Puzzle'),
-(8, 'Racing'),
-(9, 'Retro'),
-(10, 'Shooting'),
-(11, 'Sports');
-
-
-INSERT INTO ph7_games (gameId, name, title, description, keywords, thumb, file, categoryId, addedDate, downloads, votes, score, views) VALUES
--- Action (1)
-(4, '', 'Flashblox', 'Tetris clone', '', 'tetris_clone.png', 'tetris_clone.swf', 1, @sCurrentDate, 0, 0, 0, 0),
-(5, '', 'Bell Boys', 'Help the bell boy to deliver their orders to the right floor by controling the elevators', '', 'bell_boys.png', 'bell_boys.swf', 1, @sCurrentDate, 0, 0, 0, 0),
-(6, '', 'Frogger', 'This is a real classic simple and addictive. Just help the frog to cross the street with heavy traffic', '', 'frogger.png', 'frogger.swf', 1, @sCurrentDate, 0, 0, 0, 0),
-
--- Arcade (2)
-(1, '', 'America Fights Back', 'Think you can do better in the war? Well heres your chance.Use your mouse and click to fire your guns! Watch your energy level to make sure you dont die. There are no second chances or extra', 'America Fights Back', 'america_strikes_back.png', 'america_strikes_back.swf', 2, @sCurrentDate, 0, 0, 0, 0),
-(2, '', 'Altex', 'Be fast and shoot them all!', 'Altex', 'altex.png', 'altex.swf', 2, @sCurrentDate, 0, 0, 0, 0),
-(3, '', 'Alpine Escape', 'Catch the brides falling from the airship but don''t shoot it down - shoot down the fighter plane in', 'Alpine Escape', 'alpine_escape.png', 'alpine_escape.swf', 2, @sCurrentDate, 0, 0, 0, 0),
-
--- Casino (3)
-(7, '', 'Blackjack', 'This is a nice Blackjack games that allow playing upto 5 hands at once', '', 'blackjack.png', 'blackjack.swf', 3, @sCurrentDate, 0, 0, 0, 0),
-(8, '', 'The Blackjack Casino', 'One of the most popular Casino games', '', 'blackjack_casino.png', 'blackjack_casino.swf', 3, @sCurrentDate, 0, 0, 0, 0),
-(9, '', 'Casino - Let It Ride', 'Click on the chip amount you wish to bet. Press DEAL button to get cards. Determine if your hand is worth keeping or dropping one of your bets.', '', 'let_ride.png', 'let_ride.swf', 3, @sCurrentDate, 0, 0, 0, 0),
-
--- Drawing (4)
-(10, '', 'Boeing', 'NA', 'Boeing', 'boeing.jpg', 'boeing.swf', 4, @sCurrentDate, 0, 0, 0, 0),
-(11, '', 'Book', 'NA', 'Book', 'book.jpg', 'book.swf', 4, @sCurrentDate, 0, 0, 0, 0),
-(12, '', 'Burj_al_Arab', 'NA', 'Burj_al_Arab', 'burj_al_arab.jpg', 'burj_al_arab.swf', 4, @sCurrentDate, 0, 0, 0, 0),
-
--- Fighting (5)
-(13, '', 'Asteroid Field+', 'Hit as many asteriods as possible in the asteroid field', '', 'asteroid.png', 'asteroid.swf', 5, @sCurrentDate, 0, 0, 0, 0),
-(14, '', 'Brighton Bounty Hunter', 'Shoot all that you can see on screen', '', 'brighton_bounty_hunter.png', 'brighton_bounty_hunter.swf', 5, @sCurrentDate, 0, 0, 0, 0),
-(15, '', 'Sheepteroids', 'It is Sheep-teroids that we are shooting!', '', 'sheepteroids.png', 'sheepteroids.swf', 5, @sCurrentDate, 0, 0, 0, 0),
-
--- Other (6)
-(16, '', 'Blackjack Fever', 'Blackjack Las Vegas style - Double Down Split and More - Get your practice in before making your t', 'Blackjack Fever', 'bjfever.png', 'bjfever.swf', 6, @sCurrentDate, 0, 0, 0, 0),
-(17, '', 'Beermat', 'A game about fliking beermats.', 'Beermat', 'beermat.png', 'beermat.swf', 6, @sCurrentDate, 0, 0, 0, 0),
-(18, '', 'Beeku Big Adventure Ch1', 'Run around as Beeku blast away bugs and critters with your machine gun.', 'Beeku''s Big Adventure Ch1', 'beeku_big_adventure.png', 'beeku_big_adventure.swf', 6, @sCurrentDate, 0, 0, 0, 0),
-
--- Puzzle (7)
-(19, '', 'Addem Up', 'Place the next tile from the queue on the board. If all surrounding tiles add up to that number they', 'Addem Up', 'addem_up.png', 'addem_up.swf', 7, @sCurrentDate, 0, 0, 0, 0),
-(20, '', 'Aga Maze', 'A mazing game', 'Aga Maze', 'aga_maze.png', 'aga_maze.swf', 7, @sCurrentDate, 0, 0, 0, 0),
-(21, '', 'Air Heads', 'Fly your balloon across building but avoid the smog above.', 'Air Heads', 'air_heads.png', 'air_heads.swf', 7, @sCurrentDate, 0, 0, 0, 0),
-
--- Racing (8)
-(22, '', 'Stai Ruere', 'Protesters have gathered around a barracks area! It''s your duty to control them and maintain peace a', 'Stai Ruere', 'stai_ruere.png', 'stai_ruere.swf', 8, @sCurrentDate, 0, 0, 0, 0),
-(23, '', 'Stay The Distance', 'Great horse racing game. Easy but a bit harsh.', 'Stay The Distance', 'stay_the_distance.png', 'stay_the_distance.swf', 8, @sCurrentDate, 0, 0, 0, 0),
-(24, '', 'TGFG Racing', 'Race around the track smashing into your opponents to knock them off and picking up turbo so you can', 'TGFG Racing', 'tgfg_racing.png', 'tgfg_racing.swf', 8, @sCurrentDate, 0, 0, 0, 0),
-
--- Retro (9)
-(25, '', '12 Puzzle', 'Fit the puzzle pieces together to solve the puzzle!', '12 Puzzle', '12_puzzle.png', '12_puzzle.swf', 9, @sCurrentDate, 0, 0, 0, 0),
-(26, '', '24 Puzzle', 'Align the 24 numbers in order from 1-24 in this 3D puzzle.', '24 Puzzle', '24_puzzle.png', '24_puzzle.swf', 9, @sCurrentDate, 0, 0, 0, 0),
-(27, '', '3D Frogger', 'Frogger takes on a new look in this 3 dimensional version of the classic game. Make your way across', '3D Frogger', '3d_frogger.png', '3d_frogger.swf', 9, @sCurrentDate, 0, 0, 0, 0),
-
--- Shooting (10)
-(28, '', 'Amok Madman', 'Get tought by an expert in this fun shooting game.', 'Amok Madman', 'amok.png', 'amok.swf', 10, @sCurrentDate, 0, 0, 0, 0),
-(29, '', 'Aderans Forest', 'Aderans Forest is under attack. Shoot all the enemies before they destroy it! Click the mouse to shoot.', 'Aderans Forest', 'aderans_forest.png', 'aderans_forest.swf', 10, @sCurrentDate, 0, 0, 0, 0),
-(30, '', 'AO-War On Iraq', 'Shoot down Iraqi Terrorists Jeeps Grenaders and hostile villagers using AK Machine guns.', 'AO-War On Iraq', 'war_on_iraq.png', 'war_on_iraq.swf', 10, @sCurrentDate, 0, 0, 0, 0),
-
--- Sports (11)
-(31, '', 'Bullseye', 'Play this classic darts game.', 'Bullseye', 'bullseye.png', 'bullseye.swf', 11, @sCurrentDate, 0, 0, 0, 0),
-(32, '', 'Billiards', 'Billiards Flash Game. So famous, you have to play with our Cue Sports game right now. Finally, our Billiards flash game is simple to use, smart and modern.', 'Cue Sports, Cuesports, Billiard, Billiards, Snooker, poll, Billiards balls, Pool Balls', 'billiards.png', 'billiards.swf', 11, @sCurrentDate, 0, 0, 0, 0),
-(33, '', 'Bowling', 'Here you are. A superb bowling game. Turn your speakers up!Throw the ball when you see the sign. In order to throw the ball push the go botton. Keep it pressed until the yellow field at t', 'Bowling', 'bowling.png', 'bowling.swf', 11, @sCurrentDate, 0, 0, 0, 0);

_install/data/sql/PostgreSQL/pH7_SchemaGame.sql
@@ -1,43 +1,0 @@
---
---
--- Title:         SQL Schema Game Install File
---
--- Author:        Pierre-Henry Soria <hello@ph7cms.com>
--- Copyright:     (c) 2017-2019, Pierre-Henry Soria. All Rights Reserved.
--- License:       MIT License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
--- Package:       PH7 / Install / Data / Sql / PostgreSQL
---
---
-
-CREATE SEQUENCE ph7_games_seq;
-
-CREATE TABLE IF NOT EXISTS ph7_games (
-  gameId int check (gameId > 0) NOT NULL DEFAULT NEXTVAL ('ph7_games_seq'),
-  name varchar(120) DEFAULT '',
-  title varchar(120) NOT NULL,
-  description varchar(255) NOT NULL,
-  keywords varchar(255) DEFAULT '',
-  thumb varchar(200) NOT NULL,
-  file varchar(200) NOT NULL,
-  categoryId smallint check (categoryId > 0) NOT NULL DEFAULT '0',
-  addedDate timestamp(0) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  downloads int check (downloads > 0) DEFAULT '0',
-  votes int check (votes > 0) DEFAULT '0',
-  score double precision check (score > 0) DEFAULT '0',
-  views int check (views > 0) DEFAULT '0',
-  PRIMARY KEY (gameId)
-)  ;
-
-ALTER SEQUENCE ph7_games_seq RESTART WITH 1;
-
-
-CREATE SEQUENCE ph7_games_categories_seq;
-
-CREATE TABLE IF NOT EXISTS ph7_games_categories (
-  categoryId smallint check (categoryId > 0) NOT NULL DEFAULT NEXTVAL ('ph7_games_categories_seq'),
-  name varchar(40) NOT NULL DEFAULT '',
-  PRIMARY KEY (categoryId),
-  UNIQUE (name)
-)  ;
-
-ALTER SEQUENCE ph7_games_categories_seq RESTART WITH 1;

_protected/app/system/core/models/GameCoreModel.php
@@ -1,55 +1,0 @@
-<?php
-/**
- * @author         Pierre-Henry Soria <hello@ph7cms.com>
- * @copyright      (c) 2012-2019, Pierre-Henry Soria. All Rights Reserved.
- * @license        MIT License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
- * @package        PH7 / App / System / Core / Model
- */
-
-namespace PH7;
-
-use PDO;
-use PH7\Framework\Mvc\Model\Engine\Db;
-use PH7\Framework\Mvc\Model\Engine\Model;
-use stdClass;
-
-class GameCoreModel extends Model
-{
-    const CACHE_GROUP = 'db/sys/mod/game';
-    const CACHE_TIME = 93312000;
-
-    /**
-     * @param string|null $sTitle
-     * @param int|null $iGameId
-     * @param int $iOffset
-     * @param int $iLimit
-     * @param string $sOrder
-     *
-     * @return array|stdClass
-     */
-    public function get($sTitle, $iGameId, $iOffset, $iLimit, $sOrder = SearchCoreModel::NAME)
-    {
-        $iOffset = (int)$iOffset;
-        $iLimit = (int)$iLimit;
-        $bIsTitle = $sTitle !== null;
-        $bIsGameId = $iGameId !== null;
-
-        $sOrderBy = SearchCoreModel::order($sOrder, SearchCoreModel::DESC);
-
-        $sSqlGameId = ($bIsTitle && $bIsGameId) ? ' WHERE title LIKE :title AND gameId =:gameId ' : '';
-        $rStmt = Db::getInstance()->prepare('SELECT * FROM' . Db::prefix(DbTableName::GAME) . $sSqlGameId . $sOrderBy . 'LIMIT :offset, :limit');
-        if ($bIsTitle) {
-            $rStmt->bindValue(':title', $sTitle . '%', PDO::PARAM_STR);
-        }
-        if ($bIsGameId) {
-            $rStmt->bindValue(':gameId', $iGameId, PDO::PARAM_INT);
-        }
-        $rStmt->bindParam(':offset', $iOffset, PDO::PARAM_INT);
-        $rStmt->bindParam(':limit', $iLimit, PDO::PARAM_INT);
-        $rStmt->execute();
-        $mData = $bIsGameId ? $rStmt->fetch(PDO::FETCH_OBJ) : $rStmt->fetchAll(PDO::FETCH_OBJ);
-        Db::free($rStmt);
-
-        return $mData;
-    }
-}

_protected/app/system/modules/xml/controllers/MainController.php
@@ -62,7 +62,6 @@ class MainController extends Controller
         $this->view->pictures = $this->oDataModel->getPictures();
         $this->view->albums_videos = $this->oDataModel->getAlbumsVideos();
         $this->view->videos = $this->oDataModel->getVideos();
-        $this->view->games = $this->oDataModel->getGames();
 
         // For the Comments
         $this->generateCommentRouter($sAction, $mParam);
@@ -126,11 +125,6 @@ class MainController extends Controller
             case 'comment-video':
                 $this->view->table = 'video';
                 $this->view->comments = $this->isParamValid($mParam) ? $this->oDataModel->getRecipientCommentsVideos($mParam) : $this->oDataModel->getCommentsVideos();
-                break;
-
-            case 'comment-game':
-                $this->view->table = 'game';
-                $this->view->comments = $this->isParamValid($mParam) ? $this->oDataModel->getRecipientCommentsGames($mParam) : $this->view->comments = $this->oDataModel->getCommentsGames();
                 break;
         }
     }

commit 79f9e4b9b6626ba999cb10b39dd0303f582012a7

BugFix: Rss/Sitemap didnt output correct view for comments
Regression from https://github.com/pH7Software/pH7-Social-Dating-CMS/commit/04e25143a526bc357c51dfe37dec694c40cc8118\#diff-239806b04c040d24e0881bc9eb101350960a4124d1b425d6ced9dc282e197158 - https://github.com/pH7Software/pH7-Social-Dating-CMS/issues/946

_protected/app/system/modules/xml/controllers/RssController.php
@@ -76,14 +76,23 @@ class RssController extends MainController implements XmlControllable
             case 'forum-topic':
                 $this->sAction = $sAction;
                 break;
+
             case 'comment-profile':
             case 'comment-blog':
             case 'comment-note':
             case 'comment-picture':
             case 'comment-video':
+                // We disable the cache because of dynamic pages managed by the router
+                $this->view->setCaching(
+                    false
+                );
+                $this->sAction = 'comment.inc';
+                break;
+
             case 'forum-post':
                 if ($this->isParamValid($mParam)) {
-                    $this->view->setCaching(false); // We disable the cache since they are dynamic pages managed by the router
+                    // We disable the cache since they are dynamic pages managed by the router
+                    $this->view->setCaching(false);
                     $this->view->forums_messages = $this->oDataModel->getForumsMessages($mParam);
                     $this->sAction = $sAction;
                 } else {

_protected/app/system/modules/xml/controllers/SitemapController.php
@@ -78,11 +78,18 @@ class SitemapController extends MainController implements XmlControllable
             case 'comment':
             case 'picture':
             case 'video':
+                $this->sAction = $sAction;
+                break;
+
             case 'comment-profile':
             case 'comment-blog':
             case 'comment-note':
             case 'comment-picture':
             case 'comment-video':
+                // Disable the cache since they are dynamic pages managed by the router
+                $this->view->setCaching(false);
+                $this->sAction = 'comment.inc';
+                break;
 
             default:
                 $this->displayPageNotFound(t('Sitemap Not Found!'));