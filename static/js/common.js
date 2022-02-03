14a59f93381577b70cfe67bf88980a5cfd8d6e7b
Pierre-Henry Soria <hi@ph7.me>
2022-02-03 19:41:58 +1030

    Remove unused "Boad Anchor" `pH7Include` function

--- static/js/common.js
+++ static/js/common.js
@@ -5,58 +5,7 @@
  */
 
 /**
- * Allows you to include a JavaScript file in an HTML page.
- *
- *
- * @example
- *     // With some files
- *     var aFiles['foo.js', 'bar.js', 'foobar.js'];
- *     pH7Include(aFiles);
- *
- *
- * @example
- *     // With a file
- *     pH7Include('foobar.js');
- *
- *
- * @param mixed ({String} | {Array}) JS file(s).
- * @return {Void}
- */
-function pH7Include(mFile) {
-    // Verify that the method isArray is available in the JavaScript version of the web browser (e.g., IE8).
-    if (!Array.isArray) {
-        Array.isArray = function (mArg) {
-            return Object.prototype.toString.call(mArg) === '[object Array]';
-        };
-    }
-
-
-    if (Array.isArray(mFile)) {
-        for (iF in mFile) pH7Include(mFile[iF]);
-    }
-    else {
-        var sHead = document.getElementsByTagName('head')[0];
-        var sFoorer = document.getElementsByTagName('footer')[0]; // Only with HTML 5, more this tag must be present in the HTML document, but allows faster loading of the page because the files are loaded last.
-        var mContainer = (sFoorer ? sFoorer : (sHead ? sHead : false));
-
-        if (mContainer) {
-            var oScript = document.createElement('script');
-            oScript.src = mFile;
-            /*
-             // With HTML5 this is no longer necessary.
-             oScript.type = 'text/javascript';
-             */
-
-            mContainer.appendChild(oScript);
-        }
-        else {
-            alert('"pH7Include()" function must be included in a valid HTML code.');
-        }
-    }
-}
-
-/**
- * For target_blank with window.open JavaScript method.
+ * Force target_blank with window.open JavaScript method.
  */
 (function () {
     $('a').click(function () {
