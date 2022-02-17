ba3db7a755015387056ff9d11a5c2ecdf9ae8115
Pierre-Henry Soria <hi@ph7.me>
2022-02-17 20:28:02 +1030

    [BulkAction] Use strict type declarations

--- _protected/app/includes/classes/BulkAction.php
+++ _protected/app/includes/classes/BulkAction.php
@@ -1,10 +1,12 @@
 <?php
 /**
  * @author           Pierre-Henry Soria <hello@ph7cms.com>
- * @copyright        (c) 2021, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright        (c) 2021-2022, Pierre-Henry Soria. All Rights Reserved.
  * @license          MIT License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / App / Include / Class
  */
+
+declare(strict_types=1);
 
 namespace PH7;
 
@@ -13,12 +15,8 @@ trait BulkAction
     /**
      * Determines if the action can be processed or not
      * by checking if the POST 'actions' has a correct value.
-     *
-     * @param array|null $mActions
-     *
-     * @return bool
      */
-    protected function areActionsEligible($mActions)
+    protected function areActionsEligible(?array $mActions): bool
     {
         return !empty($mActions) && is_array($mActions) && count($mActions) > 0;
     }
