f02d3f0cab1bd9b495b58caad137177ebe5ecdc7
Pierre-Henry Soria <hi@ph7.me>
2022-03-28 07:15:46 +1030

    [UnitTests] Fixing "Passing null to parameter is deprecated"

--- _tests/Unit/App/System/Core/Classes/MediaCoreTest.php
+++ _tests/Unit/App/System/Core/Classes/MediaCoreTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author           Pierre-Henry Soria <hello@ph7cms.com>
- * @copyright        (c) 2017-2019, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright        (c) 2017-2022, Pierre-Henry Soria. All Rights Reserved.
  * @license          MIT License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Test / Unit / App / System / Core / Classes
  */
@@ -13,7 +13,7 @@ require_once PH7_PATH_SYS . 'core/classes/MediaCore.php';
 use PH7\MediaCore;
 use PHPUnit\Framework\TestCase;
 
-class MediaCoreTest extends TestCase
+final class MediaCoreTest extends TestCase
 {
     public function testTitle(): void
     {
--- _tests/Unit/App/System/Core/Classes/UserBirthDateCoreTest.php
+++ _tests/Unit/App/System/Core/Classes/UserBirthDateCoreTest.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @author           Pierre-Henry Soria <hello@ph7cms.com>
- * @copyright        (c) 2018-2019, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright        (c) 2018-2022, Pierre-Henry Soria. All Rights Reserved.
  * @license          MIT License; See PH7.LICENSE.txt and PH7.COPYRIGHT.txt in the root directory.
  * @package          PH7 / Test / Unit / App / System / Core / Classes
  */
@@ -15,12 +15,12 @@ require_once PH7_PATH_SYS . 'core/classes/UserBirthDateCore.php';
 use PH7\UserBirthDateCore;
 use PHPUnit\Framework\TestCase;
 
-class UserBirthDateCoreTest extends TestCase
+final class UserBirthDateCoreTest extends TestCase
 {
     /**
      * @dataProvider invalidBirthDateProvider
      */
-    public function testInvalidAgeFromBirthDate(?string $sBirthDate): void
+    public function testInvalidAgeFromBirthDate(string $sBirthDate): void
     {
         $iAge = UserBirthDateCore::getAgeFromBirthDate($sBirthDate);
 
@@ -32,7 +32,7 @@ class UserBirthDateCoreTest extends TestCase
         return [
             ['01902'],
             ['01-20-2919-29'],
-            [null]
+            ['']
         ];
     }
 }
