e0e5ded4356bf9fad93aa002a6d9365a2be7a4d4
Pierre-Henry Soria <hi@ph7.me>
2022-08-01 08:20:41 +0930

    Refactoring: Adding types, access visibility, and cast properly return values

--- _protected/framework/Cron/Run/Cron.class.php
+++ _protected/framework/Cron/Run/Cron.class.php
@@ -1,10 +1,9 @@
 <?php
 /**
- * @title            Cron Class
  * @desc             Generic class for the Periodic Cron.
  *
  * @author           Pierre-Henry Soria <hello@ph7builder.com>
- * @copyright        (c) 2012-2019, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright        (c) 2012-2022, Pierre-Henry Soria. All Rights Reserved.
  * @license          MIT License; See LICENSE.md and COPYRIGHT.md in the root directory.
  * @package          PH7 / Framework / Cron / Run
  */
@@ -18,25 +17,22 @@ use PH7\Framework\Url\Uri;
 
 abstract class Cron extends Core
 {
-    const URI_FILENAME_INDEX = 3;
-    const URI_DELAY_INDEX = 2;
-    const HOUR_IN_SECS = 3600;
-    const DELAY_FILE_EXT = '.txt';
+    private const URI_FILENAME_INDEX = 3;
+    private const URI_DELAY_INDEX = 2;
+    private const HOUR_IN_SECS = 3600;
+    private const DELAY_FILE_EXT = '.txt';
 
     /**
      * If the server takes time loading the script,
      * we add 10 secs to make sure it won't be seen as "already ran within the cron delay"
      */
-    const DELAY_TOLERANCE_INTERVAL_IN_SEC = 10;
+    private const DELAY_TOLERANCE_INTERVAL_IN_SEC = 10;
 
-    /** @var int */
-    private $iTime;
+    private int $iTime;
 
-    /** @var Uri */
-    private $oUri;
+    private Uri $oUri;
 
-    /** @var string */
-    private $sDelayPathFile;
+    private string $sDelayPathFile;
 
     public function __construct()
     {
@@ -50,7 +46,7 @@ abstract class Cron extends Core
     /**
      * @return bool Returns TRUE if the delay is valid, otherwise FALSE.
      */
-    public function checkDelay()
+    protected function checkDelay(): bool
     {
         $bStatus = true; // Default delay status is TRUE
 
@@ -72,12 +68,12 @@ abstract class Cron extends Core
     }
 
     /**
-     * @return bool|string The file contents if it exists, FALSE otherwise.
+     * @return int|false The file contents if it exists, FALSE otherwise.
      */
-    private function getSavedDelay()
+    private function getSavedDelay(): bool|int
     {
         if ($this->file->existFile($this->sDelayPathFile)) {
-            return $this->file->getFile($this->sDelayPathFile);
+            return (int)$this->file->getFile($this->sDelayPathFile);
         }
 
         return false;
@@ -86,7 +82,7 @@ abstract class Cron extends Core
     /**
      * @return string The current cron filename.
      */
-    private function getFileName()
+    private function getFileName(): string
     {
         return strtolower($this->oUri->fragment(self::URI_FILENAME_INDEX));
     }
@@ -94,7 +90,7 @@ abstract class Cron extends Core
     /**
      * @return string The full path of the delayed text file containing the UNIX time of the last cron job execution.
      */
-    private function getDelayedFilePath()
+    private function getDelayedFilePath(): string
     {
         return PH7_PATH_SYS . 'core/assets/cron/_delay/' . $this->getFileName() . self::DELAY_FILE_EXT;
     }
@@ -104,7 +100,7 @@ abstract class Cron extends Core
      *
      * @return int Delay in hour.
      */
-    private function getCronDelay()
+    private function getCronDelay(): int
     {
         /**
          * @internal We cast the value into integer type to get only the integer data (without the 'h' character).
@@ -117,7 +113,7 @@ abstract class Cron extends Core
      *
      * @return bool Returns FALSE if the delay hasn't yet elapsed, TRUE otherwise.
      */
-    private function hasDelayPassed($iCronTime)
+    private function hasDelayPassed($iCronTime): bool
     {
         return $iCronTime <= $this->iTime + self::DELAY_TOLERANCE_INTERVAL_IN_SEC;
     }
@@ -127,7 +123,7 @@ abstract class Cron extends Core
      *
      * @return int Seconds
      */
-    private function convertHoursToSeconds($iHours)
+    private function convertHoursToSeconds(int $iHours): int
     {
         return $iHours * self::HOUR_IN_SECS;
     }
