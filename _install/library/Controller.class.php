be15e07968b5146c3672c9a13e01e31602a44d1f
♚ PH⑦ de Soria™♛ <hi@ph7.me>
2023-05-12 18:29:28 +1000

    Update header

--- _install/library/Controller.class.php
+++ _install/library/Controller.class.php
@@ -1,11 +1,9 @@
 <?php
 /**
- * @title            Controller Core Class
- *
  * @author           Pierre-Henry Soria <hello@ph7builder.com>
- * @copyright        (c) 2012-2021, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright        (c) 2012-2023, Pierre-Henry Soria. All Rights Reserved.
  * @license          MIT License; See LICENSE.md and COPYRIGHT.md in the root directory.
- * @link             http://ph7builder.com
+ * @link             https://ph7builder.com
  * @package          PH7 / Install / Library
  */
 
