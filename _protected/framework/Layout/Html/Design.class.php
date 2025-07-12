pH7Software/pH7-Social-Dating-CMS

commit 005fe0633506e0f44d0c62e1f92278a6ec4a3dfd

Fix #1108 type error where strict typing would fail on `Url::encode()`

_protected/framework/Layout/Html/Design.class.php
@@ -6,8 +6,6 @@
  * @package          PH7 / Framework / Layout / Html
  */
 
-declare(strict_types=1);
-
 namespace PH7\Framework\Layout\Html;
 
 defined('PH7') or exit('Restricted access');
@@ -309,12 +307,12 @@ class Design
      * @param string $sMod
      * @param string $sCtrl
      * @param string $sAct
-     * @param int|string $mId Content ID
-     * @param string $sClass Add a CSS class
+     * @param string $sId Content ID
+     * @param string|null $sClass Add a CSS class
      *
      * @return void HTML output.
      */
-    public function popupLinkConfirm($sLabel, $sMod, $sCtrl, $sAct, $mId, $sClass = null)
+    public function popupLinkConfirm(string $sLabel, string $sMod, string $sCtrl, string $sAct, string $sId, ?string $sClass = null): void
     {
         $sClass = $sClass !== null ? ' class="' . $sClass . '" ' : ' ';
 

commit 16ed2d73566ba916dda1c1db717da1272432656f

[Fix #1182] Incorrect mismatched variable prefix naming (`$sId` and `$mId`)
Bug was introduced in https://github.com/pH7Software/pH7-Social-Dating-CMS/commit/005fe0633506e0f44d0c62e1f92278a6ec4a3dfd

_protected/framework/Layout/Html/Design.class.php
@@ -322,7 +322,7 @@ class Design
             'mod' => $sMod,
             'ctrl' => $sCtrl,
             'act' => $sAct,
-            'id' => Url::encode($mId)
+            'id' => Url::encode($sId)
         ];
 
         echo '<a', $sClass, 'href="', PH7_URL_ROOT, 'asset/ajax/popup/confirm/?', Url::httpBuildQuery($aHttpParams), '" data-popup="classic">', $sLabel, '</a>';