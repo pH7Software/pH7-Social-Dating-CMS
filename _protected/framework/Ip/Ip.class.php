312ed39533591ad9c269b906ae94ac7b29340fee
Pierre-Henry Soria <hi@ph7.me>
2026-01-06 21:52:58 +1100

    Update copyright year in Ip.class.php to 2026

--- _protected/framework/Ip/Ip.class.php
+++ _protected/framework/Ip/Ip.class.php
@@ -4,10 +4,9 @@
  * @desc             Helper for the IP Class.
  *
  * @author           Pierre-Henry Soria <hello@ph7builder.com>
- * @copyright        (c) 2012-2019, Pierre-Henry Soria. All Rights Reserved.
+ * @copyright        (c) 2012-2026, Pierre-Henry Soria. All Rights Reserved.
  * @license          MIT License; See LICENSE.md and COPYRIGHT.md in the root directory.
  * @package          PH7 / Framework / Ip
- * @version          1.2
  */
 
 namespace PH7\Framework\Ip;
