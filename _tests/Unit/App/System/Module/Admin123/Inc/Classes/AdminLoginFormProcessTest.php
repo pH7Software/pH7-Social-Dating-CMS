936512ef755b31798685de243f229a024935fc16
Pierre-Henry Soria <hi@ph7.me>
2026-04-18 21:08:36 +1000

    GH-0000 Split admin login rehash test to enforce single class per file

---
+++ _tests/Unit/App/System/Module/Admin123/Inc/Classes/AdminLoginFormProcessTest.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * @author           Pierre-Henry Soria <hello@ph7cms.com>
+ * @copyright        (c) 2026, Pierre-Henry Soria. All Rights Reserved.
+ * @license          MIT License; See LICENSE.md and COPYRIGHT.md in the root directory.
+ * @package          PH7 / Test / Unit / App / System / Module / Admin123 / Inc / Classes
+ */
+
+declare(strict_types=1);
+
+namespace PH7\Test\Unit\App\System\Module\Admin123\Inc\Classes;
+
+require_once PH7_PATH_SYS_MOD . 'admin123/models/AdminModel.php';
+require_once PH7_PATH_SYS_MOD . 'admin123/forms/processing/LoginFormProcess.php';
+
+use PH7\AdminModel;
+use PH7\DbTableName;
+use PH7\LoginFormProcess;
+use Phake;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+final class AdminLoginFormProcessTest extends TestCase
+{
+    public function testUpdatePwdHashIfNeededUpdatesOutdatedAdminHash(): void
+    {
+        $oAdminModelMock = Phake::mock(AdminModel::class);
+        $oReflection = new ReflectionClass(LoginFormProcess::class);
+
+        /** @var LoginFormProcess $oLoginFormProcess */
+        $oLoginFormProcess = $oReflection->newInstanceWithoutConstructor();
+
+        $oAdminModelProp = $oReflection->getProperty('oAdminModel');
+        $oAdminModelProp->setAccessible(true);
+        $oAdminModelProp->setValue($oLoginFormProcess, $oAdminModelMock);
+
+        $sOutdatedHash = password_hash('password', PASSWORD_BCRYPT, ['cost' => 4]);
+        $oLoginFormProcess->updatePwdHashIfNeeded('password', (string)$sOutdatedHash, 'admin@ph7.me');
+
+        $sNewHash = '';
+        Phake::verify($oAdminModelMock)->changePassword(
+            'admin@ph7.me',
+            Phake::capture($sNewHash),
+            DbTableName::ADMIN
+        );
+        $this->assertNotEmpty($sNewHash);
+        $this->assertTrue(password_verify('password', $sNewHash));
+        $this->assertNotSame($sOutdatedHash, $sNewHash);
+    }
+}
