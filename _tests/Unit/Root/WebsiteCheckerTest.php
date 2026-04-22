5fc77698bd1a5ef45d40bf0110b489f2213d24ed
Pierre-Henry Soria <hi@ph7.me>
2026-04-22 20:47:22 +1000

    Add unit tests for WebsiteChecker to validate PHP version checks and installation folder existence

---
+++ _tests/Unit/Root/WebsiteCheckerTest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * @author           Pierre-Henry Soria <hello@ph7cms.com>
+ * @copyright        (c) 2026, Pierre-Henry Soria. All Rights Reserved.
+ * @license          MIT License; See LICENSE.md and COPYRIGHT.md in the root directory.
+ * @package          PH7 / Test / Unit / Root
+ */
+
+declare(strict_types=1);
+
+namespace PH7\Test\Unit\Root;
+
+require_once dirname(__DIR__, 3) . '/WebsiteChecker.php';
+
+use PH7\WebsiteChecker;
+use PHPUnit\Framework\TestCase;
+use ReflectionClass;
+
+final class WebsiteCheckerTest extends TestCase
+{
+    public function testCheckPhpVersionDoesNotThrowOnSupportedRuntime(): void
+    {
+        $oChecker = new WebsiteChecker;
+        $oChecker->checkPhpVersion();
+
+        $this->addToAssertionCount(1);
+    }
+
+    public function testInstallFolderExistsInProjectRoot(): void
+    {
+        $this->assertTrue((new WebsiteChecker)->doesInstallFolderExist());
+    }
+
+    public function testNoConfigFoundMessageIsStable(): void
+    {
+        $this->assertSame(
+            'CONFIG FILE NOT FOUND! If you want to make a new installation, please re-upload _install/ folder and clear your database.',
+            (new WebsiteChecker)->getNoConfigFoundMessage()
+        );
+    }
+
+    public function testIncompatiblePhpVersionFlagIsFalseOnCurrentRuntime(): void
+    {
+        $oChecker = new WebsiteChecker;
+        $oReflection = new ReflectionClass(WebsiteChecker::class);
+        $oMethod = $oReflection->getMethod('isIncompatiblePhpVersion');
+        $oMethod->setAccessible(true);
+
+        $this->assertFalse($oMethod->invoke($oChecker));
+    }
+}
