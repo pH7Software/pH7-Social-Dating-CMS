bbdb62e9c235c2302e9e7982eb89546e90ae05bf
Pierre-Henry Soria <hi@ph7.me>
2026-04-29 18:07:46 +1000

    Harden cache reads to prevent object injection from serialized payloads

--- _protected/framework/Cache/Cache.class.php
+++ _protected/framework/Cache/Cache.class.php
@@ -142,7 +142,7 @@ class Cache implements GenerableFile
         $mData = $this->read($bPrint);
 
         if ($mData !== false) {
-            $mData = unserialize($mData);
+            $mData = unserialize($mData, ['allowed_classes' => false]);
         }
 
         return $mData;
