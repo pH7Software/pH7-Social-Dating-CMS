ed743cca80fe8509732f375fd0ce24ec386ecd52
Pierre-Henry Soria <hi@ph7.me>
2026-05-26 08:11:19 +1000

    Bump pH7 Social Dating Builder version to v18.3 :tada:

--- _protected/framework/Security/Version.class.php
+++ _protected/framework/Security/Version.class.php
@@ -51,9 +51,9 @@ final class Version
      *
      * More details: https://ph7builder.com/new-versioning-system/
      */
-    public const KERNEL_VERSION = '18.2.0';
+    public const KERNEL_VERSION = '18.3.0';
     public const KERNEL_BUILD = '1';
-    public const KERNEL_RELEASE_DATE = '2026-04-19';
+    public const KERNEL_RELEASE_DATE = '2026-05-27';
 
     /*** Framework Server ***/
     public const KERNEL_TECHNOLOGY_NAME = 'pH7Builder.com';
