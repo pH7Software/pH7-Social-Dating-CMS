c36afafb3cd3dd3d6f1c28e937f2b70d67a36d93
Pierre-Henry Soria <hi@ph7.me>
2026-07-29 16:01:35 +1000

    Encode AJAX messages to preserve JSON boundaries

--- _protected/framework/Ajax/Ajax.class.php
+++ _protected/framework/Ajax/Ajax.class.php
@@ -1,11 +1,11 @@
 <?php
+
 /**
  * @title            Ajax Helper Class
  *
  * @author           Pierre-Henry SORIA <hello@ph7builder.com>
  * @copyright        (c) 2012-2019, Pierre-Henry Soria. All Rights Reserved.
  * @license          MIT License; See LICENSE.md and COPYRIGHT.md in the root directory.
- * @package          PH7 / Framework / Ajax
  */
 
 declare(strict_types=1);
@@ -17,13 +17,18 @@ namespace PH7\Framework\Ajax {
     {
         /**
          * @param int $iStatus 1 = success | 0 = error
-         * @param string $sTxt
          *
          * @return string JSON Format
          */
         public static function jsonMsg(int $iStatus, string $sTxt): string
         {
-            return '{"status":' . $iStatus . ',"txt":"' . $sTxt . '"}';
+            return json_encode(
+                [
+                    'status' => $iStatus,
+                    'txt' => $sTxt
+                ],
+                JSON_UNESCAPED_UNICODE | JSON_INVALID_UTF8_SUBSTITUTE
+            );
         }
     }
 }
